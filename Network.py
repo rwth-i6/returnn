cb19ce8cec37ff4f779526dec54f18d413d618db
doetsch <patrick.doetsch@gmail.com>
2014-12-28 17:06:03 +0100

    fixed projection layer in lstm output

--- Network.py
+++ Network.py
@@ -321,13 +321,14 @@ class LstmLayer(RecurrentLayer):
     n_in = sum([s.attrs['n_out'] for s in sources])
     n_re = projection if projection != None else n_out
     self.state = self.create_bias(n_out, 'state')
-    self.act = self.create_bias(n_out, 'act')
+    self.act = self.create_bias(n_re, 'act')
+    self.b.set_value(numpy.zeros((n_out * 3 + n_re,), dtype = theano.config.floatX))
     W_proj = self.create_uniform_weights(n_out, n_re, n_in + n_out + n_re, "W_proj_%s"%self.name)
     self.W_proj.set_value(W_proj.get_value())
-    W_re = self.create_uniform_weights(n_re, n_out * 4, n_in + n_re + n_out * 4, "W_re_%s"%self.name)
+    W_re = self.create_uniform_weights(n_re, n_out * 3 + n_re, n_in + n_re + n_out * 3 + n_re, "W_re_%s"%self.name)
     self.W_re.set_value(W_re.get_value())
     for s, W in zip(sources, self.W_in):
-      W.set_value(self.create_uniform_weights(s.attrs['n_out'], n_out * 4, s.attrs['n_out'] + n_out  + n_out * 4, "W_in_%s_%s"%(s.name,self.name)).get_value(borrow=True, return_internal_type=True), borrow = True)
+      W.set_value(self.create_uniform_weights(s.attrs['n_out'], n_out * 3 + n_re, s.attrs['n_out'] + n_out  + n_out * 3 + n_re, "W_in_%s_%s"%(s.name,self.name)).get_value(borrow=True, return_internal_type=True), borrow = True)
     self.o.set_value(numpy.ones((n_out,), dtype='int8'))
     if projection:
       self.set_attr('n_out', projection)
@@ -344,7 +345,7 @@ class LstmLayer(RecurrentLayer):
         LstmLayer.sharpgates = self.create_bias(1)
         self.add_param(LstmLayer.sharpgates, 'gate_scaling')
       self.sharpness = LstmLayer.sharpgates
-    else: self.sharpness = theano.shared(value = numpy.zeros((3,), dtype=theano.config.floatX), borrow=True, name = 'lambda') #self.create_bias(3)
+    else: self.sharpness = theano.shared(value = numpy.zeros((3,), dtype=theano.config.floatX), borrow=True, name = 'lambda')
     self.sharpness.set_value(numpy.ones(self.sharpness.get_value().shape, dtype = theano.config.floatX))
     if sharpgates != 'none' and sharpgates != "shared" and sharpgates != "single": self.add_param(self.sharpness, 'gate_scaling')
 
@@ -357,30 +358,29 @@ class LstmLayer(RecurrentLayer):
 
     def step(z, i_t, s_p, h_p):
       z += T.dot(h_p, self.W_re)
-      i = T.outer(i_t, self.o)
-      partition = z.shape[1] / 4
+      i = T.outer(i_t, T.alloc(numpy.ones((n_out,), 'int8'), n_out))
+      j = i if not self.W_proj else T.outer(i_t, T.alloc(numpy.ones((n_re,), 'int8'), n_re))
       if sharpgates != 'none':
-        ingate = GI(self.sharpness[0] * z[:,partition: 2 * partition])
-        forgetgate = GF(self.sharpness[1] * z[:,2 * partition:3 * partition])
-        outgate = GO(self.sharpness[2] * z[:,3 * partition:4 * partition])
+        ingate = GI(self.sharpness[0] * z[:,n_out: 2 * n_out])
+        forgetgate = GF(self.sharpness[1] * z[:,2 * n_out:3 * n_out])
+        outgate = GO(self.sharpness[2] * z[:,3 * n_out:])
       else:
-        ingate = GI(z[:,partition: 2 * partition])
-        forgetgate = GF(z[:,2 * partition:3 * partition])
-        outgate = GO(z[:,3 * partition:4 * partition])
-      input = CI(z[:,:partition])
-      s_t = input * ingate + s_p * forgetgate
+        ingate = GI(z[:,n_out: 2 * n_out])
+        forgetgate = GF(z[:,2 * n_out:3 * n_out])
+        outgate = GO(z[:,3 * n_out:])
+      input = CI(z[:,:n_out])
+      s_i = input * ingate + s_p * forgetgate
+      s_t = s_i if not self.W_proj else T.dot(s_i, self.W_proj)
       h_t = CO(s_t) * outgate
-      if self.W_proj: h_q = CO(T.dot(h_t, self.W_proj))
-      else: h_q = h_t
-      return s_t * i, h_q * i
+      return s_i * i, h_t * j
 
     [state, act], _ = theano.scan(step,
                                   name = "scan_%s"%self.name,
                                   truncate_gradient = self.attrs['truncation'],
                                   go_backwards = self.attrs['reverse'],
                                   sequences = [ z, self.index ],
-                                  outputs_info = [ T.alloc(self.state, self.sources[0].output.shape[1], self.attrs['n_out']),
-                                                   T.alloc(self.act, self.sources[0].output.shape[1], n_re)]) #self.attrs['n_out']), ])
+                                  outputs_info = [ T.alloc(self.state, self.sources[0].output.shape[1], n_out),
+                                                   T.alloc(self.act, self.sources[0].output.shape[1], n_re), ])
 
     self.output = act[::-(2 * self.attrs['reverse'] - 1)]
 
