5b34c451fe3955abef525b17342d96bc51860497
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-01-22 11:54:06 +0100

    Config.py: formating fixes

--- Config.py
+++ Config.py
@@ -2,15 +2,17 @@
 
 __author__ = "Patrick Doetsch"
 __copyright__ = "Copyright 2014"
-__credits__ = ["Patrick Doetsch", "Paul Voigtlaender" ]
+__credits__ = ["Patrick Doetsch", "Paul Voigtlaender"]
 __license__ = "GPL"
 __version__ = "0.9"
 __maintainer__ = "Patrick Doetsch"
 __email__ = "doetsch@i6.informatik.rwth-aachen.de"
 
+
 class Config:
   def __init__(self):
     self.dict = {}
+
   def load_file(self, filename):
     for line in open(filename).readlines():
       if len(line.strip()) == 0 or line.strip()[0] == '#':
@@ -24,46 +26,51 @@ class Config:
       else:
         value = [value]
       self.dict[key] = value
-      
+
   def has(self, key):
-    return self.dict.has_key(key)
+    return key in self.dict
 
   def set(self, key, value):
     self.dict[key] = value
-  
-  def value(self, key, default, index = 0):
-    if not self.dict.has_key(key):
+
+  def value(self, key, default, index=0):
+    if key not in self.dict:
       return default
     return self.dict[key][index]
+
   def int(self, key, default, index=0):
     return int(self.value(key, default, index))
+
   def bool(self, key, default, index=0):
-    v = str(self.value(key, default, index)).lower()    
+    v = str(self.value(key, default, index)).lower()
     if v == "true" or v == "1":
       return True
     if v == "false" or v == "0":
       return False
     return default
-  def float(self, key, default, index = 0):
+
+  def float(self, key, default, index=0):
     return float(self.value(key, default, index))
-  
-  def list(self, key, default = None):
-    if default == None: default = []
-    if not self.dict.has_key(key):
+
+  def list(self, key, default=None):
+    if default is None: default = []
+    if key not in self.dict:
       return default
     return self.dict[key]
-  
-  def int_list(self, key, default = None):
-    if default == None: default = []
+
+  def int_list(self, key, default=None):
+    if default is None: default = []
     return [int(x) for x in self.list(key, default)]
-  def float_list(self, key, default = None):
-    if default == None: default = []
+
+  def float_list(self, key, default=None):
+    if default is None: default = []
     return [float(x) for x in self.list(key, default)]
-  def int_pair(self, key, default = None):
-    if default == None: default = (0,0)
+
+  def int_pair(self, key, default=None):
+    if default is None: default = (0, 0)
     if not self.has(key): return default
     value = self.value(key, '')
     if ':' in value:
-      return int(value.split(':')[0]),int(value.split(':')[1])
+      return int(value.split(':')[0]), int(value.split(':')[1])
     else:
       return int(value), int(value)
