b6c987695721a4a5d36b65dae06c8aec05e311f9
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-02-27 16:28:04 +0100

    comment Batch class

--- Engine.py
+++ Engine.py
@@ -15,14 +15,41 @@ import Device
 
 
 class Batch:
+  """
+  A batch can consists of several sequences (= segments).
+  Note that self.shape[1] is a different kind of batch - related to the data-batch-idx.
+  """
+
   def __init__(self, start = (0, 0)):
     self.shape = [0, 0]  # format (time,batch)
     self.start = list(start)
-    self.nseqs = 1
-  def try_sequence(self, length): return [max(self.shape[0], length), self.shape[1] + 1]
-  def add_sequence(self, length): self.shape = self.try_sequence(length)
-  def add_frames(self, length): self.shape = [self.shape[0] + length, max(self.shape[1], 1)]
-  def size(self): return self.shape[0] * self.shape[1]
+    self.nseqs = 1  # number of sequences which we cover (not data-batches self.shape[1])
+
+  def try_sequence(self, length):
+    """
+    :param int length: number of (time) frames
+    :return: new shape which covers the old shape and one more data-batch
+    :rtype: list[int]
+    """
+    return [max(self.shape[0], length), self.shape[1] + 1]
+
+  def add_sequence(self, length):
+    """
+    Adds one data-batch.
+    :param int length: number of (time) frames
+    """
+    self.shape = self.try_sequence(length)
+
+  def add_frames(self, length):
+    """
+    Adds frames to all data-batches.
+    Will add one data-batch if we don't have one yet.
+    :param int length: number of (time) frames
+    """
+    self.shape = [self.shape[0] + length, max(self.shape[1], 1)]
+
+  def size(self):
+    return self.shape[0] * self.shape[1]
 
 
 class TaskThread(threading.Thread):
