86751d61d7a9994e5fe3516a240728f0628d7395
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-03-03 14:19:00 +0100

    formating

--- SprintCommunicator.py
+++ SprintCommunicator.py
@@ -21,14 +21,14 @@ class ShMemOp(theano.Op):
              #include <sys/shm.h>
              #include <stdint.h>
              #include <sched.h>
-             
+
              typedef uint32_t u32;
-             
+
              enum TheanoStatus
              {
 	       IDLE = 0, REQUEST_ERRORSIGNAL = 1, ERRORSIGNAL_WRITTEN = 2, TERMINATED = 4
              };
-               
+
              namespace {
                /*layout of shared memory:
                4 bytes TheanoStatus flag
@@ -46,44 +46,44 @@ class ShMemOp(theano.Op):
                const u32 DATA_BEGIN = CTL_SEG_SIZE;
                const u32 MAX_SEG_NAME_LEN = DATA_BEGIN - SEGMENT_NAME_BEGIN;
              }
-             
+
              template <typename T>
              //careful: the offset is in bytes and not related to T
              T& at(void * shMem, u32 offset)
              {
                return *reinterpret_cast<T*>(static_cast<char*>(shMem) + offset);
              }
-             
+
              u32& shMemStatus(void * shMem)
              {
                return at<u32>(shMem, STATUS_BEGIN);
              }
-             
+
              float& shMemData(void * shMem, u32 idx)
              {
                return at<float>(shMem, DATA_BEGIN + 4 * idx);
              }
-             
+
              char& shMemSegNameChar(void * shMem, u32 idx)
              {
                return at<char>(shMem, SEGMENT_NAME_BEGIN + idx);
              }
-             
+
              u32& shMemRows(void * shMem)
              {
                return at<u32>(shMem, ROWS_BEGIN);
              }
-             
+
              u32& shMemCols(void * shMem)
              {
                return at<u32>(shMem, COLS_BEGIN);
              }
-             
+
              float& shMemLoss(void * shMem)
              {
                return at<float>(shMem, LOSS_BEGIN);
              }
-             
+
              void waitForStatus(void * shMem, u32 status)
              {
                while(shMemStatus(shMem) != status) {
@@ -95,12 +95,12 @@ class ShMemOp(theano.Op):
     with open(path + '/C_Support_Code.cpp', 'r') as f:
         src += f.read()
     return src
-    
+
 class AllocShMemOp(ShMemOp):
   def make_node(self, key):
     key = theano.tensor.as_tensor_variable(key)
     return theano.Apply(self, [key], [T.scalar('shId', 'int32'), T.scalar('shMem', 'int64')])
-  
+
   def c_code(self, node, name, inp, out, sub):
     fail = sub['fail']
     key, = inp
@@ -120,12 +120,12 @@ class AllocShMemOp(ShMemOp):
   #IMPORTANT: change this, if you change the c-code
   def c_code_cache_version(self):
     return (1.02,)
-    
+
 class DetachShMemOp(ShMemOp):
   def make_node(self, sh_mem):
     sh_mem = theano.tensor.as_tensor_variable(sh_mem)
     return theano.Apply(self, [sh_mem], [T.fscalar()]) #dummy return value
-  
+
   def c_code(self, node, name, inp, out, sub):
     fail = sub['fail']
     shMem, = inp
@@ -136,17 +136,17 @@ class DetachShMemOp(ShMemOp):
   #IMPORTANT: change this, if you change the c-code
   def c_code_cache_version(self):
     return (1.02,)
-    
+
 class CopyToShMemOp(ShMemOp):
   def make_node(self, sh_mem, data):
     sh_mem = theano.tensor.as_tensor_variable(sh_mem)
     data = theano.tensor.as_tensor_variable(data)
     return theano.Apply(self, [sh_mem, data], [T.fscalar()]) #dummy return value
-  
+
   def c_code(self, node, name, inp, out, sub):
     fail = sub['fail']
-    shMem, data = inp    
-    return """   
+    shMem, data = inp
+    return """
       CArrayF dataWr(%(data)s);
       int nRows = dataWr.dim(0);
       int nCols = dataWr.dim(1);
@@ -166,13 +166,13 @@ class CopyToShMemOp(ShMemOp):
   #IMPORTANT: change this, if you change the c-code
   def c_code_cache_version(self):
     return (1.08,)
-    
+
 class WriteStatusOp(ShMemOp):
   def make_node(self, sh_mem, status):
     sh_mem = theano.tensor.as_tensor_variable(sh_mem)
     status = theano.tensor.as_tensor_variable(status)
     return theano.Apply(self, [sh_mem, status], [T.fscalar()]) #dummy return value
-  
+
   def c_code(self, node, name, inp, out, sub):
     fail = sub['fail']
     shMem, status = inp
@@ -183,13 +183,13 @@ class WriteStatusOp(ShMemOp):
   #IMPORTANT: change this, if you change the c-code
   def c_code_cache_version(self):
     return (1.01,)
-    
+
 class WaitForStatusOp(ShMemOp):
   def make_node(self, sh_mem, status):
     sh_mem = theano.tensor.as_tensor_variable(sh_mem)
     status = theano.tensor.as_tensor_variable(status)
     return theano.Apply(self, [sh_mem, status], [T.fscalar()]) #dummy return value
-  
+
   def c_code(self, node, name, inp, out, sub):
     fail = sub['fail']
     shMem, status = inp
@@ -200,28 +200,28 @@ class WaitForStatusOp(ShMemOp):
   #IMPORTANT: change this, if you change the c-code
   def c_code_cache_version(self):
     return (1.01,)
-    
+
 class CopyFromShMemOp(ShMemOp):
   def make_node(self, sh_mem):
     sh_mem = theano.tensor.as_tensor_variable(sh_mem)
-    return theano.Apply(self, [sh_mem], [T.fvector(), T.fmatrix()])  
-  
+    return theano.Apply(self, [sh_mem], [T.fvector(), T.fmatrix()])
+
   def c_code(self, node, name, inp, out, sub):
     fail = sub['fail']
     shMem, = inp
-    loss, errsig, = out    
+    loss, errsig, = out
     return """
       Py_XDECREF(%(loss)s);
       Py_XDECREF(%(errsig)s);
-      
+
       npy_intp lossDims[] = {1};
       %(loss)s = (PyArrayObject*) PyArray_Zeros(1, lossDims, PyArray_DescrFromType(NPY_FLOAT32), 0);
       verify(%(loss)s);
       ArrayF lossWr(%(loss)s);
       lossWr(0) = shMemLoss(datavoid(%(shMem)s));
-      
+
       u32 nRows = shMemRows(datavoid(%(shMem)s));
-      u32 nCols = shMemCols(datavoid(%(shMem)s));            
+      u32 nCols = shMemCols(datavoid(%(shMem)s));
       npy_intp dims[] = {nRows, nCols};
       %(errsig)s = (PyArrayObject*) PyArray_Zeros(2, dims, PyArray_DescrFromType(NPY_FLOAT32), 0);
       verify(%(errsig)s);
@@ -263,34 +263,34 @@ class WriteSegmentNameOp(ShMemOp):
 
 class SprintCommunicator:
   IDLE, REQUEST_ERRORSIGNAL, ERRORSIGNAL_WRITTEN, TERMINATED = [0,1,2,4]
-  instance = None
-  
-  def __init__(self, key):    
-    self.segments = []  
-      
+  instance = None; """ :type: SprintCommunicator """
+
+  def __init__(self, key):
+    self.segments = []
+
     t_key = T.scalar('key', dtype='int32')
     t_sh_mem = T.scalar('sh_mem', dtype='int64')
     t_status = T.scalar('status', dtype='uint32')
     t_data = T.fmatrix('data')
     t_segment_name_int = T.ivector()
-    
+
     #alloc sh_mem
     f = theano.function([t_key], AllocShMemOp()(t_key), name="SprintCommunicator")
-    print >> log.v4, 'Allocating shared memory with key', key, '...' 
+    print >> log.v4, 'Allocating shared memory with key', key, '...'
     shId, self.sh_mem = f(key)
-        
+
     self.wait_for_status_fn = theano.function([t_sh_mem, t_status], WaitForStatusOp()(t_sh_mem, t_status))
     self.write_status_fn = theano.function([t_sh_mem, t_status], WriteStatusOp()(t_sh_mem, t_status))
     self.copy_to_sh_mem_fn = theano.function([t_sh_mem, t_data], CopyToShMemOp()(t_sh_mem, t_data))
     self.copy_from_sh_mem_fn = theano.function([t_sh_mem], CopyFromShMemOp()(t_sh_mem))
     self.detach_sh_mem_fn = theano.function([t_sh_mem], DetachShMemOp()(t_sh_mem))
     self.write_segment_name_fn = theano.function([t_sh_mem, t_segment_name_int], WriteSegmentNameOp()(t_sh_mem, t_segment_name_int))
-    
+
   def __write_segment_name(self, seg_name):
     seg_name_int = numpy.array([ord(c) for c in seg_name], dtype='int32')
     self.write_segment_name_fn(self.sh_mem, seg_name_int)
-  
-  #returns (loss, errsig)  
+
+  #returns (loss, errsig)
   def get_error_signal(self, segment_names, posteriors, seq_lengths):
     errsig = numpy.zeros_like(posteriors)
     loss = []
@@ -312,7 +312,7 @@ class SprintCommunicator:
       errsig[:seq_lengths[i],i,:] = seg_errsig
       print >> log.v5, 'success!'
     return numpy.array(loss, dtype=Tfloat), errsig
-  
+
   def finalize(self):
     print >> log.v4, 'detaching shared memory...'
     self.write_status_fn(self.sh_mem, SprintCommunicator.TERMINATED)
