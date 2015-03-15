f1c5d0f5a9cd680aa419fa1bda49271ffa4c6150
pv182253 <p.voigtlaender@googlemail.com>
2015-03-16 00:29:09 +0100

    -added NormalizedLstmLayer (normalization not yet implemented)

--- Network.py
+++ Network.py
@@ -453,6 +453,82 @@ class LstmLayer(RecurrentLayer):
 
     self.output = act[::-(2 * self.attrs['reverse'] - 1)]
 
+class NormalizedLstmLayer(RecurrentLayer):
+  def __init__(self, sources, index, n_out, L1 = 0.0, L2 = 0.0, activation = T.nnet.sigmoid, reverse = False, truncation = -1, sharpgates = 'none' , dropout = 0, mask = "unity", projection = None, layer_class = "lstm", name = ""):
+    super(LstmLayer, self).__init__(sources, index, n_out * 4, L1, L2, activation, reverse, truncation, False, dropout, mask, projection, layer_class = layer_class, name = name)
+    if not isinstance(activation, (list, tuple)):
+      activation = [T.tanh, T.nnet.sigmoid, T.nnet.sigmoid, T.nnet.sigmoid, T.tanh]
+    else: assert len(activation) == 5, "lstm activations have to be specified as 5 tuple (input, ingate, forgetgate, outgate, output)"
+    self.set_attr('sharpgates', sharpgates)
+    CI, GI, GF, GO, CO = activation #T.tanh, T.nnet.sigmoid, T.nnet.sigmoid, T.nnet.sigmoid, T.tanh
+    n_in = sum([s.attrs['n_out'] for s in sources])
+    n_re = projection if projection != None else n_out
+    #self.state = self.create_bias(n_out, 'state')
+    #self.act = self.create_bias(n_re, 'act')
+    self.b.set_value(numpy.zeros((n_out * 3 + n_re,), dtype = theano.config.floatX))
+    if projection:
+      W_proj = self.create_uniform_weights(n_out, n_re, n_in + n_out + n_re, "W_proj_%s"%self.name)
+      self.W_proj.set_value(W_proj.get_value())
+    W_re = self.create_uniform_weights(n_re, n_out * 3 + n_re, n_in + n_re + n_out * 3 + n_re, "W_re_%s"%self.name)
+    self.W_re.set_value(W_re.get_value())
+    for s, W in zip(sources, self.W_in):
+      W.set_value(self.create_uniform_weights(s.attrs['n_out'], n_out * 3 + n_re, s.attrs['n_out'] + n_out  + n_out * 3 + n_re, "W_in_%s_%s"%(s.name,self.name)).get_value(borrow=True, return_internal_type=True), borrow = True)
+    self.o.set_value(numpy.ones((n_out,), dtype='int8'))
+    if projection:
+      self.set_attr('n_out', projection)
+    else:
+      self.set_attr('n_out', self.attrs['n_out'] / 4)
+    if sharpgates == 'global': self.sharpness = self.create_uniform_weights(3, n_out)
+    elif sharpgates == 'shared':
+      if not hasattr(LstmLayer, 'sharpgates'):
+        LstmLayer.sharpgates = self.create_bias(3)
+        self.add_param(LstmLayer.sharpgates, 'gate_scaling')
+      self.sharpness = LstmLayer.sharpgates
+    elif sharpgates == 'single':
+      if not hasattr(LstmLayer, 'sharpgates'):
+        LstmLayer.sharpgates = self.create_bias(1)
+        self.add_param(LstmLayer.sharpgates, 'gate_scaling')
+      self.sharpness = LstmLayer.sharpgates
+    else: self.sharpness = theano.shared(value = numpy.zeros((3,), dtype=theano.config.floatX), borrow=True, name = 'lambda')
+    self.sharpness.set_value(numpy.ones(self.sharpness.get_value().shape, dtype = theano.config.floatX))
+    if sharpgates != 'none' and sharpgates != "shared" and sharpgates != "single": self.add_param(self.sharpness, 'gate_scaling')
+
+    z = self.b
+    #TODO normalization
+    for x_t, W in zip(self.sources, self.W_in):
+      if self.attrs['mask'] == "unity":
+        z += T.dot(x_t.output, W)
+      else:
+        z += T.dot(x_t.output, self.mass * mask * W)
+
+    def step(z, i_t, s_p, h_p):
+      z += T.dot(h_p, self.W_re)
+      i = T.outer(i_t, T.alloc(numpy.cast['int8'](1), n_out))
+      j = i if not self.W_proj else T.outer(i_t, T.alloc(numpy.cast['int8'](1), n_re))
+      if sharpgates != 'none':
+        ingate = GI(self.sharpness[0] * z[:,n_out: 2 * n_out])
+        forgetgate = GF(self.sharpness[1] * z[:,2 * n_out:3 * n_out])
+        outgate = GO(self.sharpness[2] * z[:,3 * n_out:])
+      else:
+        ingate = GI(z[:,n_out: 2 * n_out])
+        forgetgate = GF(z[:,2 * n_out:3 * n_out])
+        outgate = GO(z[:,3 * n_out:])
+      input = CI(z[:,:n_out])
+      s_i = input * ingate + s_p * forgetgate
+      s_t = s_i if not self.W_proj else T.dot(s_i, self.W_proj)
+      h_t = CO(s_t) * outgate
+      return s_i * i, h_t * j
+
+    [state, act], _ = theano.scan(step,
+                                  name = "scan_%s"%self.name,
+                                  truncate_gradient = self.attrs['truncation'],
+                                  go_backwards = self.attrs['reverse'],
+                                  sequences = [ z, self.index ],
+                                  outputs_info = [ T.alloc(numpy.cast[theano.config.floatX](0), self.sources[0].output.shape[1], n_out),
+                                                   T.alloc(numpy.cast[theano.config.floatX](0), self.sources[0].output.shape[1], n_re), ])
+
+    self.output = act[::-(2 * self.attrs['reverse'] - 1)]
+
 class WLstmLayer(RecurrentLayer):
   def __init__(self, sources, index, n_out, L1 = 0.0, L2 = 0.0, activation = T.nnet.sigmoid, reverse = False, truncation = -1, sharpgates = 'none' , dropout = 0, mask = "unity", projection = None, layer_class = "lstm", name = ""):
     super(WLstmLayer, self).__init__(sources, index, n_out, L1, L2, activation, reverse, truncation, False, dropout, mask, projection, layer_class = layer_class, name = name)
@@ -1027,6 +1103,8 @@ class LayerNetwork(object):
             network.add_layer(layer, RecurrentLayer(**params), act)
           elif cl == 'lstm':
             network.add_layer(layer, LstmLayer(**params), act)
+          elif cl == 'norm_lstm':
+            network.add_layer(layer, NormalizedLstmLayer(**params), act)
           elif cl == 'maxlstm':
             network.add_layer(layer, MaxLstmLayer(**params), act)
           else:
@@ -1082,6 +1160,8 @@ class LayerNetwork(object):
             network.add_layer(layer, RecurrentLayer(**params), act)
           elif cl == 'lstm':
             network.add_layer(layer, LstmLayer(sharpgates = model[layer].attrs['sharpgates'], **params), act)
+          elif cl == 'norm_lstm':
+            network.add_layer(layer, NormalizedLstmLayer(sharpgates = model[layer].attrs['sharpgates'], **params), act)
           elif cl == 'maxlstm':
             network.add_layer(layer, MaxLstmLayer(sharpgates = model[layer].attrs['sharpgates'], n_cores = model[layer].attrs['n_cores'], **params), act)
           else:
@@ -1177,6 +1257,8 @@ class LayerNetwork(object):
           self.add_layer(name, RecurrentLayer(**params), info[2][0])
         elif info[0] == 'lstm':
           self.add_layer(name, LstmLayer(sharpgates = sharpgates, **params), info[2][0])
+        elif info[0] == 'norm_lstm':
+          self.add_layer(name, NormalizedLstmLayer(sharpgates = sharpgates, **params), info[2][0])
         elif info[0] == 'gatelstm':
           self.add_layer(name, GateLstmLayer(sharpgates = sharpgates, **params), info[2][0])
         elif info[0] == 'peep_lstm':
@@ -1210,6 +1292,8 @@ class LayerNetwork(object):
             self.add_layer(name, RecurrentLayer(**params), info[2][0])
           elif info[0] == 'lstm':
             self.add_layer(name, LstmLayer(sharpgates = sharpgates, **params), info[2][0])
+          elif info[0] == 'norm_lstm':
+            self.add_layer(name, NormalizedLstmLayer(sharpgates = sharpgates, **params), info[2][0])
           elif info[0] == 'gatelstm':
             self.add_layer(name, GateLstmLayer(sharpgates = sharpgates, **params), info[2][0])
           elif info[0] == 'peep_lstm':
