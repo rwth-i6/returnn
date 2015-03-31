f02b505b46621658f80b6a22832c419c3eef63ad
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-03-31 10:40:43 +0200

    SprintInterface forward: small fix if there is no segment name

--- SprintInterface.py
+++ SprintInterface.py
@@ -435,6 +435,7 @@ def forward(segmentName, features):
   assert features.shape == (InputDim, T)
 
   # Init sprintDataset with one single entry: the current segment.
+  segmentName = segmentName or "dummy-segment-name"  # Works because we anyway have this single seq only.
   sprintDataset.initFromSegmentOrder([segmentName], {segmentName: {"nframes": T}})
   sprintDataset.initialize()
   sprintDataset.epoch = -1  # Force reinit in init_seq_order().
