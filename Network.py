47ae0ee4aef0511797fd32803097e3a5cb66d2e5
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-06-25 09:49:28 +0200

    fix L1 regularization

--- Network.py
+++ Network.py
@@ -169,10 +169,10 @@ class LayerNetwork(object):
     assert layer.name
     self.hidden[layer.name] = layer
     if isinstance(layer, RecurrentLayer):
-      if layer.attrs['L1'] > 0.0: self.L1 += layer.attrs['L1'] * abs(layer.W_re.sum())
+      if layer.attrs['L1'] > 0.0: self.L1 += layer.attrs['L1'] * abs(layer.W_re).sum()
       if layer.attrs['L2'] > 0.0: self.L2 += layer.attrs['L2'] * (layer.W_re ** 2).sum()
     for W in layer.W_in:
-      if layer.attrs['L1'] > 0.0: self.L1 += layer.attrs['L1'] * abs(W.sum())
+      if layer.attrs['L1'] > 0.0: self.L1 += layer.attrs['L1'] * abs(W).sum()
       if layer.attrs['L2'] > 0.0: self.L2 += layer.attrs['L2'] * (W ** 2).sum()
 
   def make_classifier(self, **kwargs):
@@ -187,7 +187,7 @@ class LayerNetwork(object):
       layer_class = FramewiseOutputLayer
     self.output = layer_class(index=self.i, n_out=self.n_out, name="output", **kwargs)
     for W in self.output.W_in:
-      if self.output.attrs['L1'] > 0.0: self.L1 += self.output.attrs['L1'] * abs(W.sum())
+      if self.output.attrs['L1'] > 0.0: self.L1 += self.output.attrs['L1'] * abs(W).sum()
       if self.output.attrs['L2'] > 0.0: self.L2 += self.output.attrs['L2'] * (W ** 2).sum()
     self.declare_train_params()
     targets = self.c if self.loss == 'ctc' else self.y
