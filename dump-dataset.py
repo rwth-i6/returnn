bdcc3aa84d231906565c7fd1756eaa41fb0d9a8f
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-07-10 17:11:39 +0200

    dump-dataset: dump postfix. gz by default

--- dump-dataset.py
+++ dump-dataset.py
@@ -15,15 +15,15 @@ def dump_dataset(dataset, options):
   print >> log.v3, "Epoch: %i" % options.epoch
   rnn.train_data.init_seq_order(options.epoch)
 
-  print >> log.v3, "Dump prefix: %r" % options.dump_prefix
+  print >> log.v3, "Dump files: %r*%r" % (options.dump_prefix, options.dump_postfix)
 
   seq_idx = options.startseq
   while dataset.is_less_than_num_seqs(seq_idx) and seq_idx <= options.endseq:
     dataset.load_seqs(seq_idx, seq_idx)
     data = dataset.get_data(seq_idx)
     targets = dataset.get_targets(seq_idx)
-    numpy.savetxt(options.dump_prefix + "%i.data.txt" % seq_idx, data)
-    numpy.savetxt(options.dump_prefix + "%i.targets.txt" % seq_idx, targets, fmt='%i')
+    numpy.savetxt("%s%i.data%s" % (options.dump_prefix, seq_idx, options.dump_postfix), data)
+    numpy.savetxt("%s%i.targets%s" % (options.dump_prefix, seq_idx, options.dump_postfix), targets, fmt='%i')
     seq_idx += 1
 
   print >> log.v3, "Done. More seqs which we did not dumped: %s" % dataset.is_less_than_num_seqs(seq_idx)
@@ -50,6 +50,7 @@ def main(argv):
   argparser.add_argument('--startseq', type=int, default=0, help='start seq idx (inclusive) (default: 0)')
   argparser.add_argument('--endseq', type=int, default=float('inf'), help='end seq idx (inclusive) (default: inf)')
   argparser.add_argument('--dump_prefix', default='/tmp/crnn.dump-dataset.')
+  argparser.add_argument('--dump_postfix', default='.txt.gz')
   args = argparser.parse_args(argv[1:])
   init(configFilename=args.crnn_config_file, commandLineOptions=[])
   dump_dataset(rnn.train_data, args)
