b1024d35e80c15774dd83d55cbded9277281c382
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-07-17 11:09:28 +0200

    reformat TestHDFDataset (cherry picked from commit a21d04dd932f6572150ad268040e1874aa37312c)

--- tests/TestHDFDataset.py
+++ tests/TestHDFDataset.py
@@ -7,46 +7,45 @@ import os
 
 
 class TestHDFDataset(object):
-    @classmethod
-    def setup_class(cls):
-        """
-        :return:
-         This method is run once before starting testing
-        """
+  @classmethod
+  def setup_class(cls):
+    """
+    :return:
+     This method is run once before starting testing
+    """
 
-    @classmethod
-    def teardown_class(cls):
-        """
-        This method is run once after completing all tests
-        :return:
-        """
+  @classmethod
+  def teardown_class(cls):
+    """
+    This method is run once after completing all tests
+    :return:
+    """
 
-    def setup(self):
-        """
-        This method is run before each test is going to be started
-        :return:
-        """
+  def setup(self):
+    """
+    This method is run before each test is going to be started
+    :return:
+    """
 
+  def teardown(self):
+    """
+    This method is run after finishing of each test
+    :return:
+    """
 
-    def teardown(self):
-        """
-        This method is run after finishing of each test
-        :return:
-        """
+  def test_init(self):
+    """
+    This method tests initialization of the HDFDataset class
+    """
+    toy_dataset = HDFDataset()
+    assert_equal(toy_dataset.file_start, [0], "self.file_start init problem, should be [0]")
+    assert_equal(toy_dataset.files, [], "self.files init problem, should be []")
+    assert_equal(toy_dataset.file_seq_start, [], "self.file_seq_start init problem, should be []")
+    return toy_dataset
 
-    def test_init(self):
-        """
-	    This method tests initialization of the HDFDataset class
-	    """
-        toy_dataset = HDFDataset()
-        assert_equal(toy_dataset.file_start, [0], "self.file_start init problem, should be [0]")
-        assert_equal(toy_dataset.files, [], "self.files init problem, should be []")
-        assert_equal(toy_dataset.file_seq_start, [], "self.file_seq_start init problem, should be []")
-        return toy_dataset
-
-    def test_addfile(self):
-        """
-	    This method tests self.addfile function
-	    """
-        toy_dataset = self.test_init()
-        toy_dataset.add_file("/u/kulikov/develop/crnn/tests/toy_set.hdf")
+  def test_addfile(self):
+    """
+    This method tests self.addfile function
+    """
+    toy_dataset = self.test_init()
+    toy_dataset.add_file("/u/kulikov/develop/crnn/tests/toy_set.hdf")
