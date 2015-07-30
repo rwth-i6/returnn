e9b59c3e4f6eab5b1ca5025e3acbcbccb0783847
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-07-30 18:36:23 +0200

    more generic Network layer topology printout

--- Engine.py
+++ Engine.py
@@ -235,11 +235,10 @@ class Engine:
     network = self.network
     print >> log.v2, "Network layer topology:"
     print >> log.v2, "  input #:", network.n_in
-    if network.description:
-      for info in network.description.hidden_info:
-        print >> log.v2, "  " + info["layer_class"] + " #:", info["n_out"]
-    else:
-      print >> log.v2, "  ..."
+    for layer_name, layer in sorted(network.hidden.items()):
+      print >> log.v2, "  %s %s #: %i" % (layer.layer_class, layer_name, layer.attrs["n_out"])
+    if not network.hidden:
+      print >> log.v2, "  (no hidden layers)"
     print >> log.v2, "  output #:", network.n_out
     print >> log.v2, "net params #:", network.num_params()
     print >> log.v2, "net trainable params:", network.train_params_vars
