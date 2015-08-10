b6ace27aba5746665d02e6c5b21ecf4562685583
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-08-10 10:49:14 +0200

    more tests

--- tests/test_Util.py
+++ tests/test_Util.py
@@ -13,5 +13,15 @@ def test_cmd_false():
   assert_raises(CalledProcessError, lambda: cmd("false"))
 
 
+def test_cmd_stdout():
+  r = cmd("echo 1; echo 2;")
+  assert_equal(r, ["1", "2"])
+
+
+def test_cmd_stderr():
+  r = cmd("echo x >/dev/stderr")
+  assert_equal(r, [], "cmd() output should only cover stdout")
+
+
 def test_uniq():
   assert (uniq(np.array([0, 1, 1, 1, 2, 2])) == np.array([0, 1, 2])).all()
