2f29957a4bb38f2e39a9f21cd4b0a017f7ec2d09
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-08-11 17:39:31 +0200

    more collect-orth-symbols options

--- collect-orth-symbols.py
+++ collect-orth-symbols.py
@@ -5,9 +5,17 @@ import sys
 import rnn
 from Log import log
 import argparse
-from Util import hms, parse_orthography
+from Util import hms, parse_orthography, parse_orthography_into_symbols
 import gzip
 import xml.etree.ElementTree as etree
+
+
+def found_sub_seq(sub_seq, seq):
+  # Very inefficient naive implementation:
+  for i in range(len(seq)):
+    if seq[i:i+len(sub_seq)] == sub_seq:
+      return True
+  return False
 
 
 def iter_dataset(dataset, callback):
@@ -51,39 +59,61 @@ def iter_bliss(filename, frame_time, callback):
 
     callback(frame_len=frame_len, orth=orth)
 
+
 def collect_stats(options, iter_corpus):
   """
   :param options: argparse.Namespace
   """
   orth_symbols_filename = options.output
-  assert not os.path.exists(orth_symbols_filename)
-  orth_syms_file = open(orth_symbols_filename, "wb")
+  if orth_symbols_filename:
+    assert not os.path.exists(orth_symbols_filename)
 
   class Stats:
     total_frame_len = 0
     total_orth_len = 0
     orth_syms_set = set()
 
+  if options.add_numbers:
+    Stats.orth_syms_set.update(map(chr, range(ord("0"), ord("9") + 1)))
+  if options.add_lower_alphabet:
+    Stats.orth_syms_set.update(map(chr, range(ord("a"), ord("z") + 1)))
+  if options.add_upper_alphabet:
+    Stats.orth_syms_set.update(map(chr, range(ord("A"), ord("Z") + 1)))
+
   def cb(frame_len, orth):
     Stats.total_frame_len += frame_len
 
     orth_syms = parse_orthography(orth)
+    if options.dump_orth_syms:
+      print >> log.v3, "Orth:", "".join(orth_syms)
+    if options.filter_orth_sym:
+      if options.filter_orth_sym in orth_syms:
+        print >> log.v3, "Found orth:", "".join(orth_syms)
+    if options.filter_orth_syms_seq:
+      filter_seq = parse_orthography_into_symbols(options.filter_orth_syms_seq)
+      if found_sub_seq(filter_seq, orth_syms):
+        print >> log.v3, "Found orth:", "".join(orth_syms)
     Stats.orth_syms_set.update(orth_syms)
     Stats.total_orth_len += len(orth_syms)
 
   iter_corpus(cb)
 
+  if options.remove_symbols:
+    filter_syms = parse_orthography_into_symbols(options.remove_symbols)
+    Stats.orth_syms_set -= set(filter_syms)
+
   print >> log.v3, "Total frame len:", Stats.total_frame_len, "time:", hms(Stats.total_frame_len * (options.frame_time / 1000.0))
   print >> log.v3, "Total orth len:", Stats.total_orth_len, "fraction:", float(Stats.total_orth_len) / Stats.total_frame_len
   print >> log.v3, "Num symbols:", len(Stats.orth_syms_set)
 
-  sys.stdout.flush()
-  for orth_sym in sorted(Stats.orth_syms_set):
-    sys.stdout.write(orth_sym + "\n")
-    orth_syms_file.write("%s\n" % unicode(orth_sym).encode("utf8"))
-  sys.stdout.flush()
-  orth_syms_file.close()
-  print >> log.v3, "Wrote orthography symbols to", orth_symbols_filename
+  if orth_symbols_filename:
+    orth_syms_file = open(orth_symbols_filename, "wb")
+    for orth_sym in sorted(Stats.orth_syms_set):
+      orth_syms_file.write("%s\n" % unicode(orth_sym).encode("utf8"))
+    orth_syms_file.close()
+    print >> log.v3, "Wrote orthography symbols to", orth_symbols_filename
+  else:
+    print >> log.v3, "Provide --output to save the symbols."
 
 
 def init(configFilename=None):
@@ -116,7 +146,14 @@ def main(argv):
   argparser = argparse.ArgumentParser(description='Collect orth symbols.')
   argparser.add_argument('input', help="either crnn config or Bliss xml")
   argparser.add_argument('--frame_time', type=int, default=10, help='time (in ms) per frame')
-  argparser.add_argument('--output', default='orth_symbols')
+  argparser.add_argument('--dump_orth_syms', action='store_true')
+  argparser.add_argument('--filter_orth_sym')
+  argparser.add_argument('--filter_orth_syms_seq')
+  argparser.add_argument('--add_numbers', type=bool, default=True)
+  argparser.add_argument('--add_lower_alphabet', type=bool, default=True)
+  argparser.add_argument('--add_upper_alphabet', type=bool, default=True)
+  argparser.add_argument('--remove_symbols', default="(){}$")
+  argparser.add_argument('--output', help='where to store the symbols (default: dont store)')
   args = argparser.parse_args(argv[1:])
 
   if is_bliss(args.input):
