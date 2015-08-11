8f97d2c7a0203f0e7850fe7b2b99c43ed08837f3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-08-11 17:11:04 +0200

    read corpus bliss directly

--- collect-orth-symbols.py
+++ collect-orth-symbols.py
@@ -6,73 +6,135 @@ import rnn
 from Log import log
 import argparse
 from Util import hms, parse_orthography
+import gzip
+import xml.etree.ElementTree as etree
 
 
-def iter_dataset(dataset, options):
+def iter_dataset(dataset, callback):
   """
   :type dataset: Dataset.Dataset
-  :param options: argparse.Namespace
   """
-  rnn.train_data.init_seq_order(epoch=1)
-
+  dataset.init_seq_order(epoch=1)
   assert "orth" in dataset.get_target_list()
-
-  orth_symbols_filename = options.output
-  assert not os.path.exists(orth_symbols_filename)
-  orth_syms_file = open(orth_symbols_filename, "w")
-
-  total_frame_len = 0
-  total_orth_len = 0
-  orth_syms_set = set()
 
   seq_idx = 0
   while dataset.is_less_than_num_seqs(seq_idx):
     dataset.load_seqs(seq_idx, seq_idx)
-    total_frame_len += dataset.get_seq_length(seq_idx)[0]
 
+    frame_len = dataset.get_seq_length(seq_idx)[0]
     orth = dataset.get_targets("orth", seq_idx)
-    orth_syms = parse_orthography(orth)
-    orth_syms_set.update(orth_syms)
-    total_orth_len += len(orth_syms)
+    callback(frame_len=frame_len, orth=orth)
 
     seq_idx += 1
 
-  print >> log.v3, "Total frame len:", total_frame_len, "time:", hms(total_frame_len * (options.frame_time / 1000.0))
-  print >> log.v3, "Total orth len:", total_orth_len, "fraction:", float(total_orth_len) / total_frame_len
-  print >> log.v3, "Num symbols:", len(orth_syms_set)
+
+def iter_bliss(filename, frame_time, callback):
+  corpus_file = gzip.GzipFile(fileobj=open(filename, 'rb'))
+  def getelements(tag):
+    """Yield *tag* elements from *filename_or_file* xml incrementaly."""
+    context = iter(etree.iterparse(corpus_file, events=('start', 'end')))
+    _, root = next(context) # get root element
+    for event, elem in context:
+      if event == 'end' and elem.tag == tag:
+        yield elem
+        root.clear() # free memory
+
+  for elem in getelements("segment"):
+    start = float(elem.attrib['start'])
+    end = float(elem.attrib['end'])
+    assert end > start
+    frame_len = (end - start) * (1000.0 / frame_time)
+    elem_orth = elem.find("orth")
+    orth_raw = elem_orth.text  # should be unicode
+    orth_split = orth_raw.split()
+    orth = " ".join(orth_split)
+
+    callback(frame_len=frame_len, orth=orth)
+
+def collect_stats(options, iter_corpus):
+  """
+  :param options: argparse.Namespace
+  """
+  orth_symbols_filename = options.output
+  assert not os.path.exists(orth_symbols_filename)
+  orth_syms_file = open(orth_symbols_filename, "wb")
+
+  class Stats:
+    total_frame_len = 0
+    total_orth_len = 0
+    orth_syms_set = set()
+
+  def cb(frame_len, orth):
+    Stats.total_frame_len += frame_len
+
+    orth_syms = parse_orthography(orth)
+    Stats.orth_syms_set.update(orth_syms)
+    Stats.total_orth_len += len(orth_syms)
+
+  iter_corpus(cb)
+
+  print >> log.v3, "Total frame len:", Stats.total_frame_len, "time:", hms(Stats.total_frame_len * (options.frame_time / 1000.0))
+  print >> log.v3, "Total orth len:", Stats.total_orth_len, "fraction:", float(Stats.total_orth_len) / Stats.total_frame_len
+  print >> log.v3, "Num symbols:", len(Stats.orth_syms_set)
 
   sys.stdout.flush()
-  for orth_sym in sorted(orth_syms_set):
+  for orth_sym in sorted(Stats.orth_syms_set):
     sys.stdout.write(orth_sym + "\n")
-    orth_syms_file.write(orth_sym + "\n")
+    orth_syms_file.write("%s\n" % unicode(orth_sym).encode("utf8"))
   sys.stdout.flush()
   orth_syms_file.close()
   print >> log.v3, "Wrote orthography symbols to", orth_symbols_filename
 
 
-def init(configFilename, commandLineOptions):
+def init(configFilename=None):
   rnn.initBetterExchook()
   rnn.initThreadJoinHack()
-  rnn.initConfig(configFilename, commandLineOptions)
-  global config
-  config = rnn.config
-  rnn.initLog()
+  if configFilename:
+    rnn.initConfig(configFilename, commandLineOptions=[])
+    rnn.initLog()
+  else:
+    log.initialize()
   print >> log.v3, "CRNN collect-orth-symbols starting up."
   rnn.initFaulthandler()
-  rnn.initConfigJson()
-  rnn.initData()
-  rnn.printTaskProperties()
+  if configFilename:
+    rnn.initConfigJson()
+    rnn.initData()
+    rnn.printTaskProperties()
+
+
+def is_bliss(filename):
+  try:
+    corpus_file = gzip.GzipFile(fileobj=open(filename, 'rb'))
+    context = iter(etree.iterparse(corpus_file, events=('start', 'end')))
+    _, root = next(context)  # get root element
+  except IOError:  # 'Not a gzipped file'
+    return False
+  return True
 
 
 def main(argv):
   argparser = argparse.ArgumentParser(description='Collect orth symbols.')
-  argparser.add_argument('crnn_config_file')
+  argparser.add_argument('input', help="either crnn config or Bliss xml")
   argparser.add_argument('--frame_time', type=int, default=10, help='time (in ms) per frame')
   argparser.add_argument('--output', default='orth_symbols')
   args = argparser.parse_args(argv[1:])
-  init(configFilename=args.crnn_config_file, commandLineOptions=[])
-  iter_dataset(rnn.train_data, args)
-  rnn.finalize()
+
+  if is_bliss(args.input):
+    bliss_filename = args.input
+    crnn_config_filename = None
+  else:
+    bliss_filename = None
+    crnn_config_filename = args.input
+  init(configFilename=crnn_config_filename)
+
+  if bliss_filename:
+    iter_corpus = lambda cb: iter_bliss(bliss_filename, frame_time=args.frame_time, callback=cb)
+  else:
+    iter_corpus = lambda cb: iter_dataset(rnn.train_data, callback=cb)
+  collect_stats(args, iter_corpus)
+
+  if crnn_config_filename:
+    rnn.finalize()
 
 
 if __name__ == '__main__':
