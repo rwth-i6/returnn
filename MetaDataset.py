1d31ef1f8fc4ee605aaf0e1d0694b11d6c2cbf39
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-09-10 14:55:48 +0200

    ConcatDataset, and move generic CachedDataset2 code

--- MetaDataset.py
+++ MetaDataset.py
@@ -1,9 +1,10 @@
 
 from Dataset import Dataset, DatasetSeq, init_dataset
+from CachedDataset2 import CachedDataset2
 from Util import NumbersDict, load_json
 
 
-class MetaDataset(Dataset):
+class MetaDataset(CachedDataset2):
 
   def __init__(self,
                seq_list_file, seq_lens_file,
@@ -61,20 +62,11 @@ class MetaDataset(Dataset):
     # Will only init the needed datasets.
     self.datasets = {key: init_dataset(datasets[key]) for key in self.dataset_keys}
 
-    self.epoch = None
-    self.init_seq_order()
-
   def init_seq_order(self, epoch=None, seq_list=None):
-    if not epoch:
-      epoch = 1
-    self.expected_load_seq_start = 0
-    self.reached_final_seq = False
-    self.added_data = []; " :type: list[DatasetSeq] "
-    self._num_timesteps_accumulated = 0
-
-    if self.epoch == epoch:
+    need_reinit = self.epoch == epoch
+    super(MetaDataset, self).init_seq_order(epoch=epoch, seq_list=seq_list)
+    if not need_reinit:
       return
-    self.epoch = epoch
 
     if seq_list:
       seq_index = [self.tag_idx[tag] for tag in seq_list]
@@ -89,51 +81,12 @@ class MetaDataset(Dataset):
     for dataset in self.datasets.values():
       dataset.init_seq_order(epoch=epoch, seq_list=self.seq_list_ordered)
 
-  def _cleanup_old_seqs(self, seq_idx_end):
-    i = 0
-    while i < len(self.added_data):
-      if self.added_data[i].seq_idx >= seq_idx_end:
-        break
-      i += 1
-    del self.added_data[:i]
-
-  def _get_seq(self, seq_idx):
-    for data in self.added_data:
-      if data.seq_idx == seq_idx:
-        return data
-    return None
-
-  def is_cached(self, start, end):
-    # Always False, to force that we call self._load_seqs().
-    # This is important for our buffer management.
-    return False
-
   def _load_seqs(self, start, end):
-    """
-    :param int start: inclusive seq idx start
-    :param int end: exclusive seq idx end
-    """
-    # We expect that start increase monotonic on each call
-    # for not-yet-loaded data.
-    # This will already be called with _load_seqs_superset indices.
-    assert start >= self.expected_load_seq_start
-    if start > self.expected_load_seq_start:
-      # Cleanup old data.
-      self._cleanup_old_seqs(start)
-      self.expected_load_seq_start = start
-    if self.added_data:
-      start = max(self.added_data[-1].seq_idx + 1, start)
-    if end > self.num_seqs:
-      end = self.num_seqs
-    if end >= self.num_seqs:
-      self.reached_final_seq = True
     for dataset in self.datasets.values():
       dataset.load_seqs(start, end)
       for seq_idx in range(start, end):
         self._check_dataset_seq(dataset, seq_idx)
-    seqs = [self._collect_single_seq(seq_idx=seq_idx) for seq_idx in range(start, end)]
-    self._num_timesteps_accumulated += sum([seq.num_frames for seq in seqs])
-    self.added_data += seqs
+    super(MetaDataset, self)._load_seqs(start=start, end=end)
 
   def _check_dataset_seq(self, dataset, seq_idx):
     """
@@ -164,25 +117,13 @@ class MetaDataset(Dataset):
     targets = {target: self._get_data(seq_idx, target) for target in self.target_list}
     return DatasetSeq(seq_idx=seq_idx, seq_tag=seq_tag, features=features, targets=targets)
 
-  @property
-  def num_seqs(self):
-    return self._num_seqs
-
-  def get_num_timesteps(self):
-    if self._num_timesteps:
-      return self._num_timesteps
-    else:
-      assert self.reached_final_seq
-      return self._num_timesteps_accumulated
-
   def get_seq_length(self, sorted_seq_idx):
     if self._seq_lens:
       return self._seq_lens[self.seq_list_ordered[sorted_seq_idx]]
-    # get_seq_length() can be called before the seq is loaded via load_seqs().
-    # Thus, we just call load_seqs() ourselves here.
-    assert sorted_seq_idx >= self.expected_load_seq_start
-    self.load_seqs(self.expected_load_seq_start, sorted_seq_idx + 1)
-    return self._get_seq(sorted_seq_idx).num_frames
+    return super(MetaDataset, self).get_seq_length(sorted_seq_idx)
+
+  def get_tag(self, sorted_seq_idx):
+    return self.seq_list_ordered[sorted_seq_idx]
 
   def get_target_list(self):
     return self.target_list
@@ -193,25 +134,95 @@ class MetaDataset(Dataset):
     :return: 1 for hard labels, num_outputs[target] for soft labels
     """
     if self.data_1_of_k[key]:
-      return 1
+      d = 1
     else:
-      return self.data_dims[key]
+      d = self.data_dims[key]
+    if self.added_data:
+      assert super(MetaDataset, self).get_data_dim(key) == d
+    return d
 
   def get_data_dtype(self, key):
-    return self.data_dtypes[key]
+    dtype = self.data_dtypes[key]
+    if self.added_data:
+      assert super(MetaDataset, self).get_data_dtype(key) == dtype
+    return dtype
 
-  def get_tag(self, sorted_seq_idx):
-    return self.seq_list_ordered[sorted_seq_idx]
 
-  def get_input_data(self, sorted_seq_idx):
-    return self._get_seq(sorted_seq_idx).features
+class ConcatDataset(CachedDataset2):
+  def __init__(self, datasets, **kwargs):
+    """
+    :param list[dict[str]] datasets: list of kwargs for init_dataset
+    """
+    super(ConcatDataset, self).__init__(**kwargs)
+    self.datasets = [init_dataset(d_kwargs) for d_kwargs in datasets]
 
-  def get_targets(self, target, sorted_seq_idx):
-    return self._get_seq(sorted_seq_idx).targets[target]
+  def init_seq_order(self, epoch=None, seq_list=None):
+    """
+    :type epoch: int|None
+    :param list[str] | None seq_list: In case we want to set a predefined order.
+    """
+    need_reinit = self.epoch == epoch
+    super(ConcatDataset, self).init_seq_order(epoch=epoch, seq_list=seq_list)
+    self.dataset_seq_idx_offsets = [0]
+    if not need_reinit:
+      return
 
-  def get_ctc_targets(self, sorted_seq_idx):
-    assert self._get_seq(sorted_seq_idx).ctc_targets
+    if seq_list:  # reference order
+      seq_lists = []
+      for dataset in self.datasets:
+        # This depends on the num_seqs of our childs.
+        seq_lists += seq_list[:dataset.num_seqs]
+        seq_list = seq_list[dataset.num_seqs:]
+      assert len(seq_list) == 0  # we have consumed all
+    else:
+      seq_lists = [None] * len(self.datasets)
+      if self.seq_ordering == "sorted":
+        # Not sure about this case. Maybe a separate implementation makes more sense.
+        raise NotImplementedError
 
+    assert len(seq_lists) == len(self.datasets)
+    for dataset, sub_list in zip(self.datasets, seq_lists):
+      dataset.init_seq_order(epoch=epoch, seq_list=sub_list)
+
+  def _get_dataset_for_seq_idx(self, seq_idx):
+    i = 0
+    while i < len(self.dataset_seq_idx_offsets):
+      if seq_idx + self.dataset_seq_idx_offsets[i] < 0:
+        return i - 1
+      i += 1
+    return i - 1
+
+  def _load_seqs(self, start, end):
+    sub_start = start
+    while True:
+      dataset_idx = self._get_dataset_for_seq_idx(sub_start)
+      dataset = self.datasets[dataset_idx]
+      dataset_seq_idx_start = sub_start + self.dataset_seq_idx_offsets[dataset_idx]
+      dataset_seq_idx_end = end + self.dataset_seq_idx_offsets[dataset_idx]
+      dataset.load_seqs(dataset_seq_idx_start, dataset_seq_idx_end)
+      if dataset.is_less_than_num_seqs(dataset_seq_idx_end):
+        self.dataset_seq_idx_offsets[dataset_idx + 1:dataset_idx + 2] = [
+          self.dataset_seq_idx_offsets[dataset_idx] - dataset.num_seqs]
+        sub_start = -self.dataset_seq_idx_offsets[dataset_idx + 1]
+      else:
+        break
+    super(ConcatDataset, self)._load_seqs(start=start, end=end)
+
+  def _collect_single_seq(self, seq_idx):
+    dataset_idx = self._get_dataset_for_seq_idx(seq_idx)
+    dataset = self.datasets[dataset_idx]
+    dataset_seq_idx = seq_idx + self.dataset_seq_idx_offsets[dataset_idx]
+    seq_tag = dataset.get_tag(dataset_seq_idx)
+    features = dataset.get_input_data(dataset_seq_idx)
+    targets = {k: dataset.get_targets(k, dataset_seq_idx) for k in dataset.get_target_list()}
+    return DatasetSeq(seq_idx=seq_idx, seq_tag=seq_tag, features=features, targets=targets)
+
+  @property
+  def num_seqs(self):
+    return sum([ds.num_seqs for ds in self.datasets])
+
+  def get_target_list(self):
+    return self.datasets[0].get_target_list()
 
 
 def _simple_to_bool(v):
