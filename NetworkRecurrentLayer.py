5b25c54eb883471c646609b6e7edcdde254b534e
doetsch <patrick.doetsch@gmail.com>
2015-10-14 02:21:57 +0200

    small lm fix

--- NetworkRecurrentLayer.py
+++ NetworkRecurrentLayer.py
@@ -579,11 +579,10 @@ class RecurrentUnitLayer(Layer):
         y = self.y_in[self.attrs['target']].flatten() #.reshape(self.index.shape)
         y_t = self.W_lm_out[y].reshape((index.shape[0]*index.shape[1],unit.n_in))
         h_e = T.exp(T.dot(outputs[0][::direction or 1], self.W_lm_in))
-        h_z = h_e / T.sum(h_e,axis=2,keepdims=True)
-        h_t = T.dot(h_z, self.W_lm_out).reshape((index.shape[0]*index.shape[1],unit.n_in))
+        h_z = h_e / T.sum(h_e,axis=2,keepdims=True).reshape((index.shape[0]*index.shape[1],h_e.shape[1]))
         #h_t = self.W_lm_out[T.argmax(T.dot(outputs[0][::direction or 1], self.W_lm_in), axis=2)].reshape((index.shape[0],index.shape[1],unit.n_in))
         #self.constraints += T.sum(T.sum(T.sqr(y_t[j] - h_t[j]),axis=1))
-        self.constraints += -T.sum(T.log(h_t[j,y[j]]))
+        self.constraints += -T.sum(T.log(h_z[j,y[j]]))
 
         #h_y = (self.y_in[self.attrs['target']].reshape(index.shape)).flatten()
         #h_e = T.dot(outputs[0][::direction or 1], self.W_lm_in)
