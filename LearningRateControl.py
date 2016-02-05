dfab6af15ecfdf9d9329f5c62b14b2fdb658aa3e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-02-05 22:47:59 +0100

    small cleanup

--- LearningRateControl.py
+++ LearningRateControl.py
@@ -55,7 +55,7 @@ class LearningRateControl(object):
     if initialLearningRates:
       if isinstance(initialLearningRates, list):
         initialLearningRates = {i + 1: v for (i, v) in enumerate(initialLearningRates)}
-      if isinstance(initialLearningRates, str) or isinstance(initialLearningRates, unicode):
+      if isinstance(initialLearningRates, (str, unicode)):
         initialLearningRates = eval(initialLearningRates)
       assert isinstance(initialLearningRates, dict)
       for epoch, v in initialLearningRates.items():
