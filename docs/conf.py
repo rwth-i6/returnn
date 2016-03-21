410b0e90712bb7bb7bf9a941673d5121411a8d6e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-03-21 18:44:04 +0100

    doc, small fix

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,10 @@
 
 import sys
 import os
+sys.path += [".", "crnn"]
 
 import generateapi
 generateapi.generate()
-sys.path += ["crnn"]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
