4e1d2efb062cff2cca28c4717a8bb5c02664d2d1
Patrick Doetsch <patrick.doetsch@i6.informatik.rwth-aachen.de>
2016-04-18 17:15:18 +0200

    shaping

--- NetworkHiddenLayer.py
+++ NetworkHiddenLayer.py
@@ -1382,13 +1382,13 @@ class AttentionLengthLayer(_NoOpLayer):
     l = 6. / (filter[0]*filter[1])
     values = numpy.asarray(self.rng.uniform(low=-l, high=l, size=(n_features, 1, filter[0], filter[1])), dtype=theano.config.floatX)
     F = self.add_param(self.shared(value=values, name="F"))
-    conv = T.nnet.conv2d(border_mode='full',
-                        input=attention.dimshuffle(1,'x',2,0), # B1TN
-                        filters=F).dimshuffle(3,0,2,1)[filter[1]/2:-filter[1]/2+1,:,filter[0]/2:-filter[0]/2+1] # NBTF
-    halting = T.exp(T.max(conv,axis=2)) # NBF
+    halting = T.nnet.conv2d(border_mode='full',
+                           input=attention.dimshuffle(1,'x',2,0), # B1TN
+                           filters=F).dimshuffle(3,0,2,1)[filter[1]/2:-filter[1]/2+1,:,filter[0]/2:-filter[0]/2+1] # NBTF
     if n_features > 1:
       W_f = self.add_param(self.create_forward_weights(n_features,1,'W_f'))
       halting = T.dot(halting, W_f)
+    halting = T.exp(T.max(halting,axis=2).reshape(self.index.shape)) # NB
     halting = halting.reshape(attention.shape)
     halting = halting / T.sum(halting,axis=0,keepdims=True)
     hyp = T.sum(halting * T.arange(halting.shape[0],dtype='float32').dimshuffle(0,'x').repeat(halting.shape[1],axis=1),axis=0)
