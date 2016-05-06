d30922ca35e9cfab93d45366cc13464cc3c34fc6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-05-06 14:22:40 +0200

    best path, more wip

--- c_support_code/best-path.cpp
+++ c_support_code/best-path.cpp
@@ -44,6 +44,101 @@ void find_best_n_kernel(float* in, long in_idx_stride, long out_idx_stride, long
 	}
 }
 
+DEF_KERNEL
+void subtensor_idxs_kernel(float* in, long in_idx_stride, long out_idx_stride, long max_idx, long n, float* in_idxs, float* out) {
+	long idx = threadIdx.x + blockDim.x * blockIdx.x;
+	while(idx < max_idx) {
+		long in_start = idx * in_idx_stride;
+		long out_start = idx * out_idx_stride;
+		for(long i = 0; i < n; ++i) {
+			long in_idx = (long) in_idxs[out_start + i]; // in_idxs like out
+			float in_v = in[out_start + in_idx];
+			out[out_start + i] = in_v;
+		}
+		idx += gridDim.x * blockDim.x;
+	}
+}
+
+DEF_KERNEL
+void subtensor_idxs_incr_kernel(float* in, long in_idx_stride, long out_idx_stride, long max_idx, long n, float* in_idxs, float* out) {
+	long idx = threadIdx.x + blockDim.x * blockIdx.x;
+	while(idx < max_idx) {
+		long in_start = idx * in_idx_stride;
+		long out_start = idx * out_idx_stride;
+		for(long i = 0; i < n; ++i) {
+			long in_idx = (long) in_idxs[out_start + i]; // in_idxs like out
+			in[out_start + in_idx] += out[out_start + i];
+		}
+		idx += gridDim.x * blockDim.x;
+	}
+}
+
+void get_n_max_and_argmax(Ndarray* v, int n, Ndarray* nbest, Ndarray* nbestidx) {
+	assert(Ndarray_NDIM(v) == 3);
+	long T = Ndarray_DIMS(v)[0];
+	long n_batch = Ndarray_DIMS(v)[1];
+	long n_dim = Ndarray_DIMS(v)[2];
+	assert(Ndarray_NDIM(nbest) == 3);
+	assert(Ndarray_NDIM(nbestidx) == 3);
+	assert(Ndarray_DIMS(nbest)[0] == T);
+	assert(Ndarray_DIMS(nbest)[1] == n_batch);
+	assert(Ndarray_DIMS(nbest)[2] == n);
+	assert(Ndarray_DIMS(nbestidx)[0] == T);
+	assert(Ndarray_DIMS(nbestidx)[1] == n_batch);
+	assert(Ndarray_DIMS(nbestidx)[2] == n);
+
+	start_dev_kernel(find_best_n_kernel, (
+		/*in*/ Ndarray_DEV_DATA(v),
+		/*in_idx_stride*/ n_dim,
+		/*out_idx_stride*/ n,
+		/*max_idx*/ T * n_batch,
+		/*n*/ n,
+		/*out*/ Ndarray_DEV_DATA(nbestidx)
+	));
+	
+	start_dev_kernel(subtensor_idxs_kernel, (
+		/*in*/ Ndarray_DEV_DATA(v),
+		/*in_idx_stride*/ n_dim,
+		/*out_idx_stride*/ n,
+		/*max_idx*/ T * n_batch,
+		/*n*/ n,
+		/*in_idxs*/ Ndarray_DEV_DATA(nbestidx),
+		/*out*/ Ndarray_DEV_DATA(nbest)
+	));
+}
+
+void grad_n_max_and_argmax(Ndarray* out, int n, Ndarray* nbestidx, Ndarray* errsignal) {
+	assert(Ndarray_NDIM(out) == 3);
+	long T = Ndarray_DIMS(out)[0];
+	long n_batch = Ndarray_DIMS(out)[1];
+	long n_dim = Ndarray_DIMS(out)[2];
+	assert(Ndarray_NDIM(errsignal) == 3);
+	assert(Ndarray_NDIM(nbestidx) == 3);
+	assert(Ndarray_DIMS(errsignal)[0] == T);
+	assert(Ndarray_DIMS(errsignal)[1] == n_batch);
+	assert(Ndarray_DIMS(errsignal)[2] == n);
+	assert(Ndarray_DIMS(nbestidx)[0] == T);
+	assert(Ndarray_DIMS(nbestidx)[1] == n_batch);
+	assert(Ndarray_DIMS(nbestidx)[2] == n);
+
+	start_dev_kernel(subtensor_idxs_incr_kernel, (
+		/*in*/ Ndarray_DEV_DATA(out),
+		/*in_idx_stride*/ n_dim,
+		/*out_idx_stride*/ n,
+		/*max_idx*/ T * n_batch,
+		/*n*/ n,
+		/*in_idxs*/ Ndarray_DEV_DATA(nbestidx),
+		/*out*/ Ndarray_DEV_DATA(errsignal)
+	));
+}
+
+DEF_KERNEL
+void find_best_path_kernel(float* in, long in_idx_stride, long out_idx_stride, long T, float* out) {
+	for(long t = 0; t < T; ++t) {
+		
+	}
+}
+
 void make_best_path(Ndarray* posteriors, int local_n_best_num) {
 	assert(Ndarray_NDIM(posteriors) == 3);
 	long T = Ndarray_DIMS(posteriors)[0];
@@ -60,6 +155,9 @@ void make_best_path(Ndarray* posteriors, int local_n_best_num) {
 		/*n*/ local_n_best_num,
 		/*out*/ Ndarray_DEV_DATA(local_n_best)
 	));
+
+	// note: we don't do a real search. we just take the local max in each time-frame.
+	
 
 	// now: which are the possible allophone states regarding these n-best labels?
 	// what are possible sequences?
