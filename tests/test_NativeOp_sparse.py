57ed82c928e9b2e5b13a8a2c5bd35f3d5cfc71f6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-05-19 14:00:15 +0200

    test_sparse_to_dense better (with mask)

--- tests/test_NativeOp_sparse.py
+++ tests/test_NativeOp_sparse.py
@@ -16,14 +16,14 @@ def test_sparse_to_dense():
   n_time = 3
   n_batch = 2
   n_dim = 5
-  s0 = np.array([[0,0], [0,1], [1,1], [1,2], [1,2], [2,2]], dtype=f32)
-  s1 = np.array([[1,2], [2,3], [1,1], [2,0], [4,1], [3,3]], dtype=f32)
-  w =  np.array([[1,2], [2,1], [1,2], [3,4], [5,6], [7,8]], dtype=f32)
-  m =  np.array([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1]], dtype=f32)
+  s0 = np.array([[0,0], [0,1], [1,1], [1,2], [1,2], [2,2], [2,2]], dtype=f32)
+  s1 = np.array([[1,2], [2,3], [1,1], [2,0], [4,1], [3,3], [4,4]], dtype=f32)
+  w =  np.array([[1,2], [2,1], [1,2], [3,4], [5,6], [7,8], [9,9]], dtype=f32)
+  m =  np.array([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,0]], dtype=f32)
   W = np.array(
     [[[0, 1, 2, 0, 0], [0, 0, 2, 0, 0]],
      [[0, 1, 3, 0, 5], [0, 2, 0, 1, 0]],
-     [[0, 0, 0, 7, 0], [4, 6, 0, 8, 0]]], dtype=f32)
+     [[0, 0, 0, 7, 9], [4, 6, 0, 8, 0]]], dtype=f32)
   assert W.shape == (n_time, n_batch, n_dim)
   W2 = NativeOp.sparse_to_dense(s0, s1, w, m, n_time, n_dim).eval()
   assert_almost_equal(W, W2)
