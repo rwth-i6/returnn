d7453e0bbc46da54f65cbb5c3e6c15ecbebb026f
Patrick Doetsch <patrick.doetsch@i6.informatik.rwth-aachen.de>
2016-10-26 06:02:28 +0200

    end-to-end inverse segmentation (wip)

--- OpInvAlign.py
+++ OpInvAlign.py
@@ -100,4 +100,60 @@ class InvAlignOp(theano.Op):
       result[start + span] = int((hmm[s]/self.numStates + 1))
     return result
 
+
+  def _fullSegmentationInv(self, start, end, scores, transcription):
+    """Fully aligns sequence from start to end but in inverse manner"""
+    inf = 1e30
+    # max skip transitions derived from tdps
+    skip = len(self.tdps)
+
+    hmm = self._buildHmm(transcription)
+    lengthT = end - start
+    lengthS = transcription.shape[0] * self.numStates * self.repetitions
+
+    # with margins of skip at the bottom or top
+    fwdScore = np.full((lengthS, lengthT + skip - 1), inf)
+    bt = np.full((lengthS, lengthT + skip - 1), -1, dtype=np.int32)
+
+    # precompute all scores and densities
+    score = np.full((lengthS, lengthT + skip - 1), inf)
+    for t in range(0, lengthT):
+      for s in range(0, lengthS):
+        score[s][t + skip - 1] = scores[start + t, hmm[s] / self.numStates]
+
+      # forward
+      # initialize first column
+      # assume virtual start at t = -1
+      scores = score[0, 0 + skip - 1:skip + skip - 2]
+      #if allFeatures:
+      #  scores = np.cumsum(scores)
+      scores = np.add(scores, self.tdps[1:])
+      fwdScore[0, 0 + skip - 1:skip + skip - 2] = scores
+      bt[0, 0 + skip - 1:skip + skip - 2] = range(1, skip)
+
+      # remaining columns
+      for s in range(1, lengthS):
+        for t in range(0, lengthT):
+          previous = fwdScore[s - 1, t:t + skip]
+          scores = score[s, t + skip - 1]
+          scores = np.add(scores, np.add(previous, self.tdps[::-1]))
+
+          best = np.argmin(scores)
+          fwdScore[s, t + skip - 1] = scores[best]
+          bt[s, t + skip - 1] = skip - 1 - best
+
+      alignment = np.full((lengthT), -2, dtype=np.int32)
+      # backtrack
+      t = lengthT - 1
+      alignment[t] = hmm[lengthS - 1]
+      for s in range(lengthS - 2, -1, -1):
+        tnew = t - bt[s + 1][t + skip - 1]
+        alignment[tnew:t] = hmm[s]
+        t = tnew
+
+      alignment[0:t] = 0
+
+      assert not -2 in alignment
+      return alignment
+
 invAlignOp = InvAlignOp([ 1e10, 0., 1.9, 3., 2.5, 2., 1.4 ])
