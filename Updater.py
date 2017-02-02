53378694dd9e6924a71803e44b66595ca341dfb2
doetsch <patrick.doetsch@gmail.com>
2017-02-02 02:58:15 +0100

    layer specific weight clipping

--- Updater.py
+++ Updater.py
@@ -422,7 +422,7 @@ class Updater:
         grads[param] = grads[param].transfer(self.device)
       gradient_scale = param.layer.gradient_scale
       if isinstance(param.layer.gradient_scale, list):
-        gradient_scale = T.switch(T.eq(T.mod(self.network.epoch - 1,gradient_scale[1]),0), gradient_scale[0], numpy.float32(0))
+        gradient_scale = T.switch(T.eq(T.mod(i_t - 1,gradient_scale[1]),0), gradient_scale[0], numpy.float32(0))
       deltas = grads[param] * T.cast(gradient_scale,'float32')
       if self.max_norm > 0:
         deltas = self.norm_constraint(deltas, self.max_norm)
@@ -910,10 +910,15 @@ class Updater:
     #if upd:
       #updates.append((param, self.norm_constraint(param + upd, 1.0)))
       #updates.append((param, param + upd))
-    if self.weight_clip > 0.0:
-      updates.extend([(p, T.clip(p + upd[p],-self.weight_clip,self.weight_clip)) for p in upd if upd[p]])
-    else:
-      updates.extend([(p, p + upd[p]) for p in upd if upd[p]])
+    for p in upd:
+      if upd[p]:
+        if p.layer.attrs.get('weight_clip', 0.0) > 0.0:
+          clip = numpy.float32(p.layer.attrs['weight_clip'])
+          updates.append((p, T.clip(p + upd[p], -clip, clip)))
+        elif self.weight_clip > 0.0:
+          updates.append((p, T.clip(p + upd[p], -self.weight_clip, self.weight_clip)))
+        else:
+          updates.append((p, p + upd[p]))
     updates.append((self.i, i_t))
     if self.adasecant:
       dt = 1 #T.cast(T.max(T.sum(self.network.output.values()[0].index,axis=0)), 'float32')
