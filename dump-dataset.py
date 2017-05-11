46aafa8d8b07ba586b929f6b35da2a27204600c3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2017-05-11 11:51:07 +0200

    dump dataset, new defaults, Python 3

--- dump-dataset.py
+++ dump-dataset.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys
 import rnn
 from Log import log
@@ -13,34 +15,36 @@ def dump_dataset(dataset, options):
   :type dataset: Dataset.Dataset
   :param options: argparse.Namespace
   """
-  print >> log.v3, "Epoch: %i" % options.epoch
+  print("Epoch: %i" % options.epoch, file=log.v3)
   rnn.train_data.init_seq_order(options.epoch)
 
   if options.type == "numpy":
-    print >> log.v3, "Dump files: %r*%r" % (options.dump_prefix, options.dump_postfix)
+    print("Dump files: %r*%r" % (options.dump_prefix, options.dump_postfix), file=log.v3)
   elif options.type == "stdout":
-    print >> log.v3, "Dump to stdout"
+    print("Dump to stdout", file=log.v3)
   else:
     raise Exception("unknown dump option type %r" % options.type)
 
   seq_idx = options.startseq
+  if options.endseq < 0:
+    options.endseq = float("inf")
   while dataset.is_less_than_num_seqs(seq_idx) and seq_idx <= options.endseq:
     dataset.load_seqs(seq_idx, seq_idx + 1)
     data = dataset.get_data(seq_idx, "data")
     if options.type == "numpy":
       numpy.savetxt("%s%i.data%s" % (options.dump_prefix, seq_idx, options.dump_postfix), data)
     elif options.type == "stdout":
-      print "seq %i data:" % seq_idx, pretty_print(data)
+      print("seq %i data:" % seq_idx, pretty_print(data))
     for target in dataset.get_target_list():
       targets = dataset.get_targets(target, seq_idx)
       if options.type == "numpy":
         numpy.savetxt("%s%i.targets.%s%s" % (options.dump_prefix, seq_idx, target, options.dump_postfix), targets, fmt='%i')
       elif options.type == "stdout":
-        print "seq %i target %r:" % (seq_idx, target), pretty_print(targets)
+        print("seq %i target %r:" % (seq_idx, target), pretty_print(targets))
 
     seq_idx += 1
 
-  print >> log.v3, "Done. More seqs which we did not dumped: %s" % dataset.is_less_than_num_seqs(seq_idx)
+  print("Done. More seqs which we did not dumped: %s" % dataset.is_less_than_num_seqs(seq_idx), file=log.v1)
 
 
 def init(configFilename, commandLineOptions):
@@ -50,7 +54,7 @@ def init(configFilename, commandLineOptions):
   global config
   config = rnn.config
   rnn.initLog()
-  print >> log.v3, "CRNN dump-dataset starting up."
+  print("CRNN dump-dataset starting up.", file=log.v1)
   rnn.initFaulthandler()
   rnn.initConfigJsonNetwork()
   rnn.initData()
@@ -62,8 +66,8 @@ def main(argv):
   argparser.add_argument('crnn_config_file')
   argparser.add_argument('--epoch', type=int, default=1)
   argparser.add_argument('--startseq', type=int, default=0, help='start seq idx (inclusive) (default: 0)')
-  argparser.add_argument('--endseq', type=int, default=float('inf'), help='end seq idx (inclusive) (default: inf)')
-  argparser.add_argument('--type', default='numpy', help="'numpy' or 'stdout'")
+  argparser.add_argument('--endseq', type=int, default=10, help='end seq idx (inclusive) or -1 (default: 10)')
+  argparser.add_argument('--type', default='stdout', help="'numpy' or 'stdout'")
   argparser.add_argument('--dump_prefix', default='/tmp/crnn.dump-dataset.')
   argparser.add_argument('--dump_postfix', default='.txt.gz')
   args = argparser.parse_args(argv[1:])
