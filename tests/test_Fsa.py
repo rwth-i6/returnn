ae044e9da0a060f37c71eb435bb836bef9e887e8
christophmluscher <chris.luscher@gmx.de>
2017-06-18 23:38:54 +0200

    basic setup for fsa test

--- tests/test_Fsa.py
+++ tests/test_Fsa.py
@@ -3,16 +3,31 @@
 from __future__ import print_function
 from __future__ import division
 
-from Fsa import Fsa
+try:
+  from Fsa import Fsa
+except ImportError:
+  import os
+  import sys
+  sys.path.append(os.path.relpath("./../"))
+  from Fsa import Fsa
 
 
-def main():
+def test_01():
   pass
 
-if __name__ == "__main__":
+def main():
   import time
-  start_time = time.time()
 
+  lex = './../recog.150k.final.lex.gz'
+
+  automaton = Fsa(lemma='Halloween is a fantastic event', fsa_type='ctc')
+
+  lexicon_time = time.time()
+
+  automaton.set_lexicon(lexicon_name=lex)
+
+  print(time.time() - lexicon_time, "seconds to load the lexicon")
+
+
+if __name__ == "__main__":
   main()
-
-  print(time.time() - start_time, "seconds")
