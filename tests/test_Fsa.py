6117d7dacf30b21f9a849a9eaa2a2b7427e11e2a
christophmluscher <chris.luscher@gmx.de>
2017-06-18 17:01:32 +0200

    add file for fsa tests

---
+++ tests/test_Fsa.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+from __future__ import division
+
+from Fsa import Fsa
+
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  import time
+  start_time = time.time()
+
+  main()
+
+  print(time.time() - start_time, "seconds")
