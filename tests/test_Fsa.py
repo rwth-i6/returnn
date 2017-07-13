ac78543eb5cb5c3248c1e8820a8123b8e9dc7a14
christophmluscher <chris.luscher@gmx.de>
2017-07-13 14:46:58 +0200

    test

--- tests/test_Fsa.py
+++ tests/test_Fsa.py
@@ -18,26 +18,42 @@ def main():
 
   lex = './../recog.150k.final.lex.gz'
   st_ty = './../state-tying.txt'
+  lem = 'test_Fsa_lemma.txt'
+  fsatype = ['asg', 'ctc', 'hmm']
 
+  # load file
+  lemmas = open(lem, "r")
+
+  # init FSA class
   automaton = Fsa()
 
-  automaton.set_lemma(lemma='Halloween is a fantastic event')
-
-  automaton.set_fsa_type(fsa_type='hmm')
-
+  # load lexicon (longest single op) and state tying file
   lexicon_start_time = time.time()
 
   automaton.set_lexicon(lexicon_name=lex)
 
   automaton.set_state_tying(st_ty)
 
+  automaton.set_hmm_depth(5)
+
   lexicon_end_time = time.time()
 
   run_start_time = time.time()
 
-  automaton.run()
+  for lemma in lemmas:
+    automaton.set_lemma(lemma=lemma)
+
+    for ft in fsatype:
+      automaton.set_fsa_type(fsa_type=ft)
+
+      automaton.run()
+
+      filename = lemma.lower().strip().replace(" ", "-") + '-' + automaton.fsa_type
+      fsa_to_dot_format(file=filename, num_states=automaton.num_states, edges=automaton.edges)
 
   run_end_time = time.time()
+
+  lemmas.close()
 
   print("Run time:", run_end_time - run_start_time, "seconds")
 
