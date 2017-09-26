4f84c8cd8530cf126778cd2f900d81aaeb8ce14f
Paul Voigtlaender <voigtlaender@vision.rwth-aachen.de>
2017-09-26 16:26:32 +0200

    added simple best path decoder

---
+++ demos/mdlstm/IAM/decode.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#do a simple best path decoding without language model or lexicon
+
+import h5py
+import numpy
+
+with open("chars.txt") as f:
+  chars = [l.strip() for l in f.readlines()]
+
+with h5py.File("mdlstm_real_valid.h5", "r") as f:
+  x = f["inputs"][...]
+  x = numpy.argmax(x, axis=1)
+  x = [chars[idx] for idx in x]
+  lens = f["seqLengths"][...]
+  tags = f["seqTags"][...]
+  start = 0
+  for tag, len_ in zip(tags, lens):
+    y = []
+    last_char = None
+    for c in x[start:start+len_]:
+      if last_char != c:
+        y.append(c)
+        last_char = c
+    y = [" " if c == "|" else c for c in y]
+    output = "".join(y).strip()
+    print tag, output
+    start += len_
