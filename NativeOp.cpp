df09d49847b6d15f900f25baa6dd759a51cd5e5c
Eugen Beck <beck@i6.informatik.rwth-aachen.de>
2017-11-23 16:49:58 +0100

    Another fix

--- NativeOp.cpp
+++ NativeOp.cpp
@@ -436,10 +436,10 @@ struct _KernelLoop {
 
 Ndarray* Ndarray_uninitialized_like(Ndarray* a) {
 	Ndarray_DIMS_Type dim = Ndarray_HOST_DIMS(a);
-#ifdef TENSORFLOW
+#if TENSORFLOW
 	Ndarray* res = (Ndarray*) Ndarray_NewDims(Ndarray_NDIM(a), dim);
 #else
-	Ndarray* res = (Ndarray*) Ndarray_NewDims(Ndarray_NDIM(a), const_cast<decltype(Ndarray_DIM_Type)>(dim));
+	Ndarray* res = (Ndarray*) Ndarray_NewDims(Ndarray_NDIM(a), const_cast<Ndarray_DIM_Type*>(dim));
 #endif
 	return res;
 }
