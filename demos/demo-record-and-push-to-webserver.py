7e0ff32ae464eebf67e80e98e1b93fcd2d3e934d
Albert Zeyer <albzey@gmail.com>
2018-03-27 16:10:20 +0200

    demo record and push, noise profile (commented out)

--- demos/demo-record-and-push-to-webserver.py
+++ demos/demo-record-and-push-to-webserver.py
@@ -12,13 +12,17 @@ argparser.add_argument("--ssh_host")
 argparser.add_argument("--http_host", default="http://localhost:12380")
 args = argparser.parse_args()
 
+#print("Noise profile")
+#check_call(["sox", "-n", "-d", "trim", "0", "1.5", "noiseprof", "/tmp/speech.noise-profile"])
+
 print("Using Sox for recording. Speak something, then pause 3 sec.")
 fn = "/tmp/rec.wav"
 if os.path.exists(fn):
   os.remove(fn)
 check_call([
   "sox", "-d", "-c", "1", "-r", "16k", "-b", "16", fn,
-  "silence", "1", "0.1", "0.1%", "1", "2.0", "0.1%",
+#  "noisered", "/tmp/speech.noise-profile", "0.3",
+  "silence", "1", "0.1", "0.1%", "1", "2.0", "1%",
   "norm"],
   stdin=sys.stdin)
 assert os.path.exists(fn)
