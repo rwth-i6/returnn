0c9a7f4fe12396e73e2cfd5dbae2d0bacc504c50
Albert Zeyer <albzey@gmail.com>
2018-07-18 09:48:55 +1000

    SelfAttentionLayer with simpler state
    Not using tf.TensorArray, but just concatenating instead.
    This might hopefully solve the memory problems.

--- TFNetworkRecLayer.py
+++ TFNetworkRecLayer.py
@@ -3491,20 +3491,12 @@ class SelfAttentionLayer(_ConcatInputLayer):
       # Memory for kv.
       kv_cur = tf.concat([k, v], axis=-1)  # (batch,heads,1,kv-dim//heads)
       kv_cur.set_shape((None, num_heads, 1, (total_key_dim + total_value_dim) // num_heads))
-      kv_left_ta = self._rec_previous_layer.rec_vars_outputs["kv_left_ta"]
-      assert isinstance(kv_left_ta, tf.TensorArray)
-      time = self.network.get_rec_step_index()
-      kv_left_ta = kv_left_ta.write(time, kv_cur)
-      self.rec_vars_outputs["kv_left_ta"] = kv_left_ta
-      kv = kv_left_ta.stack()  # (time',batch,heads,1,kv-dim//heads)
-      kv.set_shape((None, None, num_heads, 1, (total_key_dim + total_value_dim) // num_heads))
-      kv = kv[:time + 1]  # it could happen that another thread already has appended another vector in the meantime
-      # (time,batch,heads,kv-dim//heads)
-      kv = tf.reshape(
-        kv, (time + 1, batch_dim, num_heads, (total_key_dim + total_value_dim) // num_heads),
-        name="reshape_check")
-      kv = tf.transpose(kv, (1, 2, 0, 3))  # (batch,heads,time,kv-dim//heads)
-      k, v = tf.split(kv, [total_key_dim // num_heads, total_value_dim // num_heads], axis=-1)
+      # (batch,heads,time,kv-dim//heads)
+      kv_left = self._rec_previous_layer.rec_vars_outputs["kv_left"]
+      kv_left = tf.concat([kv_left, kv_cur], axis=2)
+      kv_left.set_shape((None, num_heads, None, (total_key_dim + total_value_dim) // num_heads))
+      self.rec_vars_outputs["kv_left"] = kv_left
+      k, v = tf.split(kv_left, [total_key_dim // num_heads, total_value_dim // num_heads], axis=-1)
     # Dot-attention. Resulting last time dimension will be used to perform the softmax over, and will the be reduced.
     energy = tf.matmul(q, k, transpose_b=True)  # (batch,heads,time,time)
     if self.input_data.time_dim_axis is not None:
@@ -3572,13 +3564,22 @@ class SelfAttentionLayer(_ConcatInputLayer):
     data = data.copy_as_batch_major()
     if data.time_dim_axis is None:
       # Assume inside RecLayer.
+      # Before, we used a tf.TensorArray.
+      # However, that has higher memory consumptions than just using a tensor and concatenating to it.
       total_value_dim = n_out
-      kv_left_ta = tf.TensorArray(
-        name="kv_left_ta",
-        dtype=tf.float32, size=0, dynamic_size=True, clear_after_read=False,
-        infer_shape=True,
-        element_shape=(None, num_heads, 1, (total_key_dim + total_value_dim) // num_heads))
-      return {"kv_left_ta": kv_left_ta}
+      # (batch,heads,time,kv-dim//heads)
+      kv_left = tf.zeros((batch_dim, num_heads, 0, (total_key_dim + total_value_dim) // num_heads))
+      return {"kv_left": kv_left}
+    return {}
+
+  @classmethod
+  def get_rec_initial_extra_outputs_shape_invariants(cls, num_heads, total_key_dim, n_out, sources, **kwargs):
+    data = get_concat_sources_data_template(sources)
+    data = data.copy_as_batch_major()
+    if data.time_dim_axis is None:
+      # Assume inside RecLayer. See get_rec_initial_extra_outputs.
+      total_value_dim = n_out
+      return {"kv_left": tf.TensorShape((None, num_heads, None, (total_key_dim + total_value_dim) // num_heads))}
     return {}
 
 
