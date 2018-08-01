1057ed4806ebbdc4042bae509f9f41269f7c77bc
menne <menne@cs.rwth-aachen.de>
2018-08-01 15:12:13 +0200

    fixes test_TFNetworkSigProcLayer removed tfSi6Proc dependent tests

--- tests/test_TFNetworkSigProcLayer.py
+++ tests/test_TFNetworkSigProcLayer.py
@@ -34,26 +34,6 @@ def make_scope():
       yield session
 
 
-def test_melFilterbankLayer():
-  with make_scope() as session:
-    n_in, n_out = 257, 3
-    layer_name = "mel_filterbank_layer"
-    config = Config()
-    config.update({
-      "num_outputs": n_out,
-      "num_inputs": n_in,
-      "network": {
-        layer_name: {
-          "class": "mel_filterbank", "fft_size": 512, "nr_of_filters": n_out, "n_out": n_out, "is_output_layer": True}
-      }})
-    network = TFNetwork(config=config, train_flag=True)
-    network.construct_from_dict(config.typed_value("network"))
-    layer = network.layers[layer_name]
-    test_out = session.run(
-      layer.output.placeholder, feed_dict={network.get_extern_data('data').placeholder: np.ones((1, 1, 257))})
-    assert np.sum(test_out - np.asarray([28.27923584, 53.10634232, 99.71585846], dtype=np.float32)) < 1e-5
-
-
 def test_complexLinearProjectionLayer():
   with make_scope() as session:
     n_in, n_out = 514, 128
