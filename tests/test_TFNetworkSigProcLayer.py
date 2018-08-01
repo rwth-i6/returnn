9da00d2d16198c8a71d035d4d00c5ea3acaf057d
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2018-08-01 15:07:19 +0200

    test_TFNetworkSigProcLayer fixes

--- tests/test_TFNetworkSigProcLayer.py
+++ tests/test_TFNetworkSigProcLayer.py
@@ -1,20 +1,39 @@
 
+from __future__ import print_function
+
+# Disable extensive TF debug verbosity. Must come before the first TF import.
+import logging
+logging.getLogger('tensorflow').disabled = True
+
+import sys
 import os
 import os.path
 import tensorflow as tf
 import numpy as np
+import unittest
+import contextlib
+
+# Allow Returnn imports.
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/..")
+
 from TFNetwork import *
 from TFNetworkSigProcLayer import *
 from Config import Config
-import contextlib
+
+import better_exchook
+better_exchook.replace_traceback_format_tb()
+
 
 @contextlib.contextmanager
 def make_scope():
+  """
+  :rtype: tf.Session
+  """
   with tf.Graph().as_default() as graph:
     with tf.Session(graph=graph) as session:
       yield session
 
- 
+
 def test_melFilterbankLayer():
   with make_scope() as session:
     n_in, n_out = 257, 3
@@ -30,12 +49,14 @@ def test_melFilterbankLayer():
     network = TFNetwork(config=config, train_flag=True)
     network.construct_from_dict(config.typed_value("network"))
     layer = network.layers[layer_name]
-    test_out = session.run(layer.output.placeholder, {network.get_extern_data('data').placeholder: np.ones((1, 1, 257))})
+    test_out = session.run(
+      layer.output.placeholder, feed_dict={network.get_extern_data('data').placeholder: np.ones((1, 1, 257))})
     assert np.sum(test_out - np.asarray([28.27923584, 53.10634232, 99.71585846], dtype=np.float32)) < 1e-5
+
 
 def test_complexLinearProjectionLayer():
   with make_scope() as session:
-    n_in, n_out = 514, 128 
+    n_in, n_out = 514, 128
     layer_name = "clp_layer"
     config = Config()
     config.update({
@@ -48,13 +69,36 @@ def test_complexLinearProjectionLayer():
     network = TFNetwork(config=config, train_flag=True)
     network.construct_from_dict(config.typed_value("network"))
     layer = network.layers[layer_name]
+    assert isinstance(layer, ComplexLinearProjectionLayer)
     i_r = np.ones((1, n_in // 2))
     i_i = np.ones((1, n_in // 2)) * 0.5
-    test_input = np.expand_dims(np.reshape(np.transpose(np.reshape(np.concatenate([i_r, i_i], axis=1), (1, 2, 257)), [0, 2, 1]), (1, 514)), 0)
+    test_input = np.expand_dims(np.reshape(np.transpose(
+      np.reshape(np.concatenate([i_r, i_i], axis=1), (1, 2, 257)), [0, 2, 1]), (1, 514)), 0)
     test_clp_kernel = np.ones((2, n_in // 2, 128))
-    test_clp_output = session.run(layer.output.placeholder, {network.get_extern_data('data').placeholder: test_input, layer._clp_kernel: test_clp_kernel})
+    test_clp_output = session.run(
+      layer.output.placeholder,
+      feed_dict={network.get_extern_data('data').placeholder: test_input, layer._clp_kernel: test_clp_kernel})
     assert test_clp_output[0, 0, 0] - 6.00722122 < 1e-5
 
+
 if __name__ == "__main__":
-    test_melFilterbankLayer()
-    test_complexLinearProjectionLayer()
+  better_exchook.install()
+  if len(sys.argv) <= 1:
+    for k, v in sorted(globals().items()):
+      if k.startswith("test_"):
+        print("-" * 40)
+        print("Executing: %s" % k)
+        try:
+          v()
+        except unittest.SkipTest as exc:
+          print("SkipTest:", exc)
+        print("-" * 40)
+    print("Finished all tests.")
+  else:
+    assert len(sys.argv) >= 2
+    for arg in sys.argv[1:]:
+      print("Executing: %s" % arg)
+      if arg in globals():
+        globals()[arg]()  # assume function and execute
+      else:
+        eval(arg)  # assume Python code and execute
