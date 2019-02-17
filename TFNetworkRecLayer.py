096f5dcda93c870bac9a50538f2604b0ae444fce
Albert Zeyer <albzey@gmail.com>
2019-02-17 14:21:36 +0100

    RecLayer subnet out layer, collect other seq lens

--- TFNetworkRecLayer.py
+++ TFNetworkRecLayer.py
@@ -2119,7 +2119,7 @@ class _SubnetworkRecCell(object):
       return
 
     max_len = tf.reduce_max(seq_len) if seq_len is not None else None
-    from TFUtil import tensor_array_stack
+    from TFUtil import tensor_array_stack, has_control_flow_context
     from TFNetwork import TFNetwork, ExternData
     from TFNetworkLayer import InternalLayer
     self.output_layers_net = TFNetwork(
@@ -2148,11 +2148,16 @@ class _SubnetworkRecCell(object):
       if name in loop_acc_layers:
         return loop_acc_layers[name]
       with tf.name_scope(self.layer_data_templates[name].layer_class_type.cls_get_tf_scope_name(name)):
+        inner_layer = self.net.layers[name]
         output = self.layer_data_templates[name].output.copy_template_adding_time_dim(time_dim_axis=0)
         # We should have accumulated it.
         output.placeholder = tensor_array_stack(
           loop_accumulated["output_%s" % name], stop=max_len)  # e.g. (time,batch,dim)
         output.size_placeholder = {0: seq_len}
+        if inner_layer.output.size_placeholder:
+          for i, size in inner_layer.output.size_placeholder.items():
+            if not has_control_flow_context(size):  # copy if this size comes from outside the loop
+              output.size_placeholder[i + 1] = size
         assert isinstance(self.output_layers_net, TFNetwork)
         layer = self.output_layers_net.add_layer(name=name, output=output, layer_class=InternalLayer)
         loop_acc_layers[name] = layer
