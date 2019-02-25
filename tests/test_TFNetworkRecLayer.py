bebd005490670db593acbd6c49639cb915ffecc1
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2019-02-25 11:25:53 +0100

    test_GenericAttentionLayer_weights_auto_squeeze_time_end

--- tests/test_TFNetworkRecLayer.py
+++ tests/test_TFNetworkRecLayer.py
@@ -2534,6 +2534,26 @@ def test_GenericAttentionLayer_basic():
   assert layer.output.shape == (1, 2048) and not layer.output.have_time_axis()
 
 
+def test_GenericAttentionLayer_weights_auto_squeeze_time_end():
+  # Example: weights (B,1,T), base (B,T,V)
+  from TFNetworkLayer import InternalLayer
+  net = TFNetwork(extern_data=ExternData(), config=Config({"debug_print_layer_output_template": True}))
+  kwargs = dict(
+    name="att", network=net,
+    weights=InternalLayer(
+      name="att_weights", network=net,
+      output=Data(
+        name='att_weights_output', shape=(1, None), dim=None, time_dim_axis=2, auto_create_placeholders=True)),
+    base=InternalLayer(
+      name="enc_value", network=net,
+      output=Data(name='enc_value_output', shape=(None, 2048), auto_create_placeholders=True)))
+  print("GenericAttentionLayer kwargs:")
+  pprint(kwargs)
+  kwargs["output"] = GenericAttentionLayer.get_out_data_from_opts(**kwargs)
+  layer = GenericAttentionLayer(**kwargs)
+  assert layer.output.shape == (2048,) and not layer.output.have_time_axis()
+
+
 if __name__ == "__main__":
   try:
     better_exchook.install()
