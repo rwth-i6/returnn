41b60e36486ba8108469913c337ade5be349edee
Albert Zeyer <albzey@gmail.com>
2019-03-01 15:11:20 +0100

    Data sanity_check more verbose on assert

--- TFUtil.py
+++ TFUtil.py
@@ -380,7 +380,8 @@ class Data(object):
     if self.sparse:
       assert self.feature_dim_axis is None, "%s: If sparse, there cannot be a feature dim axis." % self
     if self.feature_dim_axis is not None:
-      assert self.dim == self.batch_shape[self.feature_dim_axis], "%s: inconsistent dim" % self
+      assert self.dim == self.batch_shape[self.feature_dim_axis], (
+        "%s: inconsistent dim. feature axis or unspecified: %r." % (self, self.feature_dim_axis_or_unspecified))
     if not ignore_placeholder and self.placeholder is not None:
       self.placeholder.set_shape(self.batch_shape)
 
