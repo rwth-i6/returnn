3adfc2d92728bbee91ac73e8af2701a99253e340
Jian4yang2Tang1 <jtang@apptek.com>
2019-03-15 17:18:03 +0100

    Activate direction option for native lstm operator

--- PTNativeModules.py
+++ PTNativeModules.py
@@ -9,7 +9,6 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.cuda
 from torch.utils.cpp_extension import load_inline
-import torch.backends.cudnn
 
 torch.manual_seed(1234)
 torch.cuda.manual_seed_all(1234)
@@ -17,7 +16,7 @@ torch.cuda.manual_seed(1234)
 random.seed(1234)
 os.environ['PYTHONHASHSEED'] = str(1234)
 numpy.random.seed(1234)
-torch.backends.cudnn.enabled = True
+# torch.backends.cudnn.enabled = True
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
@@ -36,14 +35,15 @@ class LstmOp(autograd.Function):
   
   @staticmethod
   def forward(ctx, *inputs):
-    ctx.device = inputs[-1]
+    ctx.device = inputs[-2]
+    ctx.direction = inputs[-1]
     T, B, D = inputs[0].shape[0], inputs[0].shape[1], inputs[0].shape[2] // 4
     Y = torch.zeros((T, B, D), device=ctx.device)
     C = torch.zeros((T, B, D), device=ctx.device)
     H = torch.zeros((T, B, 4 * D), device=ctx.device)
     d = torch.zeros((B, D), device=ctx.device)
     y_prev = torch.zeros((B, D), device=ctx.device)
-    lstm.lstm_forward_op(*inputs[:5], 0, 1, Y, C, H, d, y_prev)
+    lstm.lstm_forward_op(*inputs[:5], 0, ctx.direction, Y, C, H, d, y_prev)
     ctx.save_for_backward(*inputs[:5], Y, C, H)
     return Y, C, d
   
@@ -56,30 +56,34 @@ class LstmOp(autograd.Function):
     dx0 = torch.zeros((DX.shape[1], DX.shape[2]), device=ctx.device)
     lstm.lstm_backward_op(
       *ctx.saved_tensors[:5],
-      0, 1,
+      0, ctx.direction,
       *ctx.saved_tensors[5:],
       grad_outputs[0].contiguous(), grad_outputs[2].contiguous(),
       DX, DW, Dy0, Dc0, dx0
     )
-    return DX, DW, Dy0, Dc0, None, None
+    return DX, DW, Dy0, Dc0, None, None, None
 
 
 class SingleLayerLstm(nn.Module):
   
-  def __init__(self, n_input, n_hidden):
+  def __init__(self, n_input, n_hidden, direction=1):
     super(SingleLayerLstm, self).__init__()
     self.input_size = n_input
     self.hidden_size = n_hidden
     self.gate_size = self.hidden_size * 4
-    stdv = 1.0 / math.sqrt(self.hidden_size)  # the same initializer as PyTorch's RNN implementation
+    self.direction = direction
+    # stdv = 1.0 / math.sqrt(self.hidden_size)  # the same initializer as PyTorch's RNN implementation
     shape_wf = (self.input_size, self.gate_size)
     shape_wr = (self.hidden_size, self.gate_size)
-    self.Wf = nn.Parameter(torch.empty(shape_wf).uniform_(-stdv, stdv))
-    self.Wr = nn.Parameter(torch.empty(shape_wr).uniform_(-stdv, stdv))
-    self.bf = nn.Parameter(torch.empty(self.gate_size).uniform_(-2 * stdv, 2 * stdv))
+    stdv_wf = math.sqrt(6.0 / (self.input_size + self.gate_size))
+    stdv_wr = math.sqrt(6.0 / (self.hidden_size + self.gate_size))
+    self.Wf = nn.Parameter(torch.empty(shape_wf).uniform_(-stdv_wf, stdv_wf))
+    self.Wr = nn.Parameter(torch.empty(shape_wr).uniform_(-stdv_wr, stdv_wr))
+    self.bf = nn.Parameter(torch.zeros(self.gate_size))
   
-  def forward(self, X, i = None, h0=None, c0=None):
+  def forward(self, X, i=None, h0=None, c0=None):
     device = self.Wf.device
+    assert device == X.device
     T, B = X.shape[0], X.shape[1]
     if h0 is None:
       h0 = torch.zeros((B, self.hidden_size), requires_grad=False, device=device)
@@ -87,8 +91,9 @@ class SingleLayerLstm(nn.Module):
     if i is None:
       i = torch.ones((T, B), requires_grad=False, device=device)
     i = i.float()
-    intern = torch.einsum("ijk,kl->ijl", (X, self.Wf)) + self.bf
-    Y, C, d = LstmOp.apply(intern, self.Wr, h0, c0, i, device)
+    intern = torch.einsum("ijk,kl->ijl", X, self.Wf).add(self.bf)
+    intern.to(device)
+    Y, C, d = LstmOp.apply(intern, self.Wr, h0, c0, i, device, self.direction)
     return Y, (C, d)
 
 
