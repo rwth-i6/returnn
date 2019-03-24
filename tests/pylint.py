9e625d9ec8cffc3c9f4127cd79bffc29fa2620ba
Albert Zeyer <albzey@gmail.com>
2019-03-24 23:39:52 +0100

    pylint simple example script

---
+++ tests/pylint.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+from glob import glob
+
+my_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.dirname(my_dir)
+sys.path.insert(0, base_dir)
+os.chdir(base_dir)
+
+import better_exchook
+better_exchook.install()
+
+# travis_fold: https://github.com/travis-ci/travis-ci/issues/1065
+print("travis_fold:start:script.install")
+subprocess.check_call(["pip", "install", "pylint"])
+print("travis_fold:end:script.install")
+
+for fn in glob(base_dir + "/*.py"):
+  print("travis_fold:start:pylint.%s" % os.path.basename(fn))
+  res = subprocess.Popen(["pylint", fn]).wait()
+  print("Return code:", res)
+  print("travis_fold:end:pylint.%s" % os.path.basename(fn))
