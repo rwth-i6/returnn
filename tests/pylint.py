962f09092dc7aeaadf92b3cfebf526769628a243
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2019-03-25 10:33:03 +0100

    PyLint, small update
    Although currently we are being hit by this:
    https://github.com/PyCQA/pylint/issues/1452
    
    Not sure how to add a workaround for that.
    But maybe instead wait for an upstream fix.
    Or do not use PyLint but something else.

--- tests/pylint.py
+++ tests/pylint.py
@@ -15,10 +15,10 @@ better_exchook.install()
 
 # travis_fold: https://github.com/travis-ci/travis-ci/issues/1065
 print("travis_fold:start:script.install")
-subprocess.check_call(["pip", "install", "pylint"])
+subprocess.check_call(["pip", "install", "pylint", "better_exchook"])
 print("travis_fold:end:script.install")
 
-for fn in glob(base_dir + "/*.py"):
+for fn in sorted(glob(base_dir + "/*.py")):
   print("travis_fold:start:pylint.%s" % os.path.basename(fn))
   res = subprocess.Popen(["pylint", fn]).wait()
   print("Return code:", res)
