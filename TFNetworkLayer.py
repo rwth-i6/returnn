rwth-i6/returnn

commit a9fcfdbd8dc650932dee80cd35122c8e33db82b6

add all layer targets to used_data_keys in SubnetworkRecCell

TFNetworkLayer.py
@@ -437,6 +437,13 @@ class LayerBase(object):
           # Not resolving this in the dict as target, but call get_layer to make it available.
           if target.startswith("layer:"):
             target_layers[target] = get_layer(target[len("layer:"):])
+          else:
+            # Note: This is a workaround for cases where we need to know about used data keys before the layer
+            # itself is constructed (e.g. in _SubnetworkRecCell.get_output).
+            # A nicer solution would be to not modify this here,
+            # but instead lazily handle it in TFNetwork.get_extern_data,
+            # such that we do not need to know in advance which data keys we need.
+            network.used_data_keys.add(target)
     if "n_out" not in d and targets and network.eval_flag:
       # Must be done here now because loss might be set to None later.
       target = targets[0]  # guess using first target

TFNetworkRecLayer.py
@@ -3614,13 +3614,11 @@ class ChoiceLayer(LayerBase):
     """
     if isinstance(d["target"], str):
       d["target"] = [d["target"]]
-    if not network.search_flag:
-      network.used_data_keys.update(d["target"])
-      if not d.get("scheduled_sampling"):
-        # In the dependency graph, we don't want it.
-        # This can enable some optimizations in the RecLayer.
-        # We do it here because we should know about the deps early in the template creation in RecLayer.
-        d["from"] = []
+    if not network.search_flag and not d.get("scheduled_sampling"):
+      # In the dependency graph, we don't want it.
+      # This can enable some optimizations in the RecLayer.
+      # We do it here because we should know about the deps early in the template creation in RecLayer.
+      d["from"] = []
     if d.get("explicit_search_source"):
       d["explicit_search_source"] = get_layer(d["explicit_search_source"]) if network.search_flag else None
     super(ChoiceLayer, cls).transform_config_dict(d, network=network, get_layer=get_layer)

commit 0438e3b24f267d0bbfc8b17a4c9b1a7ba6a5eaec

Rec subnet used_data_keys, small fix
This fixes a problem introduced in a9fcfdbd8dc.
See also the discussion in #165.

TFNetworkLayer.py
@@ -443,7 +443,9 @@ class LayerBase(object):
             # A nicer solution would be to not modify this here,
             # but instead lazily handle it in TFNetwork.get_extern_data,
             # such that we do not need to know in advance which data keys we need.
-            network.used_data_keys.add(target)
+            # Also, if we are inside a rec layer, and doing search, we also cannot do that.
+            if not network.is_inside_rec_layer() or not network.search_flag:
+              network.used_data_keys.add(target)
     if "n_out" not in d and targets and network.eval_flag:
       # Must be done here now because loss might be set to None later.
       target = targets[0]  # guess using first target