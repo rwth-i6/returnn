777844333698167047dad2c168d40b0bc77dc3a9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2020-08-04 18:29:17 +0200

    small fix for old mod mapping

--- returnn/__init__.py
+++ returnn/__init__.py
@@ -5,6 +5,6 @@ from .__setup__ import get_version_str as _get_version_str
 __version__ = _get_version_str(fallback="1.0.0")
 
 from .__old_mod_loader__ import setup as _old_mod_loader_setup
-_old_mod_loader_setup()
+_old_mod_loader_setup(modules=globals())
 
 __root_dir__ = _os.path.dirname(_os.path.dirname(_os.path.abspath(__file__)))  # can be used as __path__
