5679fd0e72994fd7b7703e5ce98bb150de2073ed
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2020-08-07 14:57:22 +0200

    inspect spelling, packages, ignored files

--- tests/lint_common.py
+++ tests/lint_common.py
@@ -22,16 +22,34 @@ assert os.path.exists("%s/rnn.py" % _root_dir)
 # Proceed like this: Fix all warnings for some file, then remove it from this list.
 # I removed already all files which really should not have warnings (mostly the TF backend + shared files).
 ignore_count_for_files = {
+  'returnn/network_description.py',
+  'returnn/util/fsa.py',
+  'returnn/util/task_system.py',
+  'returnn/util/task_system_example.py',
+  'returnn/datasets/bundle_file.py',
+  'returnn/datasets/cached.py',
+  'returnn/datasets/normalization_data.py',
+  'returnn/datasets/raw_wav.py',
+  'returnn/datasets/stereo.py',
+  'returnn/tf/layers/neural_transducer.py',
+  'returnn/tf/layers/segmental_model.py',
+  'returnn/tf/layers/signal_processing.py',
+
+  # Copied to RETURNN, still work-in-progress to clean up.
+  "returnn/extern/graph_editor/reroute.py",
+  "returnn/extern/graph_editor/subgraph.py",
+  "returnn/extern/graph_editor/transform.py",
+  "returnn/extern/graph_editor/util.py",
+  "returnn/extern/official_tf_resnet/resnet_model.py",
+
+  # Theano code is mostly outdated, thus not so critical.
   'returnn/theano/activation_functions.py',
   'returnn/theano/ops/best_path_decoder.py',
-  'returnn/datasets/bundle_file.py',
   'returnn/theano/ops/ctc.py',
-  'returnn/datasets/cached.py',
   'returnn/theano/ops/custom_lstm_functions.py',
   'returnn/theano/device.py',
   'returnn/theano/engine.py',
   'returnn/theano/engine_task.py',
-  'returnn/util/fsa.py',
   'returnn/theano/ops/function_loader.py',
   'returnn/theano/ops/inv.py',
   'returnn/theano/ops/multi_batch_beam.py',
@@ -40,7 +58,6 @@ ignore_count_for_files = {
   'returnn/theano/layers/cnn.py',
   'returnn/theano/network_copy_utils.py',
   'returnn/theano/layers/ctc.py',
-  'returnn/network_description.py',
   'returnn/theano/layers/hidden.py',
   'returnn/theano/layers/basic.py',
   'returnn/theano/layers/lstm.py',
@@ -48,7 +65,6 @@ ignore_count_for_files = {
   'returnn/theano/layers/rec.py',
   'returnn/theano/network_stream.py',
   'returnn/theano/layers/twod.py',
-  'returnn/datasets/normalization_data.py',
   'returnn/theano/ops/blstm.py',
   'returnn/theano/ops/inv_align.py',
   'returnn/theano/ops/lstm.py',
@@ -56,15 +72,8 @@ ignore_count_for_files = {
   'returnn/theano/ops/lstm_custom.py',
   'returnn/theano/ops/lstm_rec.py',
   'returnn/theano/ops/numpy_align.py',
-  'returnn/datasets/raw_wav.py',
   'returnn/theano/recurrent_transform.py',
   'returnn/theano/server.py',
-  'returnn/datasets/stereo.py',
-  'returnn/tf/layers/neural_transducer.py',
-  'returnn/tf/layers/segmental_model.py',
-  'returnn/tf/layers/signal_processing.py',
-  'returnn/util/task_system.py',
-  'returnn/util/task_system_example.py',
   'returnn/theano/util.py',
   'returnn/theano/ops/torch_wrapper.py',
   'returnn/theano/ops/two_state_best_path_decoder.py',
@@ -94,11 +103,14 @@ def find_all_py_source_files():
     # Ignore tests, or other irrelevant directories.
     if root == "":
       dirs[:] = ["returnn", "demos", "tools"]
-    # Ignore extern git submodules.
-    if root != "":
+    else:
+      dirs[:] = sorted(dirs)
+      # Ignore extern git submodules.
       dirs[:] = [d for d in dirs if not os.path.exists("%s/%s%s/.git" % (_root_dir, root, d))]
-    for file in files:
-      if file.endswith(".py"):
-        src_files.append(root + file)
-  src_files.sort(key=lambda fn: fn.split("/"))
+    for file in sorted(files):
+      if not file.endswith(".py"):
+        continue
+      if file == "_setup_info_generated.py":
+        continue
+      src_files.append(root + file)
   return src_files
