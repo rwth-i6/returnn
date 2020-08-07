e3dd0be9d6c520a49cd0a06e8611dc1ddf73dc64
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2020-08-07 13:03:17 +0200

    fix some warnings

--- demos/demo-record-and-push-to-webserver.py
+++ demos/demo-record-and-push-to-webserver.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
+"""
+Helper script when RETURNN is run in server mode.
+Will record some audio from microphone via ``sox``,
+and then send it to a running RETURNN server instance.
+"""
+
 import sys
 import os
 from argparse import ArgumentParser
 from subprocess import check_call
-import better_exchook
+import better_exchook  # noqa
 
 better_exchook.install()
-argparser = ArgumentParser()
-argparser.add_argument("--input_type", default="audio", help="audio or text. default is audio")
-argparser.add_argument("--input_file", help="existing file")
-argparser.add_argument("--input_text", help="some text")
-argparser.add_argument("--ssh_host")
-argparser.add_argument("--http_host", default="http://localhost:12380")
-args = argparser.parse_args()
+arg_parser = ArgumentParser()
+arg_parser.add_argument("--input_type", default="audio", help="audio or text. default is audio")
+arg_parser.add_argument("--input_file", help="existing file")
+arg_parser.add_argument("--input_text", help="some text")
+arg_parser.add_argument("--ssh_host")
+arg_parser.add_argument("--http_host", default="http://localhost:12380")
+args = arg_parser.parse_args()
 
 if args.input_file:
   assert os.path.exists(args.input_file)
@@ -30,8 +36,8 @@ elif args.input_type == "text" or args.input_text:
     f.write("%s\n" % text)
 
 elif args.input_type == "audio":
-  #print("Noise profile")
-  #check_call(["sox", "-n", "-d", "trim", "0", "1.5", "noiseprof", "/tmp/speech.noise-profile"])
+  # print("Noise profile")
+  # check_call(["sox", "-n", "-d", "trim", "0", "1.5", "noiseprof", "/tmp/speech.noise-profile"])
 
   print("Using Sox for recording. Speak something, then pause 3 sec.")
   fn = "/tmp/rec.wav"
@@ -51,6 +57,9 @@ else:
 
 
 def get_curl_cmd():
+  """
+  :rtype: list[str]
+  """
   return ["curl", "-F", "file=@%s" % fn, args.http_host]
 
 
