609ce204e5feff5994cbfb42ff88ca76b085d147
Albert Zeyer <albzey@gmail.com>
2020-10-23 22:27:04 +0200

    rec layer, set out seq len dim tag

--- returnn/tf/layers/rec.py
+++ returnn/tf/layers/rec.py
@@ -2115,8 +2115,7 @@ class _SubnetworkRecCell(object):
       if self.input_layers_moved_out:
         with tf.name_scope("input_layers_moved_out"):
           self._construct_input_layers_moved_out()
-          if fixed_seq_len is None and "output" in self.input_layers_moved_out:
-            assert rec_layer.output.size_placeholder  # we should have set it now
+          if fixed_seq_len is None and rec_layer.output.size_placeholder:  # might have set it by now
             fixed_seq_len = rec_layer.output.size_placeholder[0]
           for layer_name in self.input_layers_moved_out:
             # Create only Tensor arrays for those which we use inside the loop.
@@ -2990,10 +2989,17 @@ class _SubnetworkRecCell(object):
         get_layer(layer_name)
 
     # We might have figured out the real output seq length (and dim tag) by now.
-    if "output" in self.input_layers_moved_out and not self.parent_rec_layer.output.size_placeholder:
+    if not self.parent_rec_layer.output.size_placeholder and "output" in self.input_layers_moved_out:
       output_layer = self.input_layers_net.layers["output"]
       assert output_layer.output.have_time_axis()
       self.parent_rec_layer.output.size_placeholder = {0: output_layer.output.get_sequence_lengths()}
+    # This might be set e.g. by ChoiceLayer, or losses.
+    if not self.parent_rec_layer.output.size_placeholder and self.input_layers_net.used_data_keys:
+      for data_key in sorted(self.input_layers_net.used_data_keys):
+        data = self.input_layers_net.extern_data.data[data_key]
+        if data.have_time_axis():
+          self.parent_rec_layer.output.size_placeholder = {0: data.get_sequence_lengths()}
+          break
 
   def _construct_output_layers_moved_out(self, loop_accumulated, seq_len, extra_output_layers, final_net_vars):
     """
