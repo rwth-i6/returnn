3ba2402c6364c7afa8fb5284602073df87f40c3f
Albert Zeyer <albzey@gmail.com>
2021-03-02 00:46:22 +0100

    import_ prepare for symlink logic

--- returnn/import_/import_.py
+++ returnn/import_/import_.py
@@ -4,11 +4,13 @@ This provides the main :func:`import_` function.
 
 import types
 import os
-import sys
+import importlib
 from .git import get_repo_path
 
 
-_ModuleNamePrefix = "returnn.util.import_._loaded."
+_MyDir = os.path.dirname(os.path.abspath(__file__))
+_PyPkgSymlinkDir = _MyDir + "/_pkg"
+_ModuleNamePrefix = "returnn.import_._pkg."
 
 
 def import_(repo, path, version=None):
@@ -19,18 +21,9 @@ def import_(repo, path, version=None):
   :rtype: object|types.ModuleType
   """
   assert path and path[:1] != "/" and ".." not in path
-  # This code is only Python >=3.5.
-  # noinspection PyUnresolvedReferences,PyCompatibility
-  import importlib.util
   repo_path = get_repo_path(repo=repo, version=version)
-  full_path = "%s/%s" % (repo_path, path)
   mod_name = _module_name(repo=repo, repo_path=repo_path, path=path, version=version)
-  if mod_name in sys.modules:
-    return sys.modules[mod_name]
-  spec = importlib.util.spec_from_file_location(mod_name, full_path)
-  mod = importlib.util.module_from_spec(spec)
-  spec.loader.exec_module(mod)  # noqa
-  return mod
+  return importlib.import_module(mod_name)
 
 
 def _module_name(repo, repo_path, path, version):
@@ -41,12 +34,72 @@ def _module_name(repo, repo_path, path, version):
   :param str|None version:
   :rtype: str
   """
+  full_path = "%s/%s" % (repo_path, path)
+  py_pkg_dirname = _find_root_python_package(full_path)
+  assert len(py_pkg_dirname) >= len(repo_path)
+  rel_pkg_path = full_path[len(py_pkg_dirname) + 1:]
+  p = rel_pkg_path.find("/")
+  if p > 0:
+    rel_pkg_path0 = rel_pkg_path[:p]
+  else:
+    rel_pkg_path0 = rel_pkg_path
+  rel_pkg_dir = py_pkg_dirname[len(repo_path):]  # starting with "/"
+
   repo_dir_name = os.path.dirname(repo)
   repo_v = "%s/%s" % (repo_dir_name, os.path.basename(repo_path))  # eg "github.com/rwth-i6/returnn-experiments@v..."
   if version:
     repo_v = repo_v.replace("@v", "/v")
   else:
     repo_v = repo_v + "/dev"
-  repo_and_path = "%s/%s" % (repo_v, path)
+  py_pkg_dir = "%s/%s%s" % (_PyPkgSymlinkDir, repo_v, rel_pkg_dir)
+  _mk_py_pkg_dirs(_PyPkgSymlinkDir, py_pkg_dir)
+  symlink_file = "%s/%s" % (py_pkg_dir, rel_pkg_path0)
+  symlink_target = "%s%s/%s" % (repo_path, rel_pkg_dir, rel_pkg_path0)
+  if os.path.exists(symlink_file):
+    assert os.readlink(symlink_file) == symlink_target
+  else:
+    os.symlink(symlink_target, symlink_file, target_is_directory=os.path.isdir(symlink_target))
+
+  repo_and_path = "%s/%s" % (repo_v, path[:-3] if path.endswith(".py") else path)
   name = repo_and_path.replace(".", "_").replace("/", ".")
   return _ModuleNamePrefix + name
+
+
+def _mk_py_pkg_dirs(path, path_=None):
+  """
+  :param str path:
+  :param str|None path_:
+  """
+  if path_:
+    assert path_.startswith(path + "/")
+  while True:
+    if os.path.exists(path):
+      assert os.path.isdir(path) and not os.path.islink(path) and os.path.exists(path + "/__init__.py")
+    else:
+      os.mkdir(path)
+      open(path + "/__init__.py", "x").close()
+    if path_:
+      if len(path) == len(path_):
+        break
+      p = path_.find("/", len(path) + 1)
+      if p > 0:
+        path = path_[:p]
+      else:
+        break
+    else:
+      break
+
+
+def _find_root_python_package(full_path):
+  """
+  :param str full_path: some Python file
+  :return: going up from path, and first dir which does not include __init__.py
+  :rtype: str
+  """
+  p = len(full_path)
+  while True:
+    p = full_path.rfind("/", 0, p)
+    assert p > 0
+    d = full_path[:p]
+    if not os.path.exists(d + "/__init__.py"):
+      return d
