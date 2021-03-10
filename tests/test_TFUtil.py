test TFUtil, multiple fixes for broadcast batch dim

tests/test_TFUtil.py
@@ -342,7 +342,7 @@ def test_Data_find_matching_dim_map_broadcast_matches():
   assert mapping[0] == 1 and mapping[1] == 2
 
   copied = d2.copy_compatible_to(d1)
-  assert copied.batch_ndim == d1.batch_ndim and copied.batch_shape == (None, 5, 1)
+  assert copied.batch_ndim == d1.batch_ndim and copied.batch.static_dim == 1 and copied.batch_shape == (1, 5, 1)
   print('copied compatible:', copied)
 
 
@@ -485,14 +485,14 @@ def test_Data_copy_compatible_to_src_no_batch():
   d2 = Data(name="d2", shape=(), batch_dim_axis=None, time_dim_axis=None)
   d2.placeholder = tf.zeros([d if (d is not None) else 1 for d in d2.batch_shape])
   d3 = d2.copy_compatible_to(d1)
-  assert d3.batch_shape == (None, 1, 1)
+  assert d3.batch.static_dim == 1 and d3.batch_shape == (1, 1, 1)
 
 
 def test_Data_copy_compatible_to_add_batch_dim():
   common_data = Data(name='accum_att_weights_output', shape=(None, 1))
   d1 = Data(name='att_weights_avg_output', shape=(1,), batch_dim_axis=None)
   d2 = d1.copy_compatible_to(common_data)
-  assert d2.batch_dim_axis is not None and d2.batch_shape == (None, 1, 1)
+  assert d2.have_batch_axis() and d2.batch.static_dim == 1 and d2.batch_shape == (1, 1, 1)
 
 
 def test_Data_copy_compatible_to_add_feature_dim():
@@ -504,9 +504,14 @@ def test_Data_copy_compatible_to_add_feature_dim():
 
 def test_Data_copy_compatible_to_add_batch_feature_dim():
   common_data = Data(name='energy', shape=(None, 3))  # [B,T,F]
+  assert common_data.batch_shape == (None, None, 3)
   d1 = Data(name='mask', shape=(None,), batch_dim_axis=None, time_dim_axis=0, feature_dim_axis=None)  # [T]
+  assert d1.batch_shape == (None,)
   d2 = d1.copy_compatible_to(common_data)
-  assert d2.batch_dim_axis is not None and d2.time_dim_axis is not None and d2.batch_shape == (None, None, 1)
+  print(d2)
+  assert d2.have_batch_axis() and d2.have_time_axis()
+  assert d2.batch.static_dim == 1  # batch-dim is broadcasted
+  assert d2.batch_shape == (1, None, 1)
 
 
 def test_Data_copy_compatible_to_add_time_dim():