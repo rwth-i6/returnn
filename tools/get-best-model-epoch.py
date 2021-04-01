984b801fe73d83d50bad41549520605a0ad6482a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-04-01 13:01:58 +0200

    get best model epoch script, multiple warning fixes, small extension

--- tools/get-best-model-epoch.py
+++ tools/get-best-model-epoch.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+Based on the scores / errors in a learning rate file ("newbob" file),
+calculates the best epochs.
+"""
 
 from __future__ import print_function
 
-import os
 import sys
-import numpy as np
 import argparse
-from glob import glob
-import typing
 
 # Returnn imports
 import _setup_returnn_env  # noqa
@@ -20,9 +20,13 @@ from returnn.learning_rate_control import LearningRateControl
 
 
 def main():
+  """
+  Main entry point.
+  """
   arg_parser = argparse.ArgumentParser()
   arg_parser.add_argument("--config", help="RETURNN config")
   arg_parser.add_argument("--learning-rate-file", help="The learning rate file contains scores / errors per epoch.")
+  arg_parser.add_argument("--key", help="key to use, e.g. 'dev_error'")
   arg_parser.add_argument("--n", type=int, default=5, help="print best N epochs")
   args = arg_parser.parse_args()
 
@@ -34,30 +38,34 @@ def main():
   if args.config:
     config = Config()
     config.load_file(args.config)
-    lr = LearningRateControl.load_initial_from_config()
+    lr = LearningRateControl.load_initial_from_config(config)
   elif args.learning_rate_file:
     lr = LearningRateControl(
       default_learning_rate=1,  # default lr not relevant
       filename=args.learning_rate_file)
   else:
-    assert False, f"should not get here with {args}"
+    assert False, "should not get here with %r" % args
 
   epochs = sorted(lr.epoch_data.keys())
   if not epochs:
     print("Error: no epochs found")
     sys.exit(1)
-  print(f"Loaded epochs {epochs[0]}..{epochs[-1]}")
+  print("Loaded epochs", epochs[0], "..", epochs[-1])
 
-  last_epoch_with_error_info = None
-  for ep in reversed(epochs):
-    if lr.epoch_data[ep].error:
-      last_epoch_with_error_info = ep
-      break
-  if last_epoch_with_error_info is None:
-    print("Error: no scores/errors found")
-    sys.exit(1)
-  key = lr.get_error_key(last_epoch_with_error_info)
-  print(f"Using key {key} (via epoch {last_epoch_with_error_info})")
+  if args.key:
+    key = args.key
+    print("Using key %s" % key)
+  else:
+    last_epoch_with_error_info = None
+    for ep in reversed(epochs):
+      if lr.epoch_data[ep].error:
+        last_epoch_with_error_info = ep
+        break
+    if last_epoch_with_error_info is None:
+      print("Error: no scores/errors found")
+      sys.exit(1)
+    key = lr.get_error_key(last_epoch_with_error_info)
+    print("Using key %s (auto via epoch %i)" % (key, last_epoch_with_error_info))
 
   epochs_ = []
   missing_epochs = []
@@ -70,17 +78,18 @@ def main():
   if len(epochs_) == len(epochs):
     print("All epochs have the key.")
   else:
-    print(f"Epochs missing the ky: {missing_epochs}")
+    print("Epochs missing the key:", missing_epochs)
   assert epochs_
   epochs_.sort()
 
   for value, ep in epochs_[:args.n]:
     errors = lr.epoch_data[ep].error
-    print(f"Epoch {ep}, {key} {value}, " + ", ".join(f"{k} {v}" for k, v in errors.items() if k != key))
+    print(", ".join(
+      ["Epoch %i" % ep, "%r %r" % (key, value)] +
+      ["%r %r" for k, v in errors.items() if k != key]))
 
 
 if __name__ == "__main__":
   better_exchook.install()
   log.initialize(verbosity=[5])
   main()
-
