a823fa6ec5d5bb1677e801631b54bb1963472393
vieting <45091115+vieting@users.noreply.github.com>
2021-04-27 07:54:56 +0200

    Update returnn/tf/util/data.py
    Co-authored-by: Albert Zeyer <albzey@gmail.com>

--- returnn/tf/util/data.py
+++ returnn/tf/util/data.py
@@ -2574,7 +2574,7 @@ class Data(object):
       elif axes.startswith("stag-single:"):  # spatial tag which possibly matches multiple spatial axes
         # in this case, a name of form "stag-single:<idx>:<name> is expected.
         # idx is relative to the matching stags, i.e., it is the index among the list of spatial dims matching the name
-        idx_s, name = axes.split(":", 1)
+        _, idx_s, name = axes.split(":", 2)  # stag-single:<idx>:<name>
         idx = int(idx_s)
         axes = self.get_axes_by_tag_name(name, spatial_only=True)[idx]
       else:
