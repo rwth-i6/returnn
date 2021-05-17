295fc54ab7b8f9a46f87acef249bb05a9e8895b8
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2021-05-17 14:54:15 +0200

    fix wrong github link paths

--- docs/generateapi.py
+++ docs/generateapi.py
@@ -33,11 +33,11 @@ def generate():
       return
     f = open(fn, "w")
     target_python_file_path = modname.replace(".", "/")
-    if os.path.isfile(target_python_file_path):
+    if os.path.isfile(target_python_file_path + ".py"):
       f.write(":github_url: https://github.com/rwth-i6/returnn/blob/master/%s.py\n\n" % modname.replace(".", "/"))
     else:
       f.write(":github_url: https://github.com/rwth-i6/returnn/blob/master/%s.py\n\n" %
-              os.path.join(os.path.dirname(modname.replace(".", "/")), "__init__"))
+              os.path.join(modname.replace(".", "/"), "__init__"))
     title = ":mod:`%s`" % modname
     f.write("\n%s\n%s\n\n" % (title, "-" * len(title)))
     f.write(".. automodule:: %s\n\t:members:\n\t:undoc-members:\n\n" % modname)
