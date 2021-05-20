4fb00b8978deb7ff03d3d54b3f21fc0db3ef13f3
Frithjof Petrick <fmpetrick@gmail.com>
2021-05-20 19:01:24 +0200

    CombineLayer: Add kinds logical_and and logical_or

--- returnn/tf/layers/basic.py
+++ returnn/tf/layers/basic.py
@@ -5822,7 +5822,8 @@ class CombineLayer(LayerBase):
                eval=None, eval_locals=None, eval_for_output_loss=False,
                **kwargs):
     """
-    :param str kind: currently accepted values are `average`, `add`, `sub`, `mul`, 'truediv', or `eval`
+    :param str kind:
+      currently accepted values are `average`, `add`, `sub`, `mul`, `truediv`, `logical_and`, `logical_or`, or `eval`
     :param list[LayerBase] sources:
     :param str|None activation: if provided, activation function to apply, e.g. "tanh" or "relu"
     :param bool with_bias: if given, will add a trainable bias tensor
@@ -5831,7 +5832,7 @@ class CombineLayer(LayerBase):
     :param bool eval_for_output_loss: will do the same eval on layer.output_loss
     """
     super(CombineLayer, self).__init__(sources=sources, **kwargs)
-    assert kind in ["average", "add", "sub", "mul", "truediv", "eval"], (
+    assert kind in ["average", "add", "sub", "mul", "truediv", "logical_and", "logical_or", "eval"], (
       "%s: Invalid `kind` %r for this layer." % (self, kind))
     op = self._get_op(kind=kind, eval_str=eval, eval_locals=eval_locals)
     x = op(sources)
@@ -5943,6 +5944,20 @@ class CombineLayer(LayerBase):
     x = self._op_kind_add(sources)
     x /= len(sources)
     return x
+
+  def _op_kind_logical_and(self, sources):
+    """
+    :param list[LayerBase] sources:
+    :rtype: tf.Tensor
+    """
+    return self._op_dense_fn(sources, tf.logical_and)
+
+  def _op_kind_logical_or(self, sources):
+    """
+    :param list[LayerBase] sources:
+    :rtype: tf.Tensor
+    """
+    return self._op_dense_fn(sources, tf.logical_or)
 
   def _op_kind_eval(self, sources, eval_str, eval_locals=None):
     """
