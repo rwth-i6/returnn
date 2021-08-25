21d6b64483050faae75e54ea2efa7a48e399adad
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-08-25 14:54:20 +0200

    test demos cleanup

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -7,6 +7,7 @@ import re
 import os
 import sys
 from glob import glob
+import unittest
 from nose.tools import assert_less, assert_in
 from returnn.util import better_exchook
 from returnn.util.basic import which_pip
@@ -14,6 +15,15 @@ from returnn.util.basic import which_pip
 
 py = sys.executable
 print("Python:", py)
+
+os.chdir((os.path.dirname(__file__) or ".") + "/..")
+assert os.path.exists("rnn.py")
+
+
+try:
+  import theano
+except ImportError:
+  theano = None
 
 
 def build_env():
@@ -79,110 +89,111 @@ def cleanup_tmp_models(config_filename):
     os.remove(f)
 
 
-class TestDemos(object):
+@unittest.skipIf(not theano, "Theano not installed")
+def test_demo_task12ax():
+  fer = run_config_get_fer("demos/demo-theano-task12ax.config")
+  assert_less(fer, 0.01)
 
-  @classmethod
-  def setup_class(cls):
-    os.chdir((os.path.dirname(__file__) or ".") + "/..")
-    assert os.path.exists("rnn.py")
 
-  def test_demo_task12ax(self):
-    fer = run_config_get_fer("demos/demo-theano-task12ax.config")
-    assert_less(fer, 0.01)
+def test_demo_iter_dataset_task12ax():
+  cleanup_tmp_models("demos/demo-theano-task12ax.config")
+  out = run(py, "demos/demo-iter-dataset.py", "demos/demo-theano-task12ax.config")
+  assert_in("Epoch 5.", out.splitlines())
 
-  def test_demo_iter_dataset_task12ax(self):
-    cleanup_tmp_models("demos/demo-theano-task12ax.config")
-    out = run(py, "demos/demo-iter-dataset.py", "demos/demo-theano-task12ax.config")
-    assert_in("Epoch 5.", out.splitlines())
 
-  def test_demo_returnn_as_framework(self):
-    print("Prepare.")
-    import subprocess
-    import shutil
-    from glob import glob
-    from returnn.util.basic import get_login_username
-    # echo via subprocess, because this stdout as well as the other will always be visible.
-    subprocess.check_call(["echo", "travis_fold:start:test_demo_returnn_as_framework"])
-    assert os.path.exists("setup.py")
-    if glob("dist/*.tar.gz"):
-      # we want it unique below
-      for fn in glob("dist/*.tar.gz"):
-        os.remove(fn)
-    if os.path.exists("MANIFEST"):
-      os.remove("MANIFEST")  # auto-generated. will be recreated
-    if os.path.exists("docs/crnn"):
-      os.remove("docs/crnn")  # this is auto-generated, and confuses setup.py sdist
-    if os.path.exists("docs/returnn"):
-      os.remove("docs/returnn")  # this is auto-generated, and confuses setup.py sdist
-    tmp_model_dir = "/tmp/%s/returnn-demo-as-framework" % get_login_username()
-    if os.path.exists(tmp_model_dir):
-      shutil.rmtree(tmp_model_dir, ignore_errors=True)
-    print("setup.py sdist, to create package.")
-    subprocess.check_call([py, "setup.py", "sdist"])
-    dist_fns = glob("dist/*.tar.gz")
-    assert len(dist_fns) == 1
-    dist_fn = os.path.abspath(dist_fns[0])
-    pip_path = which_pip()
-    print("Pip install Returnn.")
-    in_virtual_env = hasattr(sys, 'real_prefix')  # https://stackoverflow.com/questions/1871549/
-    cmd = [py, pip_path, "install"]
-    if not in_virtual_env:
-      cmd += ["--user"]
-    cmd += ["-v", dist_fn]
-    print("$ %s" % " ".join(cmd))
-    subprocess.check_call(cmd, cwd="/")
-    print("Running demo now.")
-    subprocess.check_call([py, "demo-returnn-as-framework.py"], cwd="demos")
-    print("Success.")
-    subprocess.check_call(["echo", "travis_fold:end:test_demo_returnn_as_framework"])
+def test_demo_returnn_as_framework():
+  print("Prepare.")
+  import subprocess
+  import shutil
+  from glob import glob
+  from returnn.util.basic import get_login_username
+  # echo via subprocess, because this stdout as well as the other will always be visible.
+  subprocess.check_call(["echo", "travis_fold:start:test_demo_returnn_as_framework"])
+  assert os.path.exists("setup.py")
+  if glob("dist/*.tar.gz"):
+    # we want it unique below
+    for fn in glob("dist/*.tar.gz"):
+      os.remove(fn)
+  if os.path.exists("MANIFEST"):
+    os.remove("MANIFEST")  # auto-generated. will be recreated
+  if os.path.exists("docs/crnn"):
+    os.remove("docs/crnn")  # this is auto-generated, and confuses setup.py sdist
+  if os.path.exists("docs/returnn"):
+    os.remove("docs/returnn")  # this is auto-generated, and confuses setup.py sdist
+  tmp_model_dir = "/tmp/%s/returnn-demo-as-framework" % get_login_username()
+  if os.path.exists(tmp_model_dir):
+    shutil.rmtree(tmp_model_dir, ignore_errors=True)
+  print("setup.py sdist, to create package.")
+  subprocess.check_call([py, "setup.py", "sdist"])
+  dist_fns = glob("dist/*.tar.gz")
+  assert len(dist_fns) == 1
+  dist_fn = os.path.abspath(dist_fns[0])
+  pip_path = which_pip()
+  print("Pip install Returnn.")
+  in_virtual_env = hasattr(sys, 'real_prefix')  # https://stackoverflow.com/questions/1871549/
+  cmd = [py, pip_path, "install"]
+  if not in_virtual_env:
+    cmd += ["--user"]
+  cmd += ["-v", dist_fn]
+  print("$ %s" % " ".join(cmd))
+  subprocess.check_call(cmd, cwd="/")
+  print("Running demo now.")
+  subprocess.check_call([py, "demo-returnn-as-framework.py"], cwd="demos")
+  print("Success.")
+  subprocess.check_call(["echo", "travis_fold:end:test_demo_returnn_as_framework"])
 
-  def test_demo_sprint_interface(self):
-    import subprocess
-    # echo via subprocess, because this stdout as well as the other will always be visible.
-    subprocess.check_call(["echo", "travis_fold:start:test_demo_sprint_interface"])
-    subprocess.check_call([py, os.path.abspath("demos/demo-sprint-interface.py")], cwd="/")
-    subprocess.check_call(["echo", "travis_fold:end:test_demo_sprint_interface"])
 
-  def test_returnn_as_framework_TaskSystem(self):
-    import subprocess
-    # echo via subprocess, because this stdout as well as the other will always be visible.
-    subprocess.check_call(["echo", "travis_fold:start:test_returnn_as_framework_TaskSystem"])
-    subprocess.check_call([py, os.path.abspath("tests/returnn-as-framework.py"), "test_TaskSystem_Pickler()"], cwd="/")
-    subprocess.check_call(["echo", "travis_fold:end:test_returnn_as_framework_TaskSystem"])
+def test_demo_sprint_interface():
+  import subprocess
+  # echo via subprocess, because this stdout as well as the other will always be visible.
+  subprocess.check_call(["echo", "travis_fold:start:test_demo_sprint_interface"])
+  subprocess.check_call([py, os.path.abspath("demos/demo-sprint-interface.py")], cwd="/")
+  subprocess.check_call(["echo", "travis_fold:end:test_demo_sprint_interface"])
 
-  def test_returnn_as_framework_old_style_crnn_TFUtil(self):
-    import subprocess
-    # echo via subprocess, because this stdout as well as the other will always be visible.
-    subprocess.check_call(["echo", "travis_fold:start:test_returnn_as_framework_old_style_crnn_TFUtil"])
-    subprocess.check_call([
-      py, os.path.abspath("tests/returnn-as-framework.py"),
-      "--old-style", "--returnn-package-name", "crnn",
-      "test_old_style_import_crnn_TFUtil()"], cwd="/")
-    subprocess.check_call(["echo", "travis_fold:end:test_returnn_as_framework_old_style_crnn_TFUtil"])
 
-  def test_returnn_as_framework_old_style_TFUtil(self):
-    import subprocess
-    # echo via subprocess, because this stdout as well as the other will always be visible.
-    subprocess.check_call(["echo", "travis_fold:start:test_returnn_as_framework_old_style_TFUtil"])
-    subprocess.check_call([
-      py, os.path.abspath("tests/returnn-as-framework.py"), "--old-style", "test_old_style_import_TFUtil()"], cwd="/")
-    subprocess.check_call(["echo", "travis_fold:end:test_returnn_as_framework_old_style_TFUtil"])
+def test_returnn_as_framework_TaskSystem():
+  import subprocess
+  # echo via subprocess, because this stdout as well as the other will always be visible.
+  subprocess.check_call(["echo", "travis_fold:start:test_returnn_as_framework_TaskSystem"])
+  subprocess.check_call([py, os.path.abspath("tests/returnn-as-framework.py"), "test_TaskSystem_Pickler()"], cwd="/")
+  subprocess.check_call(["echo", "travis_fold:end:test_returnn_as_framework_TaskSystem"])
+
+
+def test_returnn_as_framework_old_style_crnn_TFUtil():
+  import subprocess
+  # echo via subprocess, because this stdout as well as the other will always be visible.
+  subprocess.check_call(["echo", "travis_fold:start:test_returnn_as_framework_old_style_crnn_TFUtil"])
+  subprocess.check_call([
+    py, os.path.abspath("tests/returnn-as-framework.py"),
+    "--old-style", "--returnn-package-name", "crnn",
+    "test_old_style_import_crnn_TFUtil()"], cwd="/")
+  subprocess.check_call(["echo", "travis_fold:end:test_returnn_as_framework_old_style_crnn_TFUtil"])
+
+
+def test_returnn_as_framework_old_style_TFUtil():
+  import subprocess
+  # echo via subprocess, because this stdout as well as the other will always be visible.
+  subprocess.check_call(["echo", "travis_fold:start:test_returnn_as_framework_old_style_TFUtil"])
+  subprocess.check_call([
+    py, os.path.abspath("tests/returnn-as-framework.py"), "--old-style", "test_old_style_import_TFUtil()"], cwd="/")
+  subprocess.check_call(["echo", "travis_fold:end:test_returnn_as_framework_old_style_TFUtil"])
 
 
 if __name__ == '__main__':
   better_exchook.install()
-  TestDemos.setup_class()
-  tests = TestDemos()
   import sys
   fns = sys.argv[1:]
   if not fns:
-    fns = [arg for arg in dir(tests) if arg.startswith("test_")]
+    fns = [arg for arg in globals() if arg.startswith("test_")]
   for arg in fns:
-    f = getattr(tests, arg)
+    f = globals()[arg]
     print("-" * 20)
     print("Run:", f)
     print("-" * 20)
-    f()
+    try:
+      f()
+    except unittest.SkipTest as exc:
+      print("(SkipTest: %s)" % exc)
     print("-" * 20)
     print("Ok.")
     print("-" * 20)
