a39c879be64134cac419c67113bf13a91cc655f1
Albert Zeyer <albzey@gmail.com>
2021-09-14 12:05:51 +0200

    Update returnn/__main__.py

--- returnn/__main__.py
+++ returnn/__main__.py
@@ -228,7 +228,6 @@ def load_data(config, cache_byte_size, files_config_key, **kwargs):
     Dataset.kwargs_update_from_config(config, kwargs)
     data = init_dataset(kwargs)
   elif config.is_typed(files_config_key) and callable(config.typed_value(files_config_key)):
-    # Config contains callable, returning dataset object, load it
     data = init_dataset(config.typed_value(files_config_key))
   else:
     config_str = config.value(files_config_key, "")
