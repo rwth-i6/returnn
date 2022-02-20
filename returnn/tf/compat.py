c4b178855ab3f620c65abafcd8b88c58703f960a
Albert Zeyer <albzey@gmail.com>
2022-02-20 19:18:51 +0100

    control flow v2 only with TF_ENABLE_COND_V2

--- returnn/tf/compat.py
+++ returnn/tf/compat.py
@@ -11,6 +11,7 @@ https://github.com/tensorflow/lingvo/blob/master/lingvo/compat.py
 """
 
 import tensorflow as tf
+import os as _os
 
 if not getattr(tf, "compat", None) or not getattr(tf.compat, "v2", None):
   v1 = tf
@@ -24,14 +25,16 @@ else:
 if v2 and tf.__version__.startswith("2."):
   tf.compat.v1.disable_eager_execution()
   tf.compat.v1.disable_v2_tensorshape()
-  # Earlier we also had disable_control_flow_v2() but this can cause problems
-  # (InvalidArgumentError: Retval[0] does not have value)
-  # and it should not be needed.
+  # There are still issues with control flow v2,
+  # so we do not enable it yet by default.
+  if _os.environ.get("TF_ENABLE_COND_V2", "") == "1":
+    # https://github.com/tensorflow/tensorflow/issues/54458
+    tf.compat.v1.experimental.output_all_intermediates(True)
+  else:
+    tf.compat.v1.disable_control_flow_v2()
   # There is also disable_v2_behavior() which disables all the TF2 behavior,
   # including control flow, eager execution, etc.,
   # but we want to make use of TF2 features as much as possible.
-  # https://github.com/tensorflow/tensorflow/issues/54458
-  tf.compat.v1.experimental.output_all_intermediates(True)
 
 try:
   import tensorflow.contrib
