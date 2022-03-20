837c632a579e0affcce7ffafc9f9cb1ce256f917
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2022-03-20 20:12:54 +0100

    compile TF graph, output_batch_major, small fix if no feature

--- tools/compile_tf_graph.py
+++ tools/compile_tf_graph.py
@@ -1509,7 +1509,10 @@ def main(argv):
       if layer.output.batch_dim_axis is None:
         continue
       with tf_util.reuse_name_scope(layer.get_absolute_name_scope_prefix()[:-1], absolute=True):
-        tf.identity(layer.output.copy_as_batch_major().copy_with_feature_last().placeholder, name="output_batch_major")
+        out = layer.output.copy_as_batch_major()
+        if out.have_feature_axis():
+          out = out.copy_with_feature_last()
+        tf.identity(out.placeholder, name="output_batch_major")
 
     tf.group(*network.get_post_control_dependencies(), name="post_control_dependencies")
 
