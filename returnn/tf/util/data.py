8a695af2e9a46b2d60ea704e00ab7e3460bb7d63
Albert Zeyer <albzey@gmail.com>
2022-05-20 20:42:23 +0200

    _create_size_placeholder small fix

--- returnn/tf/util/data.py
+++ returnn/tf/util/data.py
@@ -5597,9 +5597,10 @@ def _create_size_placeholder(name, axis_wo_b, tag, batch_dim):
   from .basic import reuse_name_scope
   with reuse_name_scope("extern_data/placeholders/%s" % name, absolute=True):
     dyn_size_ext = Data(
-      "%s_dim%i_size" % (name, axis_wo_b), dtype=Data.size_dtype, dim_tags=[batch_dim] if batch_dim else None)
+      "%s_dim%i_size" % (name, axis_wo_b), dtype=Data.size_dtype, dim_tags=[batch_dim] if batch_dim else [])
     dyn_size = tf_compat.v1.placeholder(
       name=dyn_size_ext.name, dtype=dyn_size_ext.dtype, shape=dyn_size_ext.batch_shape)
+    dyn_size_ext.placeholder = dyn_size
     tag.dyn_size_ext = dyn_size_ext
     tag.set_tag_on_size_tensor(dyn_size)
 
