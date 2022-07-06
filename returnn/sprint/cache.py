ca5512ebf8307752c3165b0f524333165734efee
Daniel Mann <mann@i6.informatik.rwth-aachen.de>
2022-07-06 19:09:46 +0200

    Fix typ variable ambiguity in sprint.cache.FileArchive._raw_read

--- returnn/sprint/cache.py
+++ returnn/sprint/cache.py
@@ -313,11 +313,10 @@ class FileArchive:
 
     elif typ in ["align", "align_raw"]:
       type_len = self.read_U32()
-      typ = self.read_str(type_len)
-      assert typ == "flow-alignment"
+      assert self.read_str(type_len) == "flow-alignment"
       self.read_u32()  # flag ?
-      typ = self.read_str(8)
-      if typ in ["ALIGNRLE", "AALPHRLE"]:
+      alignment_header = self.read_str(8)
+      if alignment_header in ["ALIGNRLE", "AALPHRLE"]:
         # In case of AALPHRLE, after the alignment, we include the alphabet of the used labels.
         # We ignore this at the moment.
         size = self.read_U32()
@@ -356,7 +355,7 @@ class FileArchive:
           raise NotImplementedError("No support for weighted "
                                     "alignments yet.")
       else:
-        raise Exception("No valid alignment header found (found: %r). Wrong cache?" % typ)
+        raise Exception("No valid alignment header found (found: %r). Wrong cache?" % alignment_header)
 
   def has_entry(self, filename):
     """
