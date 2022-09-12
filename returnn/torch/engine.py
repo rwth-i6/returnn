4db5d8d97f73e097665b8d9f21c20bc9e59786fe
Albert Zeyer <albzey@gmail.com>
2022-09-12 16:27:34 +0200

    PyTorch engine dummy
    #1120

--- returnn/torch/engine.py
+++ returnn/torch/engine.py
@@ -2,8 +2,8 @@
 Main engine for PyTorch
 """
 
-from returnn.config import Config
 from returnn.engine.base import EngineBase
+from returnn.datasets.basic import init_dataset
 
 
 class Engine(EngineBase):
@@ -13,7 +13,33 @@ class Engine(EngineBase):
 
   def __init__(self, config):
     """
-    :param Config config:
+    :param returnn.config.Config config:
     """
     super(Engine, self).__init__()
     self.config = config
+    self.train_dataset = None
+    self.eval_datasets = {}
+
+  def init_train_from_config(self, config=None, train_data=None, dev_data=None, eval_data=None):
+    """
+    :param returnn.config.Config|None config:
+    :param returnn.datasets.basic.Dataset|None train_data:
+    :param returnn.datasets.basic.Dataset|None dev_data:
+    :param returnn.datasets.basic.Dataset|None eval_data:
+    """
+    assert config is self.config
+    self.train_dataset = train_data
+    self.eval_datasets.clear()
+    if dev_data:
+      self.eval_datasets["dev"] = dev_data
+    if eval_data:
+      self.eval_datasets["eval"] = eval_data
+    if config.has("eval_datasets"):
+      for dataset_name, dataset_opts in config.typed_value("eval_datasets", {}).items():
+        self.eval_datasets[dataset_name] = init_dataset(dataset_opts, default_kwargs={"name": dataset_name})
+
+  def train(self):
+    """
+    Main training loop.
+    """
+    pass
