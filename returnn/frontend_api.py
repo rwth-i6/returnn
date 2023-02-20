f5b65a4f63066091d66f0dc95ad9402784270bfc
Albert Zeyer <albzey@gmail.com>
2023-02-20 23:13:21 +0100

    frontend reduce TODO

--- returnn/frontend_api.py
+++ returnn/frontend_api.py
@@ -2,8 +2,33 @@
 Frontend API
 """
 
+from __future__ import annotations
+from typing import Union, Sequence
+
+from returnn.tensor import Tensor, Dim
+from returnn.util.basic import NotSpecified
+
 
 class Frontend:
     """
     Abstract base class for the frontend
     """
+
+    @staticmethod
+    def reduce(
+        source: Tensor,
+        *,
+        mode: str,
+        axis: Union[Dim, Sequence[Dim]],
+        use_time_mask: bool = NotSpecified,
+    ) -> Tensor:
+        """
+        Reduce the tensor along the given axis
+
+        :param source:
+        :param mode: "max", "min", "mean", "sum", "prod", "any", "all"
+        :param axis:
+        :param use_time_mask: if True, use the time mask (part of dim tag) to ignore padding frames
+        :return: tensor with axis removed
+        """
+        raise NotImplementedError
--- returnn/tf/frontend/frontend.py
+++ returnn/tf/frontend/frontend.py
@@ -2,10 +2,22 @@
 Frontend for exposing TensorFlow-specific functionality.
 """
 
+from __future__ import annotations
+from typing import Union, Sequence
+
+from returnn.util.basic import NotSpecified
 from returnn.frontend_api import Frontend
+from returnn.tensor import Tensor, Dim
 
 
 class TFFrontend(Frontend):
     """
     TensorFlow frontend
     """
+
+    @staticmethod
+    def reduce(
+        source: Tensor, *, mode: str, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified
+    ) -> Tensor:
+        """Reduce"""
+        pass  # TODO
