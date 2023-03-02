9c4e17fadef089f0fec86433af81660de31c2479
Albert Zeyer <albzey@gmail.com>
2023-03-02 17:04:02 +0100

    Dim get_for_batch_ctx, ignore when undefined

--- returnn/tensor/_dim_extra.py
+++ returnn/tensor/_dim_extra.py
@@ -434,6 +434,8 @@ class _DimMixin:
             # If static dim, no effect.
             assert not self.batch
             return self
+        if not self.is_dim_known():
+            return self
         if batch.is_broadcast():
             return self  # just leave as-is. should not matter.
         if self._extra:
