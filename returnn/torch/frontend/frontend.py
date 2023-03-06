8066ba3ee143f194890c75a931926f2471fad902
patrick-wilken <pwilken@apptek.com>
2023-03-06 13:44:39 -0500

    TorchFrontend: dot product

--- returnn/torch/frontend/frontend.py
+++ returnn/torch/frontend/frontend.py
@@ -3,10 +3,17 @@ Frontend for exposing PyTorch-specific functionality.
 """
 
 from __future__ import annotations
+from typing import Union, Sequence
 
 import torch
 
 from returnn.frontend_api import Frontend
+from ._internal_frontend import TorchInternalFrontend
+from returnn.tensor import Tensor, Dim
+
+from returnn.util.basic import prod
+
+_TT = Tensor[torch.Tensor]
 
 
 # Ignore this warning until we really expect that we implemented everything.
@@ -17,3 +24,74 @@ class TorchFrontend(Frontend[torch.Tensor]):
     """
 
     RawTensorType = torch.Tensor
+    _internal_frontend = TorchInternalFrontend
+
+    @staticmethod
+    def dot(a: _TT, b: _TT, *, reduce: Union[Dim, Sequence[Dim]]) -> _TT:
+        """
+        dot-product of a and b, see base class doc string
+        """
+        if isinstance(reduce, Dim):
+            reduce = [reduce]
+
+        a_dims = a.dims
+        b_dims = b.dims
+
+        assert all(
+            dim in a_dims for dim in reduce
+        ), f"'a' does not have the specified reduce dim(s) {reduce} (a dims: {a_dims})"
+        assert all(
+            dim in b_dims for dim in reduce
+        ), f"'b' does not have the specified reduce dim(s) {reduce} (b dims: {b_dims})"
+
+        reduce_dims_sorted_by_a = [dim for dim in a_dims if dim in reduce]
+
+        a_reduce_axes = [a_dims.index(reduce_dim) for reduce_dim in reduce_dims_sorted_by_a]
+        b_reduce_axes = [b_dims.index(reduce_dim) for reduce_dim in reduce_dims_sorted_by_a]
+
+        a_unique_axes = [i for (i, dim) in enumerate(a_dims) if dim not in b_dims]
+        b_unique_axes = [i for (i, dim) in enumerate(b_dims) if dim not in a_dims]
+
+        common_dims = [dim for dim in a_dims if dim in b_dims and dim not in reduce]
+
+        a_common_axes = [a_dims.index(common_dim) for common_dim in common_dims]
+        b_common_axes = [b_dims.index(common_dim) for common_dim in common_dims]
+
+        a_raw = a.raw_tensor
+        b_raw = b.raw_tensor
+
+        a_shape = a_raw.shape
+        b_shape = b_raw.shape
+
+        common_axes_shape = tuple(a_shape[i] for i in a_common_axes)
+        b_common_axes_shape = tuple(b_shape[i] for i in b_common_axes)
+        assert common_axes_shape == b_common_axes_shape, "Tensor shape for common Dims of a and b does not match."
+
+        a_unique_axes_shape = tuple(a_shape[i] for i in a_unique_axes)
+        b_unique_axes_shape = tuple(b_shape[i] for i in b_unique_axes)
+
+        a_unique_axes_total_dimension = prod(a_unique_axes_shape)
+        b_unique_axes_total_dimension = prod(b_unique_axes_shape)
+
+        reduce_axes_shape = tuple(a_shape[i] for i in a_reduce_axes)
+        b_reduce_axes_shape = tuple(b_shape[i] for i in b_reduce_axes)
+        assert reduce_axes_shape == b_reduce_axes_shape, "Tensor shape for reduce Dims does not match between a and b."
+
+        reduce_axes_total_dimension = prod(reduce_axes_shape)
+
+        a_raw = torch.permute(a_raw, a_common_axes + a_unique_axes + a_reduce_axes)
+        a_raw = torch.reshape(a_raw, common_axes_shape + (a_unique_axes_total_dimension, reduce_axes_total_dimension))
+
+        b_raw = torch.permute(b_raw, b_common_axes + b_reduce_axes + b_unique_axes)
+        b_raw = torch.reshape(b_raw, common_axes_shape + (reduce_axes_total_dimension, b_unique_axes_total_dimension))
+
+        raw_result = torch.matmul(a_raw, b_raw)
+        raw_result = torch.reshape(raw_result, common_axes_shape + a_unique_axes_shape + b_unique_axes_shape)
+
+        a_unique_dims = [a_dims[i] for i in a_unique_axes]
+        b_unique_dims = [b_dims[i] for i in b_unique_axes]
+        result_dims = common_dims + a_unique_dims + b_unique_dims
+
+        result_tensor = Tensor(name="dot", dims=result_dims, raw_tensor=raw_result)
+
+        return result_tensor
---
+++ tests/test_torch_frontend.py
@@ -0,0 +1,271 @@
+"""
+tests for returnn.torch.frontend
+"""
+
+import _setup_test_env  # noqa
+
+import torch
+import pytest
+
+from returnn.tensor import Tensor, Dim
+from returnn.torch.frontend import TorchFrontend as rf
+
+
+def test_dot_scalar_multiplication():
+    a_raw = torch.tensor(2.0)
+    b_raw = torch.tensor(3.0)
+
+    a = Tensor(name="a", dims=[], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[])
+
+    assert pytest.approx(result.raw_tensor) == 6.0
+
+
+def test_dot_scalar_product():
+    a_raw = torch.tensor([1.0, 2.0, 3.0])
+    b_raw = torch.tensor([4.0, 5.0, 6.0])
+
+    feature_dim = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[feature_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[feature_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[feature_dim])
+
+    assert pytest.approx(result.raw_tensor) == 32.0
+
+
+def test_dot_outer_product():
+    a_raw = torch.tensor([1.0, 2.0, 3.0])
+    b_raw = torch.tensor([4.0, 5.0, 6.0])
+
+    a_feature_dim = Dim(dimension=3)
+    b_feature_dim = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[a_feature_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[b_feature_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[])
+
+    assert result.dims == (a_feature_dim, b_feature_dim)
+    assert result.raw_tensor.shape == (3, 3)
+
+
+def test_dot_matrix_vector_product():
+    a_raw = torch.tensor([[1.0, 2.0, 3.0], [-1.0, -2.0, -3.0]])
+    b_raw = torch.tensor([4.0, 5.0])
+
+    a_feature_dim = Dim(dimension=3)
+    reduce_dim = Dim(dimension=2)
+
+    a = Tensor(name="a", dims=[reduce_dim, a_feature_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[reduce_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[reduce_dim])
+
+    assert result.dims == (a_feature_dim,)
+    assert result.raw_tensor.tolist() == pytest.approx([-1.0, -2.0, -3.0])
+
+
+def test_dot_matrix_matrix_product():
+    a_raw = torch.tensor([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+    b_raw = torch.tensor([[1.0, -2.0], [2.0, -4.0]])
+
+    a_feature_dim = Dim(dimension=3)
+    b_feature_dim = Dim(dimension=2)
+    reduce_dim = Dim(dimension=2)
+
+    a = Tensor(name="a", dims=[a_feature_dim, reduce_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[reduce_dim, b_feature_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[reduce_dim])
+
+    assert result.dims == (a_feature_dim, b_feature_dim)
+    assert torch.allclose(result.raw_tensor, torch.tensor([[5.0, -10.0], [11.0, -22.0], [17.0, -34.0]]))
+
+
+def test_dot_scale_matrix():
+    a_raw = torch.tensor([[1.0, 2.0, 3.0], [-1.0, -2.0, -3.0]])
+    b_raw = torch.tensor(2.0)
+
+    a_feature_dim1 = Dim(dimension=2)
+    a_feature_dim2 = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[a_feature_dim1, a_feature_dim2], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[])
+
+    assert result.dims == (a_feature_dim1, a_feature_dim2)
+    assert torch.allclose(result.raw_tensor, torch.tensor([[2.0, 4.0, 6.0], [-2.0, -4.0, -6.0]]))
+
+
+def test_dot_batched_scalar_multiplication():
+    a_raw = torch.tensor([1.0, 2.0, 3.0])
+    b_raw = torch.tensor([4.0, 5.0, 6.0])
+
+    batch_dim = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[batch_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[batch_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[])
+
+    assert result.dims == (batch_dim,)
+    assert result.raw_tensor.tolist() == pytest.approx([4.0, 10.0, 18.0])
+
+
+def test_dot_batched_scalar_product():
+    a_raw = torch.tensor([[1.0, 2.0, 3.0], [-1.0, -2.0, -3.0]])
+    b_raw = torch.tensor([[4.0, 5.0, 6.0], [4.0, 5.0, 6.0]])
+
+    batch_dim = Dim(dimension=2)
+    feature_dim = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[batch_dim, feature_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[batch_dim, feature_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[feature_dim])
+
+    assert result.dims == (batch_dim,)
+    assert result.raw_tensor.tolist() == pytest.approx([32.0, -32.0])
+
+
+def test_dot_batched_outer_product():
+    a_raw = torch.tensor([[1.0, 2.0, 3.0], [-1.0, -2.0, -3.0]])
+    b_raw = torch.tensor([[4.0, 5.0, 6.0], [4.0, 5.0, 6.0]])
+
+    batch_dim = Dim(dimension=2)
+    a_feature_dim = Dim(dimension=3)
+    b_feature_dim = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[batch_dim, a_feature_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[batch_dim, b_feature_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[])
+
+    assert result.dims == (batch_dim, a_feature_dim, b_feature_dim)
+    assert result.raw_tensor.shape == (2, 3, 3)
+
+
+def test_dot_batched_matrix_vector_product():
+    a_raw = torch.tensor([[[1.0, -1.0], [2.0, -2.0]], [[3.0, -3.0], [4.0, -4.0]], [[5.0, -5.0], [6.0, -6.0]]])
+    b_raw = torch.tensor([[1.0, 2.0], [2.0, 4.0]])
+
+    batch_dim = Dim(dimension=2)
+    a_feature_dim = Dim(dimension=3)
+    reduce_dim = Dim(dimension=2)
+
+    # intentionally test strange batch axis
+    a = Tensor(name="a", dims=[a_feature_dim, reduce_dim, batch_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[batch_dim, reduce_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[reduce_dim])
+
+    assert result.dims == (batch_dim, a_feature_dim)
+    assert torch.allclose(result.raw_tensor, torch.tensor([[5.0, 11.0, 17.0], [-10.0, -22.0, -34.0]]))
+
+
+def test_dot_batched_matrix_matrix_product():
+    a_raw = torch.tensor([[[1.0, 2.0], [-1.0, -2.0]], [[3.0, 4.0], [-3.0, -4.0]], [[5.0, 6.0], [-5.0, -6.0]]])
+    b_raw = torch.tensor([[[1.0, 1.0], [2.0, 2.0]], [[3.0, 3.0], [4.0, 4.0]], [[5.0, 5.0], [6.0, 6.0]]])
+
+    batch_dim = Dim(dimension=2)
+    a_feature_dim = Dim(dimension=3)
+    b_feature_dim = Dim(dimension=3)
+    reduce_dim = Dim(dimension=2)
+
+    # intentionally test strange batch axis
+    a = Tensor(name="a", dims=[a_feature_dim, reduce_dim, batch_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[b_feature_dim, batch_dim, reduce_dim], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[reduce_dim])
+
+    assert result.dims == (batch_dim, a_feature_dim, b_feature_dim)
+    assert torch.allclose(result.raw_tensor, torch.zeros(size=(2, 3, 3)))  # values chosen such that everything cancels
+
+
+def test_dot_batched_scale_matrix():
+    a_raw = torch.tensor([2.0, 3.0])
+    b_raw = torch.tensor([[[1.0, 2.0, 3.0], [-1.0, -2.0, -3.0]], [[2.0, 3.0, 4.0], [-2.0, -3.0, -4.0]]])
+
+    batch_dim = Dim(dimension=2)
+    b_feature_dim1 = Dim(dimension=2)
+    b_feature_dim2 = Dim(dimension=3)
+
+    a = Tensor(name="a", dims=[batch_dim], dtype="float32", raw_tensor=a_raw)
+    b = Tensor(name="b", dims=[batch_dim, b_feature_dim1, b_feature_dim2], dtype="float32", raw_tensor=b_raw)
+
+    result = rf.dot(a, b, reduce=[])
+
+    assert result.dims == (batch_dim, b_feature_dim1, b_feature_dim2)
+    assert torch.allclose(
+        result.raw_tensor,
+        torch.tensor([[[2.0, 4.0, 6.0], [-2.0, -4.0, -6.0]], [[6.0, 9.0, 12.0], [-6.0, -9.0, -12.0]]]),
+    )
+
+
+def test_dot_multiple_dims():
+    a_raw = torch.rand(size=(2, 4, 6, 9, 5, 3, 8, 1))
+    b_raw = torch.rand(size=(7, 2, 6, 8, 3, 1, 5, 4))
+
+    reduce_dim_1 = Dim(dimension=3)
+    reduce_dim_2 = Dim(dimension=6)
+    reduce_dim_3 = Dim(dimension=1)
+
+    common_dim_1 = Dim(dimension=2)
+    common_dim_2 = Dim(dimension=8)
+    common_dim_3 = Dim(dimension=5)
+
+    a_unique_dim_1 = Dim(dimension=9)
+    a_unique_dim_2 = Dim(dimension=4)
+
+    b_unique_dim_1 = Dim(dimension=7)
+    b_unique_dim_2 = Dim(dimension=4)
+
+    a = Tensor(
+        name="a",
+        dims=[
+            common_dim_1,
+            a_unique_dim_2,
+            reduce_dim_2,
+            a_unique_dim_1,
+            common_dim_3,
+            reduce_dim_1,
+            common_dim_2,
+            reduce_dim_3,
+        ],
+        dtype="float32",
+        raw_tensor=a_raw,
+    )
+    b = Tensor(
+        name="b",
+        dims=[
+            b_unique_dim_1,
+            common_dim_1,
+            reduce_dim_2,
+            common_dim_2,
+            reduce_dim_1,
+            reduce_dim_3,
+            common_dim_3,
+            b_unique_dim_2,
+        ],
+        dtype="float32",
+        raw_tensor=b_raw,
+    )
+
+    result = rf.dot(a, b, reduce=[reduce_dim_1, reduce_dim_2, reduce_dim_3])
+
+    # assumes common dims as sorted in a, unique dims as sorted in a / b respectively
+    assert result.dims == (
+        common_dim_1,
+        common_dim_3,
+        common_dim_2,
+        a_unique_dim_2,
+        a_unique_dim_1,
+        b_unique_dim_1,
+        b_unique_dim_2,
+    )
+    assert result.raw_tensor.shape == (2, 5, 8, 4, 9, 7, 4)
