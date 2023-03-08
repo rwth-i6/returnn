50b3a33457d8772bc1a2270730405c42998a361c
Icemole <nahurobe3@gmail.com>
2023-03-08 18:57:16 +0100

    Torch internal frontend: expand_dims_raw, combine_raw, compare_raw

--- returnn/torch/frontend/_internal_frontend.py
+++ returnn/torch/frontend/_internal_frontend.py
@@ -40,3 +40,61 @@ class TorchInternalFrontend(InternalFrontend[torch.Tensor]):
         :return: shape of raw tensor; here for PyTorch the full shape is always known
         """
         return tuple(raw_tensor.size())
+
+    @staticmethod
+    def expand_dims_raw(raw_tensor: torch.Tensor, axis: int) -> torch.Tensor:
+        """
+        :param raw_tensor:
+        :param axis: e.g. 1
+        :return: raw tensor with new axis
+        """
+        return raw_tensor.unsqueeze(axis)
+
+    @staticmethod
+    def compare_raw(a: torch.Tensor, kind: str, b: torch.Tensor) -> torch.Tensor:
+        """
+        :param a:
+        :param kind: "equal"|"==", "less"|"<", "less_equal"|"<=", "greater"|">", "greater_equal"|">=",
+            "not_equal"|"!="|"<>"
+        :param b:
+        :return: a `kind` b
+        """
+        assert a.dim() == b.dim()
+        kind = {
+            "==": "eq",
+            "<=": "less_equal",
+            "<": "less",
+            ">=": "greater_equal",
+            ">": "greater",
+            "!=": "not_equal",
+            "<>": "not_equal",
+        }.get(kind, kind)
+        op = getattr(torch, kind)  # e.g. torch.equal
+        return op(a, b)
+
+    @staticmethod
+    def combine_raw(a: torch.Tensor, kind: str, b: torch.Tensor) -> torch.Tensor:
+        """
+        :param a:
+        :param kind: "add"|"+", "sub"|"-", "mul"|"*", "truediv"|"/", "floordiv"|"//", "mod"|"%", "pow"|"**",
+            "max"|"maximum", "min"|"minimum", "logical_and", "logical_or", "squared_difference"
+        :param b:
+        :return: a `kind` b
+        """
+        assert a.dim() == b.dim()
+        kind = {
+            "+": "add",
+            "-": "sub",
+            "*": "mul",
+            "/": "true_divide",
+            "truediv": "true_divide",
+            "//": "floor_divide",
+            "floordiv": "floor_divide",
+            "%": "remainder",  # Python-like modulo, not C-like (torch.fmod)
+            "mod": "remainder",
+            "**": "pow",
+            "max": "maximum",
+            "min": "minimum",
+        }.get(kind, kind)
+        op = getattr(torch, kind)  # e.g. torch.add
+        return op(a, b)
