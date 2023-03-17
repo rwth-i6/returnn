f47f3da80c8c7cf64f73e415a2ecc30fc9bcd27a
Albert Zeyer <albzey@gmail.com>
2023-03-17 10:13:41 +0100

    frontend types export Tensor, Dim

--- returnn/frontend/types.py
+++ returnn/frontend/types.py
@@ -5,7 +5,8 @@ Types
 from __future__ import annotations
 from typing import Union
 import numpy
+from returnn.tensor import Tensor, Dim
 
-__all__ = ["RawTensorTypes"]
+__all__ = ["RawTensorTypes", "Tensor", "Dim"]
 
 RawTensorTypes = Union[int, float, complex, numpy.number, numpy.ndarray, bool, str]
