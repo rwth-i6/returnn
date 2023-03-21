94972a5400d4409f3a1089b79a03bc928822d737
Albert Zeyer <albzey@gmail.com>
2023-03-21 15:00:22 +0100

    frontend explicit reduce functions

--- returnn/frontend/reduce.py
+++ returnn/frontend/reduce.py
@@ -10,7 +10,18 @@ from ._backend import get_backend_by_tensor
 
 T = TypeVar("T")
 
-__all__ = ["reduce"]
+__all__ = [
+    "reduce",
+    "reduce_sum",
+    "reduce_max",
+    "reduce_min",
+    "reduce_mean",
+    "reduce_logsumexp",
+    "reduce_any",
+    "reduce_all",
+    "reduce_argmin",
+    "reduce_argmax",
+]
 
 
 def reduce(
@@ -31,3 +42,117 @@ def reduce(
     """
     rf = get_backend_by_tensor(source)
     return rf.reduce(source=source, mode=mode, axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_sum(source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="sum", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_max(source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="max", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_min(source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="min", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_mean(source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="mean", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_logsumexp(
+    source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified
+) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="logsumexp", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_any(source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="any", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_all(source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="all", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_argmin(
+    source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified
+) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="argmin", axis=axis, use_time_mask=use_time_mask)
+
+
+def reduce_argmax(
+    source: Tensor[T], *, axis: Union[Dim, Sequence[Dim]], use_time_mask: bool = NotSpecified
+) -> Tensor[T]:
+    """
+    Reduce the tensor along the given axis
+
+    :param source:
+    :param axis:
+    :param use_time_mask: if True (default), use the time mask (part of dim tag) to ignore padding frames
+    :return: tensor with axis removed
+    """
+    return reduce(source=source, mode="argmax", axis=axis, use_time_mask=use_time_mask)
