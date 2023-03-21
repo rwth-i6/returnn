b974593ed8377d132164311529a731017f43b932
Albert Zeyer <albzey@gmail.com>
2023-03-21 15:14:53 +0100

    frontend doc better

--- returnn/frontend/__init__.py
+++ returnn/frontend/__init__.py
@@ -5,7 +5,7 @@ https://github.com/rwth-i6/returnn/issues/1120
 
 The convention for the user is to do::
 
-    from returnn import frontend as rf
+    import returnn.frontend as rf
 """
 
 # Not necessarily all here, but the most common ones.
