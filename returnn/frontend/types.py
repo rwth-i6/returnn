c9fc6dd6d1fc661b2f2af3f925cdce53bd17edac
Albert Zeyer <albzey@gmail.com>
2023-03-30 11:22:28 +0200

    RF get_raw_tensor_type

--- returnn/frontend/types.py
+++ returnn/frontend/types.py
@@ -3,10 +3,24 @@ Types
 """
 
 from __future__ import annotations
-from typing import Union
+from typing import Union, Type, TYPE_CHECKING
 import numpy
 from returnn.tensor import Tensor, Dim
+from ._backend import global_backend
 
-__all__ = ["RawTensorTypes", "Tensor", "Dim"]
+
+__all__ = ["RawTensorTypes", "Tensor", "Dim", "get_raw_tensor_type"]
+
 
 RawTensorTypes = Union[int, float, complex, numpy.number, numpy.ndarray, bool, str]
+
+
+def get_raw_tensor_type() -> Type:
+    """
+    :return: the raw tensor type of the current selected backend, e.g. ``torch.Tensor`` or ``tf.Tensor``
+    """
+    if TYPE_CHECKING:
+        import torch
+
+        return torch.Tensor  # just as an example
+    return global_backend.RawTensorType
