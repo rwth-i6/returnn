f06d3b63893f6ea88cd34e055c60dcad8c1d058f
Albert Zeyer <albzey@gmail.com>
2023-04-01 00:09:00 +0200

    RF net dict, register dim from dyn layer

--- returnn/tf/frontend_layers/dims.py
+++ returnn/tf/frontend_layers/dims.py
@@ -26,10 +26,11 @@ def get_dim_deps(dim: Union[Dim, Sequence[Dim]]) -> List[Tensor]:
     if not isinstance(dim, Dim):
         raise TypeError(f"expected nn.Dim, got {type(dim)}")
     dim = dim.get_same_base()
-    if dim.dimension is not None:  # static dim -> no deps
+    if dim.is_static():  # static dim -> no deps
         return []
     if dim.special:
         raise ValueError(f"{dim} deps not defined for special tags")
+    _register_dim_via_dyn_layer(dim)
     if dim in _dim_deps:
         deps = _dim_deps[dim]
         if _deps_valid_in_cur_name_ctx(deps):
@@ -49,6 +50,9 @@ def _register_dim_deps_when_novel(dim: Dim, deps: List[Tensor]):
     if dim.derived_from_op:
         return  # not needed
     dim = dim.get_same_base()
+    # noinspection PyProtectedMember
+    if _register_dim_via_dyn_layer(dim):
+        return
     if dim in _dim_deps:
         # We could just always keep the first dep list.
         # But there are cases where the new dep list might be better:
@@ -69,3 +73,33 @@ def _register_dim_deps_when_novel(dim: Dim, deps: List[Tensor]):
 
 def _deps_valid_in_cur_name_ctx(deps: List[Tensor]) -> bool:
     return all(dep.raw_tensor.root == rfl.Layer.top().root for dep in deps)
+
+
+def _register_dim_via_dyn_layer(dim: Dim) -> bool:
+    """
+    :param dim:
+    :return: whether we registered the dim
+    """
+    if dim.is_static():
+        return False
+    assert dim.dyn_size_ext
+    if dim.dyn_size_ext.raw_tensor is None:
+        return False
+    assert isinstance(dim.dyn_size_ext.raw_tensor, rfl.Layer)
+    # It means the user probably has created some dynamic dim directly.
+    # This is valid.
+    # But for the net dict backend, we must handle it differently.
+    # This is the logic as in returnn-common make_dim_from_length.
+    # The actual range tensor is never used, but this has the side effect to set up the dim tag.
+    layer_with_dim = rfl.make_layer(
+        {
+            "class": "range_from_length",
+            "from": dim.dyn_size_ext.copy(),
+            "dtype": dim.dyn_size_ext.dtype,
+            "out_spatial_dim": dim,
+        },
+        name=dim.dyn_size_ext.name,
+    )
+    _register_dim_deps_when_novel(dim, [layer_with_dim])
+    dim.dyn_size_ext.raw_tensor = None
+    return True
