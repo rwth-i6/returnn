7ea129244431b10b5d77d49b4ed62fdab0b1856b
Albert Zeyer <albzey@gmail.com>
2023-04-04 09:42:12 +0200

    RF tests, some more

--- tests/test_rf_base.py
+++ tests/test_rf_base.py
@@ -9,7 +9,80 @@ from returnn.tensor import Tensor, Dim, TensorDict, batch_dim
 from rf_utils import run_model
 
 
-def test_simple_net_linear():
+def test_linear_direct():
+    time_dim = Dim(Tensor("time", [batch_dim], dtype="int32"))
+    in_dim, out_dim = Dim(7, name="in"), Dim(13, name="out")
+    extern_data = TensorDict(
+        {
+            "data": Tensor("data", [batch_dim, time_dim, in_dim], dtype="float32"),
+            "classes": Tensor("classes", [batch_dim, time_dim], dtype="int32", sparse_dim=out_dim),
+        }
+    )
+
+    # noinspection PyShadowingNames
+    def _forward_step(*, model: rf.Linear, extern_data: TensorDict):
+        out = model(extern_data["data"])
+        out.mark_as_default_output()
+
+    run_model(extern_data, lambda *, epoch, step: rf.Linear(in_dim, out_dim), _forward_step)
+
+
+def test_linear():
+    time_dim = Dim(Tensor("time", [batch_dim], dtype="int32"))
+    in_dim, out_dim = Dim(7, name="in"), Dim(13, name="out")
+    extern_data = TensorDict(
+        {
+            "data": Tensor("data", [batch_dim, time_dim, in_dim], dtype="float32"),
+            "classes": Tensor("classes", [batch_dim, time_dim], dtype="int32", sparse_dim=out_dim),
+        }
+    )
+
+    class _Net(rf.Module):
+        def __init__(self):
+            super().__init__()
+            self.linear = rf.Linear(in_dim, out_dim)
+
+        def __call__(self, x: Tensor) -> Tensor:
+            return self.linear(x)
+
+    # noinspection PyShadowingNames
+    def _forward_step(*, model: _Net, extern_data: TensorDict):
+        out = model(extern_data["data"])
+        out.mark_as_default_output()
+
+    run_model(extern_data, lambda *, epoch, step: _Net(), _forward_step)
+
+
+def test_2layers():
+    time_dim = Dim(Tensor("time", [batch_dim], dtype="int32"))
+    in_dim, hidden_dim, out_dim = Dim(7, name="in"), Dim(11, name="hidden"), Dim(13, name="out")
+    extern_data = TensorDict(
+        {
+            "data": Tensor("data", [batch_dim, time_dim, in_dim], dtype="float32"),
+            "classes": Tensor("classes", [batch_dim, time_dim], dtype="int32", sparse_dim=out_dim),
+        }
+    )
+
+    class _Net(rf.Module):
+        def __init__(self):
+            super().__init__()
+            self.layer1 = rf.Linear(in_dim, hidden_dim)
+            self.layer2 = rf.Linear(hidden_dim, out_dim)
+
+        def __call__(self, x: Tensor) -> Tensor:
+            x = rf.relu(self.layer1(x))
+            x = self.layer2(x)
+            return x
+
+    # noinspection PyShadowingNames
+    def _forward_step(*, model: _Net, extern_data: TensorDict):
+        out = model(extern_data["data"])
+        out.mark_as_default_output()
+
+    run_model(extern_data, lambda *, epoch, step: _Net(), _forward_step)
+
+
+def test_linear_same_dim():
     time_dim = Dim(Tensor("time", [batch_dim], dtype="int32"))
     in_dim, out_dim = Dim(7, name="in"), Dim(13, name="out")
     extern_data = TensorDict(
