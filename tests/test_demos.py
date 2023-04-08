535865ceb0b6992b8c3faa5fd69219bf04c2524d
Albert Zeyer <albzey@gmail.com>
2023-04-09 01:39:03 +0200

    test_demo_rf_torch_task12ax

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -139,6 +139,13 @@ def test_demo_torch_task12ax():
     # TODO also check FER. So far this is not properly reported. https://github.com/rwth-i6/returnn/issues/1120
 
 
+@unittest.skipIf(not torch, "no PyTorch")
+def test_demo_rf_torch_task12ax():
+    cleanup_tmp_models("demos/demo-rf.config")
+    run(py, "rnn.py", "demos/demo-rf.config", print_stdout=True)
+    # TODO also check FER. So far this is not properly reported. https://github.com/rwth-i6/returnn/issues/1120
+
+
 def test_demo_iter_dataset_task12ax():
     # there should be no actual TF dependency, we just iterate the dataset
     cleanup_tmp_models("demos/demo-tf-vanilla-lstm.12ax.config")
