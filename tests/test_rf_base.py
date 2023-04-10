d460a8ed9fb17f2ca0544ba8852d7fe40da1e9ef
Albert Zeyer <albzey@gmail.com>
2023-04-10 02:07:44 +0200

    RF test_pack

--- tests/test_rf_base.py
+++ tests/test_rf_base.py
@@ -3,6 +3,7 @@ RETURNN frontend (returnn.frontend) tests
 """
 
 from __future__ import annotations
+from typing import Tuple
 import _setup_test_env  # noqa
 import returnn.frontend as rf
 from returnn.tensor import Tensor, Dim, TensorDict, batch_dim
@@ -138,5 +139,27 @@ def test_linear_cross_entropy():
         targets = extern_data["classes"]
         loss = rf.cross_entropy(estimated=logits, estimated_type="logits", target=targets, axis=out_dim)
         loss.mark_as_default_output()
+
+    run_model(extern_data, lambda *, epoch, step: _Net(), _forward_step)
+
+
+def test_pack():
+    time_dim = Dim(Tensor("time", [batch_dim], dtype="int32"))
+    in_dim = Dim(7, name="in")
+    extern_data = TensorDict(
+        {
+            "data": Tensor("data", [batch_dim, time_dim, in_dim], dtype="float32"),
+        }
+    )
+
+    class _Net(rf.Module):
+        def __call__(self, x: Tensor) -> Tuple[Tensor, Dim]:
+            pack, pack_dim = rf.pack(x, dims=[batch_dim, time_dim], enforce_sorted=False)
+            return pack, pack_dim
+
+    # noinspection PyShadowingNames
+    def _forward_step(*, model: _Net, extern_data: TensorDict):
+        out, pack_dim = model(extern_data["data"])
+        out.mark_as_default_output(shape=(pack_dim, in_dim))
 
     run_model(extern_data, lambda *, epoch, step: _Net(), _forward_step)
