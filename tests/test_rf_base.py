3edf9ca21fad5682673bda6dfff4ab8d27b81e77
Albert Zeyer <albzey@gmail.com>
2023-04-12 13:38:40 +0200

    RF test_dropout

--- tests/test_rf_base.py
+++ tests/test_rf_base.py
@@ -163,3 +163,24 @@ def test_pack():
         out.mark_as_default_output(shape=(pack_dim, in_dim))
 
     run_model(extern_data, lambda *, epoch, step: _Net(), _forward_step)
+
+
+def test_dropout():
+    time_dim = Dim(Tensor("time", [batch_dim], dtype="int32"))
+    in_dim = Dim(7, name="in")
+    extern_data = TensorDict(
+        {
+            "data": Tensor("data", [batch_dim, time_dim, in_dim], dtype="float32"),
+        }
+    )
+
+    class _Net(rf.Module):
+        def __call__(self, x: Tensor) -> Tensor:
+            return rf.dropout(x, 0.5, axis=in_dim, on_forward=True)
+
+    # noinspection PyShadowingNames
+    def _forward_step(*, model: _Net, extern_data: TensorDict):
+        out = model(extern_data["data"])
+        out.mark_as_default_output(shape=(batch_dim, time_dim, in_dim))
+
+    run_model(extern_data, lambda *, epoch, step: _Net(), _forward_step)
