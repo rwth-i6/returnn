40ea23eb3b44b70a4fbc9450d459839812765711
Albert Zeyer <albzey@gmail.com>
2023-05-04 14:59:41 +0200

    RF TF-net-dict parameter assign, more explicit control deps
    This will allow to cleanup the VariableLayer _WrappedVariable logic.

--- returnn/tf/frontend_layers/parameter_assign.py
+++ returnn/tf/frontend_layers/parameter_assign.py
@@ -7,7 +7,7 @@ for this graph-based backend.
 """
 
 from __future__ import annotations
-
+from typing import Optional, Sequence
 from tensorflow.python.util import nest
 from returnn.tensor import Tensor
 from ... import frontend as rf
@@ -25,6 +25,18 @@ def parameter_assign(param: rf.Parameter[Layer], value: Tensor, op: str = "assig
     :param op:
     :return:
     """
+    if param.raw_tensor.layer_dict["class"] == "variable":
+        # This is the first assignment on the variable.
+        # There might be some previous reads.
+        # To make sure that any previous reads are performed before the assign,
+        # we need to replace any previous access by an explicit variable-read.
+        assign_helper_initial_read = _AssignHelper(
+            param=param, read_layer_name=f"{param.name}_initial_read", read_control_deps=None
+        )
+        assign_helper_initial_read.reassign_param_variable_read()
+        # Copy over the usage refs, such that we properly replace the instance below.
+        param.raw_tensor.usages.extend(assign_helper_initial_read.old_param_raw.usages)
+
     # Find proper layer context to create the assign op.
     # This is about finding the proper control flow context.
     layer_ctx = rfl.Layer.top()
@@ -48,12 +60,15 @@ def parameter_assign(param: rf.Parameter[Layer], value: Tensor, op: str = "assig
             assign_helper.map_param_usage_to_old_param_copy()
 
             # Now set it again to the true-branch value.
-            if param.raw_tensor.layer_dict["class"] == "variable_read":
-                # Can keep that, just add the control dep.
-                param.raw_tensor.layer_dict["control_dependencies"].append(op_)
-            else:
+            if param.raw_tensor is assign_helper.old_param_raw:
                 # Set again.
                 assign_helper.reassign_param_variable_read()
+            else:
+                # Some other code has overwritten it again in the meantime.
+                # This was probably another parameter_assign.
+                assert param.raw_tensor.layer_dict["class"] == "variable_read"
+                # Can keep that, just add the control dep.
+                param.raw_tensor.layer_dict["control_dependencies"].append(op_)
 
         layer_ctx.module.cond.add_other_branch_prehook(_other_branch_prehook)
         layer_ctx.module.cond.add_other_branch_posthook(_other_branch_posthook)
@@ -61,16 +76,18 @@ def parameter_assign(param: rf.Parameter[Layer], value: Tensor, op: str = "assig
     # Make sure it is always executed.
     _utils.mark_as_output_in_scope(op_, scope=root_ctx)
 
-    assign_helper = _AssignHelper(param=param, assign_op=op_, op_kind=op)
+    assign_helper = _AssignHelper(param=param, read_layer_name=f"{param.name}_after_{op}", read_control_deps=[op_])
     assign_helper.reassign_param_variable_read()
     assign_helper.map_param_usage_to_old_param_copy()
 
 
 class _AssignHelper:
-    def __init__(self, *, param: rf.Parameter[Layer], assign_op: Tensor, op_kind: str):
+    def __init__(
+        self, *, param: rf.Parameter[Layer], read_layer_name: str, read_control_deps: Optional[Sequence[Tensor]]
+    ):
         self.param = param
-        self.assign_op = assign_op
-        self.op_kind = op_kind
+        self.read_layer_name = read_layer_name
+        self.read_control_deps = read_control_deps
 
         # We need to make sure that any further access to the param will use the new value.
         # So replace the raw param with an identity layer with control deps.
@@ -85,8 +102,8 @@ class _AssignHelper:
         with root_ctx:
             # We expect that RETURNN handles this properly.
             rfl.make_layer(
-                {"class": "variable_read", "var": self.old_param_copy, "control_dependencies": [self.assign_op]},
-                name=f"{self.param.name}_{self.op_kind}",
+                {"class": "variable_read", "var": self.old_param_copy, "control_dependencies": self.read_control_deps},
+                name=self.read_layer_name,
                 out=self.param,
             )
 
