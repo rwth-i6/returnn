effb93b0c85dd315f103e74b314236d8adff9966
Albert Zeyer <albzey@gmail.com>
2023-05-11 17:52:17 +0200

    RF tests masking small fix

--- tests/rf_utils.py
+++ tests/rf_utils.py
@@ -312,4 +312,4 @@ def _pad_mask_zeros(x: Union[TensorDict, Tensor, Dim]):
                 print(f"Warning: cannot apply mask {mask} for dim {d} on tensor {x}.")
                 continue
             mask = mask.copy_compatible_to(x, check_sparse=False, check_dtype=False)
-            x.raw_tensor = numpy.where(mask.raw_tensor, x.raw_tensor, 0.0)
+            x.raw_tensor = numpy.where(mask.raw_tensor, x.raw_tensor, 0)
