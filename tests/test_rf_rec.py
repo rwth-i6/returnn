e8fc739d2f26f16df73284e5cf0d5abd56f4c5ab
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-05-15 13:51:08 +0000

    test RF rec direct call

--- tests/test_rf_rec.py
+++ tests/test_rf_rec.py
@@ -342,3 +342,30 @@ def test_zoneout_lstm_tf_layers_vs_rf_pt():
         raise Exception(f"should not get here, mismatches: {remarks}")
 
     print("all close!")
+
+
+if __name__ == "__main__":
+    import sys
+    import unittest
+    from returnn.util import better_exchook
+
+    better_exchook.install()
+    if len(sys.argv) <= 1:
+        for k, v in sorted(globals().items()):
+            if k.startswith("test_"):
+                print("-" * 40)
+                print("Executing: %s" % k)
+                try:
+                    v()
+                except unittest.SkipTest as exc:
+                    print("SkipTest:", exc)
+                print("-" * 40)
+        print("Finished all tests.")
+    else:
+        assert len(sys.argv) >= 2
+        for arg in sys.argv[1:]:
+            print("Executing: %s" % arg)
+            if arg in globals():
+                globals()[arg]()  # assume function and execute
+            else:
+                eval(arg)  # assume Python code and execute
