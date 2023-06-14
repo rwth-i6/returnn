0963d5b0ad55145a092c1de9bba100c94ee8600c
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2023-06-14 15:49:14 +0200

    Min and max seq len for Pytorch (#1344)
    Co-authored-by: Albert Zeyer <albzey@gmail.com>

--- returnn/torch/engine.py
+++ returnn/torch/engine.py
@@ -57,6 +57,12 @@ class Engine(EngineBase):
 
         self._start_epoch = None  # type: Optional[int]
         self._final_epoch = None  # type: Optional[int]
+        self._min_seq_length = config.typed_value("min_seq_length", None) or config.int(
+            "min_seq_length", None
+        )  # type: Union[int,float,Dict[str,int],NumbersDict]
+        self._max_seq_length = config.typed_value("max_seq_length", None) or config.int(
+            "max_seq_length", None
+        )  # type: Union[int,float,Dict[str,int],NumbersDict]
         self._orig_model = None  # type: Optional[Union[rf.Module, torch.nn.Module]]
         self._pt_model = None  # type: Optional[torch.nn.Module]
         self._train_step_func = None  # type: Optional[Callable]
@@ -416,7 +422,10 @@ class Engine(EngineBase):
         )
 
         wrapped_dataset = returnn_dataset_wrapper.ReturnnDatasetIterDataPipe(dataset, reset_callback=dataset_reset)
-
+        if (self._min_seq_length is not None) or (self._max_seq_length is not None):
+            wrapped_dataset = data_pipeline.LenFilterDataPipe(
+                wrapped_dataset, min_seq_length=self._min_seq_length, max_seq_length=self._max_seq_length
+            )
         chunking = self.config.typed_value("chunking", None)
         if chunking:
             wrapped_dataset = data_pipeline.ChunkingIterDataPipe(wrapped_dataset, chunking)
--- tests/test_torch_engine.py
+++ tests/test_torch_engine.py
@@ -4,6 +4,7 @@ Tests for PyTorch engine.
 
 import _setup_test_env  # noqa
 import torch
+
 from returnn.config import Config, global_config_ctx
 from returnn.tensor import TensorDict, Tensor
 from returnn.torch.engine import Engine
@@ -51,3 +52,43 @@ def test_torch_engine():
         engine.forward_with_callback(callback=callback, dataset=dataset)
         assert callback.num_seqs == 100
         assert callback.init_called and callback.finish_called
+
+
+def test_min_seq_len():
+
+    from returnn.datasets.generating import DummyDataset
+
+    config = Config({"min_seq_length": 2})
+    dataset = DummyDataset(input_dim=1, output_dim=4, num_seqs=1, seq_len=1)
+    engine = Engine(config=config)
+    data_loader = engine._create_data_loader(dataset)
+    for _ in data_loader:
+        assert False, "Should not contain sequences"
+
+    config = Config()
+    dataset = DummyDataset(input_dim=1, output_dim=4, num_seqs=1, seq_len=3)
+    engine = Engine(config=config)
+    data_loader = engine._create_data_loader(dataset)
+    for _ in data_loader:
+        return
+    assert False, "Should have contained sequences"
+
+
+def test_max_seq_len():
+
+    from returnn.datasets.generating import DummyDataset
+
+    config = Config({"max_seq_length": 4})
+    dataset = DummyDataset(input_dim=1, output_dim=4, num_seqs=1, seq_len=5)
+    engine = Engine(config=config)
+    data_loader = engine._create_data_loader(dataset)
+    for _ in data_loader:
+        assert False, "Should not contain sequences"
+
+    config = Config()
+    dataset = DummyDataset(input_dim=1, output_dim=4, num_seqs=1, seq_len=3)
+    engine = Engine(config=config)
+    data_loader = engine._create_data_loader(dataset)
+    for _ in data_loader:
+        return
+    assert False, "Should have contained sequences"
