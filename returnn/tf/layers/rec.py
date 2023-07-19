b6eb0b7cd2db34661c1e5acdc123bd684efdd780
Albert Zeyer <albzey@gmail.com>
2023-07-19 12:51:05 +0200

    TF RecLayer template construction dims better (#1370)

--- returnn/tf/layers/rec.py
+++ returnn/tf/layers/rec.py
@@ -1868,8 +1868,29 @@ class _SubnetworkRecCell(object):
                             # from a previous (partial) construction.
                             # Copy if it matches.
                             # Do this even if there was an exception, but with new partial construction.
-                            if earlier_layer_output.matches_var_dim_pattern(layer_.output):
-                                layer_.output.size_placeholder = earlier_layer_output.size_placeholder.copy()
+                            # Earlier, we used matches_var_dim_pattern.
+                            # Now we do the same logic more explicitly, to directly take over dim tags,
+                            # and not work with size_placeholder.
+                            earlier_dyn_dims = [
+                                (i, d) for i, d in enumerate(earlier_layer_output.dims) if d.is_dynamic()
+                            ]
+                            new_dyn_dims = [(i, d) for i, d in enumerate(layer_.output.dims) if d.is_dynamic()]
+                            if len(earlier_dyn_dims) == len(new_dyn_dims):
+                                out_dims = list(layer_.output.dims)
+                                for (new_axis, new_dim), (old_axis, old_dim) in zip(new_dyn_dims, earlier_dyn_dims):
+                                    new_dim: Dim
+                                    old_dim: Dim
+                                    assert old_dim.is_dynamic() and new_dim.is_dynamic()
+                                    if new_dim.dyn_size_ext and new_dim.dyn_size_ext.raw_tensor is not None:
+                                        continue
+                                    if not old_dim.dyn_size_ext:
+                                        continue
+                                    if not new_dim.dyn_size_ext:
+                                        out_dims[new_axis] = old_dim
+                                        continue
+                                    if old_dim.dyn_size_ext.raw_tensor is not None:
+                                        out_dims[new_axis] = old_dim
+                                        continue
 
                 # It was constructed now.
                 assert layer_.is_initialized
