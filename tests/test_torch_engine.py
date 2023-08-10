3b687fd18f004556acb27605a315aeafcc790b99
Albert Zeyer <albzey@gmail.com>
2023-08-10 12:28:09 +0200

    test_torch_forward_raw_strings

--- tests/test_torch_engine.py
+++ tests/test_torch_engine.py
@@ -3,6 +3,7 @@ Tests for PyTorch engine.
 """
 
 import _setup_test_env  # noqa
+import numpy
 import torch
 
 from returnn.config import Config, global_config_ctx
@@ -32,6 +33,8 @@ def test_torch_engine():
             self.init_called = True
 
         def process_seq(self, *, seq_tag: str, outputs: TensorDict):
+            assert isinstance(seq_tag, str) and seq_tag.startswith("seq-")
+            assert isinstance(outputs, TensorDict)
             out = outputs["output"]
             assert isinstance(out, Tensor)
             assert out.batch_ndim == 2 and out.batch_shape[-1] == 9
@@ -52,6 +55,61 @@ def test_torch_engine():
         engine.forward_with_callback(callback=callback, dataset=dataset)
         assert callback.num_seqs == 100
         assert callback.init_called and callback.finish_called
+
+
+def test_torch_forward_raw_strings():
+    # In OggZipDataset, but maybe also other datasets,
+    # in combination with forward task, we get all kind of different string formats:
+    #   - seq_tag (numpy U8 str)
+    #   - raw (numpy object -> str)
+    #   - orth (uint8 bytes)
+    # Test all of them.
+
+    from test_Dataset import create_ogg_zip_txt_only_dataset
+
+    def _get_model(**_kwargs):
+        return torch.nn.Module()
+
+    def _forward_step(*, extern_data: TensorDict, **_kwargs):
+        for key, value in extern_data.data.items():
+            rf.get_run_ctx().mark_as_output(value, key)
+
+    config = Config(
+        dict(
+            task="forward",
+            extern_data={
+                "classes": {"shape": (None,), "dim": 29, "sparse": True},
+                "orth": {"shape": (None,), "dim": 256, "sparse": True},
+                "raw": {"shape": (), "dtype": "string"},
+            },
+            get_model=_get_model,
+            forward_step=_forward_step,
+        )
+    )
+
+    _demo_txt = "hello world"
+    _demo_seq_tag = "seq-000000"
+
+    class _ForwardCallback(ForwardCallbackIface):
+        def process_seq(self, *, seq_tag: str, outputs: TensorDict):
+            assert isinstance(seq_tag, str) and seq_tag == _demo_seq_tag
+            raw = outputs["raw"].raw_tensor
+            orth = outputs["orth"].raw_tensor
+            classes = outputs["classes"].raw_tensor
+            assert isinstance(raw, numpy.ndarray) and raw.dtype.name.startswith("str") and raw.shape == ()
+            raw_ = raw.item()
+            assert isinstance(raw_, str) and raw_ == _demo_txt
+            assert isinstance(orth, numpy.ndarray) and orth.dtype == numpy.uint8 and orth.ndim == 1
+            orth_ = orth.tostring()
+            assert orth_.decode("utf8") == _demo_txt
+            assert isinstance(classes, numpy.ndarray) and classes.dtype == numpy.int32 and classes.ndim == 1
+            classes_ = "".join([dataset.targets.id_to_label(c) for c in classes])
+            assert classes_ == _demo_txt + "."
+
+    with global_config_ctx(config), create_ogg_zip_txt_only_dataset(text=_demo_txt, seq_tag=_demo_seq_tag) as dataset:
+        engine = Engine(config=config)
+        engine.init_network_from_config()
+        engine.forward_with_callback(callback=_ForwardCallback(), dataset=dataset)
 
 
 def test_min_seq_len():
