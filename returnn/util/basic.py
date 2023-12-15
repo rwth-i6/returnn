190c791f4856e6f577452489d151d03672c7507b
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-15 02:36:21 +0100

    model_epoch_from_filename, handle PT, return None

--- returnn/util/basic.py
+++ returnn/util/basic.py
@@ -645,7 +645,7 @@ def model_epoch_from_filename(filename):
     """
     :param str filename:
     :return: epoch number
-    :rtype: int
+    :rtype: int|None
     """
     # We could check via:
     # tf.contrib.framework.python.framework.checkpoint_utils.load_variable()
@@ -654,16 +654,17 @@ def model_epoch_from_filename(filename):
     # We don't have it in the model, though.
     # For now, just parse it from filename.
     # If TF, and symlink, resolve until no symlink anymore (e.g. if we symlinked the best epoch).
-    while True:
-        tf_meta_fn = "%s.meta" % filename
-        if os.path.exists(tf_meta_fn) and os.path.islink(tf_meta_fn):
-            tf_meta_fn_ = os.readlink(tf_meta_fn)
-            assert tf_meta_fn_.endswith(".meta"), "strange? %s, %s" % (filename, tf_meta_fn)
-            filename = tf_meta_fn_[: -len(".meta")]
-        else:
-            break
+    for potential_ext in [".meta", ".pt"]:
+        if not os.path.exists(filename + potential_ext):
+            continue
+        while os.path.exists(filename + potential_ext) and os.path.islink(filename + potential_ext):
+            fn_with_ext_ = os.readlink(filename + potential_ext)
+            assert fn_with_ext_.endswith(potential_ext), "strange? %s, %s" % (filename, potential_ext)
+            filename = fn_with_ext_[: -len(potential_ext)]
+        break
     m = re.match(".*\\.([0-9]+)", filename)
-    assert m, "no match for %r" % filename
+    if not m:
+        return None
     return int(m.groups()[0])
 
 
