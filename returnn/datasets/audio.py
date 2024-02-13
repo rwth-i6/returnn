042f10f8249d313f6e1f0f93ed699ff8db170e2c
Benedikt Hilmes <hilmes@hltpr.rwth-aachen.de>
2024-02-13 12:03:03 +0100

    Fix typo in audio.py

--- returnn/datasets/audio.py
+++ returnn/datasets/audio.py
@@ -316,7 +316,7 @@ class OggZipDataset(CachedDataset2):
         elif seq_list is not None:
             seqs = {self._get_tag_from_info_dict(seq): i for i, seq in enumerate(self._data)}
             for seq_tag in seq_list:
-                assert seq_tag in seqs, "did not found all requested seqs. we have eg: %s" % (
+                assert seq_tag in seqs, "did not find all requested seqs. We have eg: %s" % (
                     self._get_tag_from_info_dict(self._data[0]),
                 )
             self._seq_order = [seqs[seq_tag] for seq_tag in seq_list]
