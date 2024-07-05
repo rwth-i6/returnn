c8960d0586e7465c5bd12927ada03cf9d59917dc
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-07-05 13:09:42 +0200

    playing around with mem consumption test

--- tests/test_torch_util.py
+++ tests/test_torch_util.py
@@ -23,6 +23,7 @@ def test_gradient_checkpoint_scope():
         def __init__(self, *, use_grad_ckpt: bool = False):
             super().__init__()
             self.var = torch.nn.Parameter(torch.randn(shape))
+            self.input_var = torch.nn.Parameter(torch.randn(shape))
             self.opt = torch.optim.SGD(self.parameters(), lr=0.1)  # not common to have this here but ok for the test
             self.use_grad_ckpt = use_grad_ckpt
 
@@ -39,17 +40,46 @@ def test_gradient_checkpoint_scope():
             return v_ * x
 
         def demo_run(self):
-            x = torch.randn(shape, requires_grad=True)
+            x = self.input_var
             y = self(x)
             loss = y.sum()  # dummy...
             loss.backward()
             self.opt.step()
+            self.opt.zero_grad()
 
     model = _Model(use_grad_ckpt=False)
-    with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
+    with profile(activities=[ProfilerActivity.CPU], profile_memory=True, with_stack=True, record_shapes=True) as prof:
         with record_function("train_step_no_grad_ckpt"):
             model.demo_run()
-    print(prof.key_averages().table())
+    print(prof.events())
+    for ev in prof.events():
+        ev: torch.autograd.profiler.FunctionEvent
+        # print(ev)
+        print(
+            ev.name,
+            ev.id,
+            ev.cpu_memory_usage,
+            [c.id for c in ev.cpu_children],
+            ev.cpu_parent.id if ev.cpu_parent else None,
+            ev.stack,
+        )
+    print(prof.profiler)
+    print(prof.profiler.kineto_results)
+    tot_mem_usage = 0
+    for ev in prof.profiler.kineto_results.events():
+        tot_mem_usage += ev.nbytes()
+        print(
+            ev.name(),
+            ev.nbytes(),
+            ev.stack(),
+            ev.scope(),
+            ev.sequence_nr(),
+            ev.correlation_id(),
+            tot_mem_usage,
+        )
+    mem_prof = prof._memory_profile()
+    for time, action, (tensor_key, version), size in mem_prof.timeline:
+        print(f"{time=} {action=} {tensor_key=} {version=} {size=}")
     # TODO how to understand memory? i want to know: where is what allocated? and when deallocated? total consumption?
     # TODO...
     # TODO now model = _Model(use_grad_ckpt=True) ...
