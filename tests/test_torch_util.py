ff82cdb709a648626b81abd7096759b662b1a1df
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-07-05 15:09:56 +0200

    playing around with mem consumption test cleanup

--- tests/test_torch_util.py
+++ tests/test_torch_util.py
@@ -53,20 +53,12 @@ def test_gradient_checkpoint_scope():
     with profile(activities=[ProfilerActivity.CPU], profile_memory=True, with_stack=True, record_shapes=True) as prof:
         with record_function("train_step_no_grad_ckpt"):
             model.demo_run()
-    print(prof.events())
-    for ev in prof.events():
-        ev: torch.autograd.profiler.FunctionEvent
-        # print(ev)
-        print(
-            ev.name,
-            ev.id,
-            ev.cpu_memory_usage,  # unclear what this is exactly. how much is freed? what is current total usage?
-            [c.id for c in ev.cpu_children],
-            ev.cpu_parent.id if ev.cpu_parent else None,
-            ev.stack,  # why empty?
-        )
-    print(prof.profiler)
-    print(prof.profiler.kineto_results)
+
+    # Note: I tried prof.events(), prof.profiler.kineto_results.events(), prof._memory_profile().timeline,
+    # but they all are not really giving me the information I want.
+    # Either the Python stack is missing, or the memory information is incomplete,
+    # or the Python/TorchOp events are missing.
+    # The only complete information source seems to be prof.profiler.kineto_results.experimental_event_tree().
 
     from torch.profiler._utils import traverse_dfs
     from torch._C._profiler import _EventType  # noqa
@@ -91,74 +83,6 @@ def test_gradient_checkpoint_scope():
             ) == os.path.basename(__file__):
                 print("pycall", ex0.function_name, ex0.file_name, ":", ex0.line_number, "->", ex1.function_name)
 
-    tot_mem_usage = 0
-    for ev in prof.profiler.kineto_results.events():
-        # ev: torch._C._autograd._KinetoEvent
-        tot_mem_usage += ev.nbytes()
-        print(
-            ev.name(),
-            ev.nbytes(),
-            ev.stack(),
-            ev.scope(),
-            ev.sequence_nr(),
-            ev.correlation_id(),
-            tot_mem_usage,
-        )
-    """
-    train_step_no_grad_ckpt 0 [] 7 -1 1 0
-    aten::randn 0 [] 0 -1 2 0
-    aten::empty 0 [] 0 -1 3 0
-    [memory] 41612 [] 0 -1 0 41612    # new (noise): empty
-    aten::normal_ 0 [] 0 -1 4 41612
-    aten::add 0 [] 0 0 5 41612
-    [memory] 41612 [] 0 -1 0 83224    # new (v_): add
-    [memory] -41612 [] 0 -1 0 41612   # del (noise)
-    aten::mul 0 [] 0 1 6 41612
-    [memory] 41612 [] 0 -1 0 83224    # new (v_ * x): mul
-    aten::sum 0 [] 0 2 7 83224
-    aten::sum 0 [] 0 -1 8 83224
-    [memory] 4 [] 0 -1 0 83228        # new (loss): sum
-    aten::as_strided 0 [] 0 -1 9 83228
-    aten::fill_ 0 [] 0 -1 10 83228
-    aten::ones_like 0 [] 0 -1 11 83228
-    aten::empty_like 0 [] 0 -1 12 83228
-    aten::empty_strided 0 [] 0 -1 13 83228
-    [memory] 4 [] 0 -1 0 83232
-    aten::fill_ 0 [] 0 -1 14 83232
-    autograd::engine::evaluate_function: SumBackward0 0 [] 0 2 15 83232
-    SumBackward0 0 [] 1 2 16 83232
-    aten::expand 0 [] 0 -1 17 83232
-    aten::as_strided 0 [] 0 -1 18 83232
-    autograd::engine::evaluate_function: MulBackward0 0 [] 0 1 19 83232
-    MulBackward0 0 [] 1 1 20 83232
-    aten::mul 0 [] 0 -1 21 83232
-    [memory] 41612 [] 0 -1 0 124844
-    aten::mul 0 [] 0 -1 22 124844
-    [memory] 41612 [] 0 -1 0 166456
-    [memory] -41612 [] 0 -1 0 124844
-    autograd::engine::evaluate_function: torch::autograd::AccumulateGrad 0 [] 0 -1 23 124844
-    torch::autograd::AccumulateGrad 0 [] 1 -1 24 124844
-    aten::detach 0 [] 0 -1 25 124844
-    detach 0 [] 0 -1 26 124844
-    autograd::engine::evaluate_function: AddBackward0 0 [] 0 0 27 124844
-    AddBackward0 0 [] 1 0 28 124844
-    autograd::engine::evaluate_function: torch::autograd::AccumulateGrad 0 [] 0 -1 29 124844
-    torch::autograd::AccumulateGrad 0 [] 1 -1 30 124844
-    aten::detach 0 [] 0 -1 31 124844
-    detach 0 [] 0 -1 32 124844
-    [memory] -4 [] 0 -1 0 124840
-    Optimizer.step#SGD.step 0 [] 7 -1 33 124840
-    aten::add_ 0 [] 0 -1 34 124840
-    aten::add_ 0 [] 0 -1 35 124840
-    Optimizer.zero_grad#SGD.zero_grad 0 [] 7 -1 36 124840
-    [memory] -41612 [] 0 -1 0 83228
-    [memory] -41612 [] 0 -1 0 41616
-    [memory] -41612 [] 0 -1 0 4
-    [memory] -4 [] 0 -1 0 0
-    """
-    mem_prof = prof._memory_profile()
-    for time, action, (tensor_key, version), size in mem_prof.timeline:
-        print(f"{time=} {action=} {tensor_key=} {version=} {size=}")
     # TODO how to understand memory? i want to know: where is what allocated? and when deallocated? total consumption?
     # TODO...
     # TODO now model = _Model(use_grad_ckpt=True) ...
