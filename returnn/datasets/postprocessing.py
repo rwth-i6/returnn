aa4627e4ff951859a1b1bef95914bb57bdbe1258
Moritz Gunz <moritz.gunz@gmail.com>
2024-07-25 10:56:59 -0400

    refactor to subclass from CachedDataset2

--- returnn/datasets/postprocessing.py
+++ returnn/datasets/postprocessing.py
@@ -1,34 +1,62 @@
-__all__ = ["PostprocessingDataset"]
+"""
+Provides :class:`PostprocessingDataset`.
+"""
 
-from dataclasses import dataclass
+from __future__ import annotations
+
 from numpy import ndarray
 from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple
 
-from returnn.tensor import TensorDict
-from returnn.util.basic import NumbersDict
+from returnn.datasets.basic import DatasetSeq
+from returnn.tensor import Tensor, TensorDict
 from .basic import Dataset, init_dataset
+from .cached2 import CachedDataset2
 from .util.strings import str_to_numpy_array
 
-
-@dataclass
-class LoadedData:
-    tensors: TensorDict
-    seq_idx: int
+__all__ = ["PostprocessingDataset"]
 
 
-Postprocessor = Callable[[TensorDict], TensorDict]
-StreamingPostprocessor = Callable[[Iterator[TensorDict]], Iterator[TensorDict]]
+class PostprocessingDataset(CachedDataset2):
+    """
+    A dataset that allows for generic post-processing of data from another dataset
+    using a function on the segment level and on the level of multiply segments via
+    an iterator.
 
+    This allows integrating various data augmentation techniques like e.g. Mixup, or
+    speed pertubation into the data loading pipeline.
 
-class PostprocessingDataset(Dataset):
+    The integration into the data loading pipeline makes it easy to distribute the
+    data processing work across multiple CPU cores using `MultiProcDataset` and in
+    turn frees the GPU from data preprocessing tasks.
+
+    Both functions can be specified at the same time, and their results will be
+    combined by first applying the segment-level function and then the
+    multiple-segments-level function.
+
+    Example usage:
+        train = {
+            "class": "PostprocessingDataset", "dataset": { "class": "..." },
+        }
+    """
+
     def __init__(
         self,
         dataset: Dict[str, Any],
-        map_seq: Optional[Postprocessor] = None,
-        map_seq_stream: Optional[StreamingPostprocessor] = None,
+        map_seq: Optional[Callable[[TensorDict], TensorDict]] = None,
+        map_seq_stream: Optional[Callable[[Iterator[TensorDict]], Iterator[TensorDict]]] = None,
         _meta_info_cache: Optional[Dict[str, Any]] = None,
         **kwargs,
     ):
+        """
+        :param dataset: inner dataset to be post-processed
+        :param map_seq: post processor function operating on the single-segment level.
+        :param map_seq_stream: post processor function operating on the multiple segment level via an iterator.
+            Allows merging multiple segments into one, or generating multiple output segments from one input segment.
+            When both functions are specified, the segment-level function is applied first to every segment, and the
+            results are passed to the iterator-level function.
+        :param _meta_info_cache: internal usage
+        :param kwargs: see :class:`CachedDataset2`, :class:`Dataset`
+        """
         super().__init__(**kwargs)
 
         assert (
@@ -41,51 +69,46 @@ class PostprocessingDataset(Dataset):
         self._dataset_def = dataset
         self._map_seq = map_seq or self._identity
         self._map_seq_stream = map_seq_stream or self._identity
-        self._num_seqs = None
 
-        self._added_data: List[LoadedData] = []
-        self._expected_load_seq_start = 0
         self._dataset: Optional[Dataset] = None
         self._data_keys: Optional[List[str]] = None
         self._data_iter: Optional[Iterator[Tuple[int, TensorDict]]] = None
-        self._max_seq_idx = -1
-        self._reached_final_seq = False
+        self._data_iter_seq_idx = -1
 
         if _meta_info_cache:
-            self._data_keys = _meta_info_cache["_data_keys"]
+            self._estimated_num_seqs = _meta_info_cache["_estimated_num_seqs"]
+            self.labels = _meta_info_cache["labels"]
             self.num_inputs = _meta_info_cache["num_inputs"]
             self.num_outputs = _meta_info_cache["num_outputs"]
 
-    @property
-    def num_seqs(self) -> int:
-        if self._num_seqs is not None:
-            return self._num_seqs
-        if self.epoch is None:
-            return 0
-        raise NotImplementedError
+    def initialize(self):
+        """init"""
+        self._lazy_init_num_outputs()
+        super().initialize()
 
     @property
     def _meta_info_cache(self) -> Optional[Dict[str, Any]]:
-        if self._data_keys is None:
+        if self.num_outputs is None:
             return None
         return {
-            "_data_keys": self._data_keys,
+            "_estimated_num_seqs": self._estimated_num_seqs,
+            "labels": self.labels,
             "num_inputs": self.num_inputs,
             "num_outputs": self.num_outputs,
         }
 
     def _lazy_init_num_outputs(self):
-        if self._data_keys is not None:
+        if self.num_outputs is not None:
             return
 
         dataset = init_dataset(self._dataset_def, parent_dataset=self)
         dataset.init_seq_order(epoch=1)
-        data = next(self._map_seq_stream(map(self._map_seq, self._iterate_dataset(dataset))))
+        self._estimated_num_seqs = dataset.estimated_num_seqs
+        tdict = next(self._build_dataset_iter(dataset))
 
-        self._data_keys = sorted(data.data.keys())
-        raise NotImplementedError  # TODO: num_outputs & friends
-        self.num_outputs = None
-        self.num_inputs = None
+        # TODO: is this correct?
+        self.num_inputs = tdict.data["data"].feature_dim.size
+        self.num_outputs = {k: (t.feature_dim_or_sparse_dim.size, t.ndim) for k, t in tdict.data.items()}
 
     def init_seq_order(self, epoch: Optional[int] = None, seq_list=None, seq_order=None):
         super().init_seq_order(epoch=epoch, seq_list=seq_list, seq_order=seq_order)
@@ -97,83 +120,39 @@ class PostprocessingDataset(Dataset):
         if self._dataset is None:
             self._dataset = init_dataset(self._dataset_def, parent_dataset=self)
         self._dataset.init_seq_order(epoch=epoch, seq_list=seq_list, seq_order=seq_order)
-
-        self._added_data = []
-        self._expected_load_seq_start = 0
-        self._data_iter = enumerate(self._map_seq_stream(map(self._map_seq, self._iterate_dataset(self._dataset))))
-        self._max_seq_idx = -1
-        self._num_seqs = None
-        self._reached_final_seq = False
-
+        self._data_iter = enumerate(self._build_dataset_iter(self._dataset))
+        self._data_iter_seq_idx = -1
         self._lazy_init_num_outputs()
-
         return True
 
-    def _cleanup_old_seqs(self, seq_idx_end: int):
-        i = next((i for i, el in enumerate(self._added_data) if el.seq_idx < seq_idx_end), 0)
-        del self._added_data[:i]
+    def _collect_single_seq(self, seq_idx: int) -> DatasetSeq | None:
+        assert seq_idx > self._data_iter_seq_idx, "collecting seqs must be done strictly monotonically"
+        self._data_iter_seq_idx = seq_idx
 
-    def _get_seq(self, seq_idx: int) -> Optional[LoadedData]:
-        return next((data for data in self._added_data if data.seq_idx == seq_idx), None)
-
-    def is_less_than_num_seqs(self, n: int):
-        if n < self._expected_load_seq_start:
-            return True
-        if self.epoch is None:
-            return False
-        try:
-            return super().is_less_than_num_seqs(n)
-        except NotImplementedError:  # we only define num_seqs once we reach the end of the dataset
-            assert n >= self._expected_load_seq_start
-            self._load_seqs(self._expected_load_seq_start, n + 1)
-            if self._get_seq(n) is not None:
-                return True
-            # We reached the end.
-            assert n >= self._num_seqs
-            return False
-
-    def _load_seqs(self, start: int, end: int):
-        # implementation similar to CachedDataset2
-        assert start >= self._expected_load_seq_start
-        if start > self._expected_load_seq_start:
-            # Cleanup old data.
-            self._cleanup_old_seqs(start)
-            self._expected_load_seq_start = start
-
-        while not self._added_data or self._added_data[-1].seq_idx < end:
+        while True:
             try:
-                seq_idx, tensor_dict = next(self._data_iter)
+                loaded_seq_idx, tensor_dict = next(self._data_iter)
             except StopIteration:
-                self._reached_final_seq = True
-                self._num_seqs = self._max_seq_idx + 1
-                break
+                return None
+            if loaded_seq_idx != seq_idx:
+                continue
+            seq = DatasetSeq(features=tensor_dict.as_raw_tensor_dict(), seq_idx=seq_idx)
+            return seq
 
-            loaded_data_keys = sorted(tensor_dict.data.keys())
-            assert (
-                loaded_data_keys == self._data_keys
-            ), f"mismatch in data keys returned from data iterator, expected {self._data_keys} but received {loaded_data_keys}"
-            self._added_data.append(LoadedData(tensors=tensor_dict, seq_idx=seq_idx))
-            self._max_seq_idx = seq_idx
-
-    def get_data(self, seq_idx: int, key: str) -> ndarray:
-        return self._get_seq(seq_idx).tensors[key].raw_tensor
-
-    def get_data_keys(self):
-        self._lazy_init_num_outputs()
-        return self._data_keys
-
-    def get_seq_length(self, seq_idx: int) -> NumbersDict:
-        assert seq_idx >= self._expected_load_seq_start
-        self.load_seqs(self._expected_load_seq_start, seq_idx + 1)
-        raise NotImplementedError
-        return self._get_seq(seq_idx).tensors.something
+    def _build_dataset_iter(self, dataset: Dataset) -> Iterator[TensorDict]:
+        """
+        :return: an iterator applying both the segment level and across-segment transformations on the given dataset
+        """
+        return self._map_seq_stream(map(self._map_seq, self._iterate_dataset(dataset)))
 
     @staticmethod
     def _data_dict_to_tensor_dict(data_dict: Dict[str, ndarray]) -> TensorDict:
         """
         :return: the given data dict converted to a TensorDict class
         """
+        tdict = TensorDict({k: Tensor(name=k, dtype=v.dtype, raw_tensor=v) for k, v in data_dict.items()})
         raise NotImplementedError
+        return tdict
 
     @staticmethod
     def _iterate_dataset(dataset: Dataset) -> Iterator[TensorDict]:
