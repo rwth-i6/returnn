af092f2012292872d6b0df7f52e03d52e3e61b4f
Moritz Gunz <moritz.gunz@gmail.com>
2025-01-10 13:25:29 +0100

    review suggestions

--- tests/test_threading.py
+++ tests/test_threading.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 
 import _setup_test_env  # noqa
 from subprocess import Popen, PIPE, STDOUT, CalledProcessError
-import re
 import os
 import sys
 import unittest
@@ -19,13 +18,6 @@ __my_dir__ = os.path.dirname(os.path.abspath(__file__))
 __base_dir__ = os.path.dirname(__my_dir__)
 __main_entry__ = __base_dir__ + "/rnn.py"
 py = sys.executable
-PY3 = sys.version_info[0] >= 3
-
-
-def build_env():
-    """build env"""
-    env_update = os.environ.copy()
-    return env_update
 
 
 def run(args, input=None):
@@ -33,7 +25,7 @@ def run(args, input=None):
     args = list(args)
     print("run:", args)
     # RETURNN by default outputs on stderr, so just merge both together
-    p = Popen(args, stdout=PIPE, stderr=STDOUT, stdin=PIPE, env=build_env())
+    p = Popen(args, stdout=PIPE, stderr=STDOUT, stdin=PIPE)
     out, _ = p.communicate(input=input)
     print("Return code is %i" % p.returncode)
     print("std out/err:\n---\n%s\n---\n" % out.decode("utf8"))
@@ -48,8 +40,8 @@ def test_thread_exc_hook():
         #!rnn.py
 
         backend = "torch"  # just require any backend
-        task = "nop"
         log_verbosity = 5
+        task = "nop"
 
         import threading
 
