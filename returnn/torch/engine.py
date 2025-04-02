394f35c5b0355e68c4760eb0ccb6e930794e86c5
schmitt <robin.schmitt1@rwth-aachen.de>
2025-04-02 09:23:42 +0200

    update

--- returnn/torch/engine.py
+++ returnn/torch/engine.py
@@ -20,7 +20,6 @@ from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader
 from torch import autocast
 from torch.cuda import amp
-from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 
 import returnn
@@ -132,7 +131,12 @@ class Engine(EngineBase):
         self._reset_dev_memory_caches = config.bool("reset_dev_memory_caches", False)
         self._forward_auto_split_batch_on_oom = config.bool("forward_auto_split_batch_on_oom", False)
         self._stop_on_nonfinite_train_score = config.bool("stop_on_nonfinite_train_score", True)
-        self._tensorboard_writer = SummaryWriter()
+
+        if config.bool("use_tensorboard", False):
+            from torch.utils.tensorboard import SummaryWriter
+            self._tensorboard_writer = SummaryWriter()
+        else:
+            self._tensorboard_writer = None
 
         default_float_dtype = config.value("default_float_dtype", None)
         if default_float_dtype is not None:
@@ -257,7 +261,8 @@ class Engine(EngineBase):
             self.init_train_epoch()
             self.train_epoch()
 
-        self._tensorboard_writer.close()
+        if self._tensorboard_writer:
+            self._tensorboard_writer.close()
 
         print(f"Finished training at epoch {self.epoch}, global train step {self.global_train_step}", file=log.v3)
 
@@ -485,9 +490,10 @@ class Engine(EngineBase):
                     batch_size_info=_get_batch_size_info(extern_data) if self._log_batch_size else None,
                     log_memory_usage_device=self._device if self._log_memory_usage else None,
                 )
-                # write losses/errors to tensorboard
-                for key, val in eval_info.items():
-                    self._tensorboard_writer.add_scalar(f"train/{key}", val, global_step=self.global_train_step)
+                if self._tensorboard_writer:
+                    # write losses/errors to tensorboard
+                    for key, val in eval_info.items():
+                        self._tensorboard_writer.add_scalar(f"train/{key}", val, global_step=self.global_train_step)
 
                 if self._stop_on_nonfinite_train_score:
                     if any(np.isinf(v) or np.isnan(v) for v in accumulated_losses_dict.values()):
@@ -672,13 +678,12 @@ class Engine(EngineBase):
                         start_elapsed=step_end_time - eval_start_time,
                         log_memory_usage_device=self._device if self._log_memory_usage else None,
                     )
-                    # write losses/errors to tensorboard
-                    for key, val in eval_info.items():
-                        self._tensorboard_writer.add_scalar(
-                            f"{dataset_name}/{key}",
-                            val,
-                            global_step=self.global_train_step
-                        )
+                    if self._tensorboard_writer:
+                        # write losses/errors to tensorboard
+                        for key, val in eval_info.items():
+                            self._tensorboard_writer.add_scalar(
+                                f"{dataset_name}/{key}", val, global_step=self.global_train_step
+                            )
 
                     step_idx += 1
 
