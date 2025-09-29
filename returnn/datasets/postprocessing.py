c08733f4c995bb703d9f54950357a4833471fab5
Moritz Gunz <moritz.gunz@gmail.com>
2025-09-29 16:00:29 +0200

    docs

--- returnn/datasets/postprocessing.py
+++ returnn/datasets/postprocessing.py
@@ -424,6 +424,8 @@ class PostprocessingDataset(CachedDataset2):
             _WorkerProcParent(
                 name=f"{self.__class__.__name__} {self.name} worker",
                 buffer_size=self._buf_size,
+                # use reasonable default if unset,
+                # see https://github.com/rwth-i6/returnn/pull/1765 for discussion
                 gc_interval=self._gc_interval if self._gc_interval is not None else max(self._buf_size, 100),
                 index=i,
                 map_seq=self._map_seq,
