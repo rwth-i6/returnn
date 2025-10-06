414053943de46528b063da00fd15ba83caf39073
Albert Zeyer <albzey@gmail.com>
2025-10-06 12:51:25 +0200

    test HF dataset, setup HF env

--- tests/test_datasets_huggingface.py
+++ tests/test_datasets_huggingface.py
@@ -1,8 +1,26 @@
 from __future__ import annotations
 
 import _setup_test_env  # noqa
+import os
+import tempfile
+import atexit
+import shutil
 from returnn.datasets.huggingface import HuggingfaceDataset
 from test_Dataset import dummy_iter_dataset
+
+
+def _setup_hf_env():
+    if "HF_HOME" not in os.environ:
+        os.environ["HF_HOME"] = _get_tmp_dir()
+
+
+def _get_tmp_dir() -> str:
+    fn = tempfile.mkdtemp()
+    atexit.register(lambda: shutil.rmtree(fn))
+    return fn
+
+
+_setup_hf_env()
 
 
 def test_HuggingfaceDataset():
