821a0a203b080377529e650d58c5b8b54321e47b
Albert Zeyer <albzey@gmail.com>
2025-10-13 00:31:41 +0200

    file cache helper script, more options

--- tools/file-cache.py
+++ tools/file-cache.py
@@ -21,7 +21,20 @@ def main():
     argparser = argparse.ArgumentParser(description="Dump something from dataset.")
     argparser.add_argument("--temp-dir", help="Temp directory to use.", default=None)
     argparser.add_argument("--cache-dir", help="Cache directory to use.", default=None)
-    argparser.add_argument("--require-free-space", type=_parse_size, default=0)
+    argparser.add_argument("--cleanup-require-free-space", type=_parse_size, default=0)
+    argparser.add_argument(
+        "--cleanup-files-always-older-than-days", type=float, help="Cleanup files always older than this many days."
+    )
+    argparser.add_argument(
+        "--cleanup-files-wanted-older-than-days", type=float, help="Cleanup files wanted older than this many days."
+    )
+    argparser.add_argument(
+        "--cleanup-disk-usage-wanted-free-ratio", type=float, help="Cleanup to reach this free disk space ratio."
+    )
+    argparser.add_argument(
+        "--cleanup-disk-usage-wanted-multiplier", type=float, help="Cleanup to free this multiplier of required space."
+    )
+
     argparser.add_argument("action", help="Action to perform.", choices=["info", "cleanup"])
     args = argparser.parse_args()
 
@@ -35,7 +48,29 @@ def main():
             break
     print("temp dir:", get_temp_dir(with_username=False))
 
-    cache = file_cache.FileCache(**({"cache_directory": args.cache_dir} if args.cache_dir else {}))
+    cache = file_cache.FileCache(
+        **({"cache_directory": args.cache_dir} if args.cache_dir else {}),
+        **(
+            {"cleanup_files_always_older_than_days": args.cleanup_files_always_older_than_days}
+            if args.cleanup_files_always_older_than_days is not None
+            else {}
+        ),
+        **(
+            {"cleanup_files_wanted_older_than_days": args.cleanup_files_wanted_older_than_days}
+            if args.cleanup_files_wanted_older_than_days is not None
+            else {}
+        ),
+        **(
+            {"cleanup_disk_usage_wanted_free_ratio": args.cleanup_disk_usage_wanted_free_ratio}
+            if args.cleanup_disk_usage_wanted_free_ratio is not None
+            else {}
+        ),
+        **(
+            {"cleanup_disk_usage_wanted_multiplier": args.cleanup_disk_usage_wanted_multiplier}
+            if args.cleanup_disk_usage_wanted_multiplier is not None
+            else {}
+        ),
+    )
     print("cache dir:", cache.cache_directory)
 
     disk_usage = shutil.disk_usage(cache.cache_directory)
@@ -47,8 +82,8 @@ def main():
 
     if args.action == "cleanup":
         print("Cleanup.")
-        print(f"Require free space: {human_bytes_size(args.require_free_space)}")
-        res = cache.cleanup(need_at_least_free_space_size=args.require_free_space)
+        print(f"Require free space: {human_bytes_size(args.cleanup_require_free_space)}")
+        res = cache.cleanup(need_at_least_free_space_size=args.cleanup_require_free_space)
         print(res)
 
 
