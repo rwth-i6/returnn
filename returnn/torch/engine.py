39ab278571f2a5c21e2b811bb0ad83c44d874180
Robin Schmitt <81317322+robin-p-schmitt@users.noreply.github.com>
2025-11-03 15:31:46 +0100

    Torch, add safetensors support (#1788)

--- returnn/torch/engine.py
+++ returnn/torch/engine.py
@@ -930,7 +930,12 @@ class Engine(EngineBase):
             if not os.path.exists(filename) and os.path.exists(model_epoch_filename):
                 filename = model_epoch_filename
             print("Load model %s" % (filename,), file=log.v4)
-            checkpoint_state = torch.load(filename, map_location=self._device)
+            if filename.endswith(".safetensors"):
+                from safetensors.torch import load_file as safetensors_load
+
+                checkpoint_state = safetensors_load(filename, device=self._device)
+            else:
+                checkpoint_state = torch.load(filename, map_location=self._device)
             if epoch is None:
                 epoch = checkpoint_state.get("epoch", self._start_epoch or 1)
             step = checkpoint_state.get("step", 1)
@@ -1030,7 +1035,12 @@ class Engine(EngineBase):
                         print("(No relevant parameters matching.)", file=log.v3)
                     continue
                 print(f"Pre-load weights for key '{preload_key}' from {opts['filename']}", file=log.v3)
-                preload_model_state = torch.load(opts["filename"], map_location=self._device)
+                if opts["filename"].endswith(".safetensors"):
+                    from safetensors.torch import load_file as safetensors_load
+
+                    preload_model_state = safetensors_load(opts["filename"], device=self._device)
+                else:
+                    preload_model_state = torch.load(opts["filename"], map_location=self._device)
                 if opts.get("checkpoint_key", "model") is not None:
                     # This can be used if an external checkpoint saves a checkpoint a different structure that just the
                     # model state dict. E.g., if a checkpoint is created using
