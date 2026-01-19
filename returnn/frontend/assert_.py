f0cdaa964c95c309528f8873139d52236281ba1f
Albert Zeyer <albzey@gmail.com>
2026-01-19 01:47:11 +0100

    RF assert_, automatically reduce to scalar

--- returnn/frontend/assert_.py
+++ returnn/frontend/assert_.py
@@ -2,7 +2,9 @@
 Assertion utility functions for validating conditions in Python code.
 """
 
+from __future__ import annotations
 from typing import Union
+import returnn.frontend as rf
 from returnn.tensor import Tensor
 
 
@@ -24,6 +26,8 @@ def assert_(condition: Union[Tensor, bool], message: str):
             raise AssertionError(message)
 
     elif isinstance(condition, Tensor):
+        if condition.dims:
+            condition = rf.reduce_all(condition, axis=condition.dims)  # reduce to scalar
         # noinspection PyProtectedMember
         condition._raw_backend.assert_(condition, message=message)
 
