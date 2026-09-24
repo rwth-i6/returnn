9ddc806ed6f5ed0d8607a6cd1c138b389dec2208
polycrit <sh.vladymyr@gmail.com>
2026-09-24 15:17:55 +0200

    postprocessing seq order not implemented

--- returnn/datasets/postprocessing.py
+++ returnn/datasets/postprocessing.py
@@ -317,7 +317,9 @@ class PostprocessingDataset(CachedDataset2):
     def get_current_seq_order(self):
         """:return: current seq order of wrapped dataset, if map_seq_stream is not used"""
         if self._map_seq_stream is not None:
-            raise Exception(f"{self}: get_current_seq_order is not allowed when map_seq_stream is set.")
+            raise util.OptionalNotImplementedError(
+                f"{self}: get_current_seq_order is not allowed when map_seq_stream is set."
+            )
         assert self._dataset is not None
         return self._dataset.get_current_seq_order()
 
